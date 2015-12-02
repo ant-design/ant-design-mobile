77664cd313ab5a6de90d0e0535b4445752dbaeb5
pingan1927 <pengsencai1986@gmail.com>
2015-12-02 19:48:38 +0800

    inputitem

--- components/inputitem/demo/basic.md
+++ components/inputitem/demo/basic.md
@@ -14,7 +14,28 @@ ReactDOM.render(
   <ListWrap >
     <ListBody>
       <InputItem
-        label="我是内容"
+        label="我"
+        name="yyy"
+        value=""
+        placeholder="dadads"
+        clear={true}
+        icon="camera"
+        onChange={function(){}}
+        didMount={function(){}}
+        extraFormData={{"pic1":"22","pic2":"222"}}
+      />
+      <InputItem
+        label="我是"
+        name="yyy"
+        value="dada22"
+        placeholder="dadads"
+        clear={true}
+        onChange={function(){}}
+        didMount={function(){}}
+        extraFormData={{"pic1":"22","pic2":"222"}}
+      />
+      <InputItem
+        label="我是内"
         name="yyy"
         value="dada22"
         placeholder="dadads"
@@ -23,7 +44,7 @@ ReactDOM.render(
         extraFormData={{"pic1":"22","pic2":"222"}}
       />
       <InputItem
-        label="我是内容"
+        label="我是内内"
         name="yyy"
         value="dada22"
         placeholder="dadads"
@@ -32,7 +53,15 @@ ReactDOM.render(
         extraFormData={{"pic1":"22","pic2":"222"}}
       />
       <InputItem
-        label="我是内容"
+        name="yyy"
+        value="dada22"
+        placeholder="dadads"
+        onChange={function(){}}
+        didMount={function(){}}
+        extraFormData={{"pic1":"22","pic2":"222"}}
+      />
+      <InputItem
+        label="我是内内内内"
         name="yyy"
         value="dada22"
         placeholder="dadads"
--- components/inputitem/index.jsx
+++ components/inputitem/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 const {PropTypes} = React;
 const InputItem = React.createClass({
   propTypes: {
@@ -6,6 +7,7 @@ const InputItem = React.createClass({
     name: PropTypes.string,
     value: PropTypes.any,
     placeholder: PropTypes.string,
+    clear: PropTypes.bool,
     icon: PropTypes.any,
     didMount: PropTypes.func,
     onChange: PropTypes.func,
@@ -30,17 +32,68 @@ const InputItem = React.createClass({
     this.setState({
       value: e.target.value
     });
+    if(!!this.props.clear) {
+      ReactDOM.findDOMNode(this).querySelector('.am-icon-clear').style.visibility = this.state.value.length>0?"visible":"hidden";
+    }
     this.props.onChange.call(this,e);
   },
+  _clearInput:function() {
+    this.setState({
+      value: ''
+    });
+    /*clearTrigger.addEventListener('touchstart', function () {
+      clearInput.value = '';
+      clearInput.focus();
+      clearTrigger.style.visibility = 'hidden';
+    }, false);
+    clearTrigger.addEventListener('click', function () {
+      clearInput.value = '';
+      clearInput.focus();
+      clearTrigger.style.visibility = 'hidden';
+    }, false);
+    clearInput.addEventListener('focus', function () {
+      clearTrigger.style.visibility = (clearInput.value.length > 0) ? 'visible' : 'hidden';
+    }, false);
+    clearInput.addEventListener('input', function () {
+      clearTrigger.style.visibility = (clearInput.value.length > 0) ? 'visible' : 'hidden';
+    }, false);
+    clearInput.addEventListener('blur', function () {
+      setTimeout(function(){
+        clearTrigger.style.visibility = 'hidden';
+      },20);
+    }, false);*/
+  },
+
   render: function(){
     var extraFormData = this.state.extraFormData;
     var extraFormDataArray = [];
     for(var key in extraFormData) {
       extraFormDataArray.push((<input type="hidden" key={key} name={key} value={extraFormData[key]}/>));
     }
+    var labelDom = '';
+    if(this.state.label) {
+      labelDom = (<div className="am-list-label">{this.state.label}</div>);
+    }
+
+    var clearDom = '',
+    clearClass = this.props.clear?"am-list-item am-input-autoclear":"am-list-item";
+    if(!!this.props.clear) {
+      if(this.state.value.length>0) {
+        clearDom = (<div className="am-list-clear"><i className="am-icon am-icon-clear" style={{visibility:"visible"}} data-am-mode="clear" onClick={this._clearInput}  onTouchStart={this._clearInput}></i></div>);
+      } else {
+        clearDom = (<div className="am-list-clear"><i className="am-icon am-icon-clear" data-am-mode="clear" onClick={this._clearInput}  onTouchStart={this._clearInput}></i></div>);
+      }
+    }
+
+    var iconDom,
+      iconType="";
+    if(this.state.icon) {
+      iconType = "form-"+this.state.icon;
+      iconDom = (<div className="am-list-thumb"><i className="am-icon" data-am-mode={iconType}></i></div>);
+    }
     return (
-      <div className="am-list-item" onClick={this._handleClick}>
-        <div className="am-list-label">{this.state.label}</div>
+      <div className={clearClass} onClick={this._handleClick}>
+        {labelDom}
         <div className="am-list-control">
           <input type="text"
              name={this.state.name}
@@ -49,8 +102,8 @@ const InputItem = React.createClass({
              onChange={this._onInputChange}/>
           {extraFormDataArray}
         </div>
-        <div className="am-list-clear"><i className="am-icon am-icon-clear" data-am-mode="clear"></i></div>
-        <div className="am-list-thumb"><i className="am-icon" data-am-mode="form-camera"></i></div>
+        {clearDom}
+        {iconDom}
       </div>
     );
   }
