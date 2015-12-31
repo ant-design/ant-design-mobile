220e27e5b51a1ea8a784904ce56030da4d0d0a4d
pingan1927 <pengsencai1986@gmail.com>
2015-12-31 20:01:00 +0800

    optimize

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ import React from 'react';
 require('./style/index.less');
 
 const antm = {
-  List         : require('./components/list'),
-  ListPicker     : require('./components/list-picker'),
-  ListDatePicker : require('./components/list-date-picker'),
-  ListSelector : require('./components/list-selector'),
-  InputItem    : require('./components/input-item'),
-  CheckboxItem : require('./components/checkbox-item'),
-  TextareaItem : require('./components/textarea-item'),
-  SelectItem   : require('./components/select-item'),
-  Button       : require('./components/button'),
-  Modal        : require('./components/modal'),
-  Tab          : require('./components/tab'),
-  PageResult   : require('./components/page-result'),
-  Search       : require('./components/search'),
-  SwitchItem   : require('./components/switch-item')
+  List            : require('./components/list'),
+  InputItem       : require('./components/input-item'),
+  CheckboxItem    : require('./components/checkbox-item'),
+  TextareaItem    : require('./components/textarea-item'),
+  SelectItem      : require('./components/select-item'),
+  SwitchItem      : require('./components/switch-item'),
+  Button          : require('./components/button'),
+  ListPicker      : require('./components/list-picker'),
+  ListDatePicker  : require('./components/list-date-picker'),
+  ListSelector    : require('./components/list-selector'),
+  Search          : require('./components/search'),
+  Tab             : require('./components/tab'),
+  Modal           : require('./components/modal'),
+  PageResult      : require('./components/page-result')
 };
 
 antm.version = require('./package.json').version;
