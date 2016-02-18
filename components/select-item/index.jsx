88cad21fc2c7bf825df2705ff54e853f5d7035df
加里 <xiaofeng.mxf@taobao.com>
2016-02-18 10:57:54 +0800

    update select arrow

--- components/select-item/index.jsx
+++ components/select-item/index.jsx
@@ -42,16 +42,32 @@ const SelectItem = React.createClass({
     for (let i = 0, len = options.length; i < len; i++) {
       Options.push((<option value={options[i].val} key={options[i].val}>{options[i].txt}</option>));
     }
-    let dirctionStyle = align === 'right' ? {'direction' : 'rtl'} : {};
+
+    // 2016.2 在align==right的时候，点击icon无法触发select，所以这里用style标签来修正
+    let selectStyle = {};
+    let arrowStyle = {};
+    if(align === 'right'){
+      selectStyle = {
+        direction: 'rtl',
+        paddingRight: '15px',
+        boxSizing: 'border-box'
+      };
+      arrowStyle = {
+        position: ' absolute',
+        right: '15px',
+        top: '18px'
+      };
+    }
+
     return (
       <div className={wrapCls}>
         {labelDom}
         <div className="am-list-control">
-          <select name={name} value={value} onChange={this._onChange} style={dirctionStyle}>
+          <select name={name} value={value} onChange={this._onChange} style={selectStyle}>
             {Options}
           </select>
         </div>
-        <div className="am-list-arrow"><span className="am-icon am-icon-arrow-vertical"/></div>
+        <div className="am-list-arrow" style={arrowStyle}><span className="am-icon am-icon-arrow-vertical"/></div>
       </div>
     );
   }
