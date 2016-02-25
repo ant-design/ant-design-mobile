a6fb0693d1c8600c1ae4f8bd6953965bca2516f5
yiminghe <yiminghe@gmail.com>
2016-02-25 16:07:54 +0800

    add change-author sh

---
+++ scripts/change-author.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="yiminghe@gmail.com"
+CORRECT_NAME="chengyu"
+CORRECT_EMAIL="chengyu@taobao.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
