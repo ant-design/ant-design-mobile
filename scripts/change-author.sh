aa1e4d68e4eb8c6f01e97a54ecd840fcd4f83d24
yiminghe <yiminghe@gmail.com>
2016-02-25 16:15:49 +0800

    update change author

--- scripts/change-author.sh
+++ scripts/change-author.sh
@@ -16,4 +16,4 @@ then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
-' --tag-name-filter cat -- --branches --tags
+' --tag-name-filter cat -- --branches --tags -f
