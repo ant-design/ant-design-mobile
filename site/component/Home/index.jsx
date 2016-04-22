ef636c55775ce0da73648e10c2c6b74fd139ddc3
yiminghe <yiminghe@gmail.com>
2016-04-22 11:10:15 +0800

    update index

--- site/component/Home/index.jsx
+++ site/component/Home/index.jsx
@@ -3,71 +3,30 @@ import './index.less';
 
 import * as utils from '../utils';
 
-export default class Home extends React.Component {
-  componentDidMount() {
-    this.componentDidUpdate();
+class Home extends React.Component {
+  componentWillMount() {
+    document.body.className += ` app-home`;
   }
-  componentDidUpdate() {
+
+  componentDidMount() {
     utils.setTitle('Ant Design - 一个 UI 设计语言');
   }
-  // To store style which is only for Home and has conflicts with others.
-  getStyle() {
-    return `
-      #react-content,
-      #react-content > div {
-        height: 100%;
-      }
-      .main-wrapper {
-        background: transparent;
-        width: auto;
-        margin: 0;
-        border-radius: 0;
-        padding: 0;
-        overflow: unset;
-        min-height: 600px;
-      }
-      #header {
-        position: fixed;
-        z-index: 999;
-        background: rgba(0, 0, 0, 0.25);
-        border-bottom: 1px solid transparent;
-        transition: border .5s cubic-bezier(0.455, 0.03, 0.515, 0.955), background .5s cubic-bezier(0.455, 0.03, 0.515, 0.955);
-      }
-      #header .ant-select-selection,
-      #header .ant-menu {
-        background: transparent;
-      }
-      #nav a {
-        color: #eee;
-        transition: color 0.5s cubic-bezier(0.455, 0.03, 0.515, 0.955);
-      }
-      #search-box {
-        border-left-color: rgba(235, 237, 238, .5);
-        transition: border 0.5s cubic-bezier(0.455, 0.03, 0.515, 0.955);
-      }
-      section {
-        height: 100%;
-        width: 100%;
-        background: #fff;
-      }
-      #footer {
-        background: #000;
-      }
-      #footer,
-      #footer h2 {
-        color: #999;
-      }
-      #footer a {
-        color: #eee;
-      }
-    `;
+
+  componentWillUnmount() {
+    document.body.className = document.body.className.replace(' app-home', '');
   }
 
   render() {
     return (
       <div className="main-wrapper">
-        <style dangerouslySetInnerHTML={{ __html: this.getStyle() }} />
+        <iframe
+          style={{width: 320, height: 548, display: 'block', margin: '100px auto'}}
+          frameBorder="0"
+          src="kitchen-sink.html"
+        />
       </div>
     );
   }
 }
+
+export default Home;
