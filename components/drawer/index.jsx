61ce75ab04e6c7bd13e188120d9a9c1900acb1af
warmhug <hualei5280@gmail.com>
2016-04-28 17:41:14 +0800

    add mobile drawer example

--- components/drawer/index.jsx
+++ components/drawer/index.jsx
@@ -1,19 +1,13 @@
 import React, { PropTypes } from 'react';
 import RcDrawer from 'rc-drawer';
-function noop() {}
+// function noop() {}
 
 export default class Drawer extends React.Component {
   static propTypes = {
     prefixCls: PropTypes.string,
-    onClick: PropTypes.func,
   }
   static defaultProps = {
     prefixCls: 'am-drawer',
-    onClick: noop,
-  }
-
-  onClick = () => {
-    this.props.onClick(this);
   }
 
   render() {
