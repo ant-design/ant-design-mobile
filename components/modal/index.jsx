ec873c3ee0191a1ee4b1fd4ef1a8af4d4ff1f782
silentcloud <rjmuqiang@gmail.com>
2016-04-29 09:39:31 +0800

    modal develop

--- components/modal/index.jsx
+++ components/modal/index.jsx
@@ -1,37 +1,21 @@
-import React, { PropTypes } from 'react';
-import ReactDom from 'react-dom';
-const Modal = React.createClass({
-  propTypes: {
-    style: PropTypes.object
-  },
-  getDefaultProps() {
-    return {
-      style: {}
-    };
-  },
-  updateContent() {
-    ReactDom.render(<div className="am-modal" style={this.props.style}>
-      {this.props.children}
-    </div>, this.div);
-  },
-  componentDidMount() {
-    this.div = document.createElement('div');
-    document.body.appendChild(this.div);
-    document.body.style.overflow = 'hidden';
+import Modal from './Modal';
+// import confirm from './confirm';
 
-    this.updateContent();
-  },
-  componentDidUpdate() {
-    this.updateContent();
-  },
-  componentWillUnmount() {
-    document.body.style.overflow = '';
-    ReactDom.unmountComponentAtNode(this.div);
-    document.body.removeChild(this.div);
-  },
-  render() {
-    return null;
-  }
-});
+// Modal.info = function (props) {
+//
+// };
+//
+// Modal.success = function (props) {
+//
+// };
+//
+//
+// Modal.error = function (props) {
+//
+// };
+//
+// Modal.confirm = function (props) {
+//
+// };
 
 export default Modal;
