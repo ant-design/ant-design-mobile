90cf49ebf86c6a52b23237e6060db97ed3c2a61b
pingan1927 <pengsencai1986@gmail.com>
2016-05-11 02:04:29 +0800

    list

--- components/list/ListFooter.jsx
+++ components/list/ListFooter.jsx
@@ -1,34 +1,24 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
-function noop() {}
 
 export default class ListFooter extends React.Component {
   static propTypes = {
     prefixCls: PropTypes.string,
-    style: PropTypes.object,
-    align: PropTypes.string,
-    onClick: PropTypes.func,
   };
 
   static defaultProps = {
     prefixCls: 'am-list',
-    align: 'left',
-    onClick: noop,
   };
 
-  onClick = (e) => {
-    this.props.onClick(e);
-  };
 
   render() {
-    let { style, children, className, prefixCls, align } = this.props;
+    let { prefixCls, children, className, ...other } = this.props;
     const wrapCls = classNames({
       [`${prefixCls}-footer`]: true,
-      [`${prefixCls}-right`]: align === 'right',
       [className]: className
     });
     return (
-      <div className={wrapCls} style={style} onClick={this.onClick}>
+      <div {...other} className={wrapCls}>
         {children}
       </div>
     );
