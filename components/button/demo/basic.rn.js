2beb45ae7423b25a0c1269527ef2a1f7fb5a59a5
yiminghe <yiminghe@gmail.com>
2016-05-20 16:10:01 +0800

    support .rn

--- components/button/demo/basic.rn.js
+++ components/button/demo/basic.rn.js
@@ -1,11 +1,11 @@
 import { Button } from 'antm';
 import React, { AppRegistry, Text } from 'react-native';
 
-
+// entry file is independent
 class BasicButtonExample extends React.Component {
   render() {
     return <Button><Text>basic button</Text></Button>;
   }
 }
 
-AppRegistry.registerComponent('basic', () => BasicButtonExample);
+AppRegistry.registerComponent('basic.rn', () => BasicButtonExample);
