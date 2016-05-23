558dfe493ed36f5a92ef4a223d1c47deac6fb216
沐泊 <haoquan.bai@alibaba-inc.com>
2016-05-23 11:01:28 +0800

    code review change

--- components/white-space/index.ios.js
+++ components/white-space/index.ios.js
@@ -1,24 +1,21 @@
 // WhiteSpace
 import React, { View, PropTypes } from 'react-native';
 
-const WhiteSpace = React.createClass({
-  propTypes: {
+class WhiteSpace extends React.Component {
+  static propTypes = {
     mode: PropTypes.oneOf([4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48]),
-  },
+  };
 
-  getDefaultProps() {
-    return {
-      mode: 8
-    };
-  },
+  static defaultProps = {
+    mode: 8
+  };
 
   render() {
     const marginHeight = this.props.mode;
     return (
-      // <View {...this.props} style={{height:marginHeight}}>
       <View style={[{ height: marginHeight }, this.props.style]} />
     );
   }
-});
+}
 
 export default WhiteSpace;
--- components/wing-blank/index.ios.js
+++ components/wing-blank/index.ios.js
@@ -1,27 +1,24 @@
 // WingBlank
 import React, { View, PropTypes } from 'react-native';
 
-const WingBlank = React.createClass({
-  propTypes: {
+class WingBlank extends React.Component {
+  static propTypes = {
     mode: PropTypes.oneOf([4, 8, 12, 16, 20, 24, 28, 32]),
-  },
+  };
 
-  getDefaultProps() {
-    return {
-      mode: 8
-    };
-  },
+  static defaultProps = {
+    mode: 8
+  };
 
   render() {
-    const marginLeft = this.props.mode;
-    const marginRight = this.props.mode;
+    const margin = this.props.mode;
     return (
       // <View {...this.props} style={{height:marginHeight}}>
-      <View style={[{ marginLeft: marginLeft, marginRight: marginRight }, this.props.style]}>
+      <View style={[{ marginLeft: margin, marginRight: margin }, this.props.style]}>
         {this.props.children}
       </View>
     );
   }
-});
+}
 
 export default WingBlank;
