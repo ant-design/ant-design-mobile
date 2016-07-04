6f270b5fb07aab102d06770f30388665c53b934a
yiminghe <yiminghe@gmail.com>
2016-07-04 19:21:25 +0800

    add View Text StyleSheet for web

---
+++ components/text/index.web.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import View from '../view';
+
+export default class Text extends React.Component<any, any> {
+  static defaultProps={
+    Component: 'span'
+  };
+  render() {
+    return <View {...this.props}/>;
+  }
+}
