91d52ff976e335a9f63aa3ff58fe1c99abe25d3d
silentcloud <rjmuqiang@gmail.com>
2016-07-13 10:05:28 +0800

    fix picker error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ export { default as Button } from './components/button/';
 export { default as Card } from './components/card/';
 export { default as List } from './components/list/';
 export { default as Flex } from './components/flex/';
+export { default as Picker } from './components/picker/';
 export { default as Tag } from './components/tag/';
 export { default as WhiteSpace } from './components/white-space/';
 export { default as WingBlank } from './components/wing-blank/';
