3543c67dec577d126246e32e1a768713293c5732
silentcloud <rjmuqiang@gmail.com>
2016-07-25 16:00:07 +0800

    uploader-rn

---
+++ components/uploader/demo/basic.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { Uploader, WhiteSpace } from 'antm';
+import { View, Text } from 'react-native';
+
+export default class UploaderExample extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: [{
+        url: 'https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png',
+        id: '2121',
+      }, {
+        url: 'https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png',
+        id: '2122',
+      }, {
+        url: 'https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png',
+        id: '2123',
+      }, {
+        url: 'https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png',
+        id: '2124',
+      }, {
+        url: 'https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png',
+        id: '2125',
+      }, {
+        url: 'https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png',
+        id: '2126',
+      }],
+      files2: [],
+    };
+  }
+
+  render() {
+    return (
+      <View style={{ marginTop: 20, marginLeft: 20 }}>
+        <Uploader
+          onChange={(files) => {
+            console.log(files);
+            this.setState({
+              files,
+            });
+          }}
+          files={this.state.files}
+        />
+        <WhiteSpace />
+        <Uploader
+          onChange={(files2) => {
+            console.log(files2);
+            this.setState({
+              files2,
+            });
+          }}
+          files={this.state.files2}
+        />
+      </View>
+    );
+  }
+}
