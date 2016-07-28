6a7be838037220c83698ddd1b085e3c90e8ad35d
afc163 <afc163@gmail.com>
2016-07-28 12:20:33 +0800

    fix site

--- docs/react/upgrade-notes.md
+++ docs/react/upgrade-notes.md
@@ -3,15 +3,8 @@ order: 3
 english: 升级指南
 ---
 
-此处着重列出升级中的不兼容变化和推荐改动。所有变动请见 [Changelog](/#/components/changelog)。
-
+此处着重列出升级中的不兼容变化和推荐改动。所有变动请见 [Changelog](/changelog)。
 
 ## 0.5.x => 0.6.0
-请查看 [Changelog](/#/components/changelog?scrollTo=0.6.0)
 
-## 0.1.29 => 0.5.0
-
-注意：两个版本间变动比较大、具体变化不一一列出。
-
-为什么变化巨大？`0.1.29` 版本是基于 AntUI (支付宝钱包设计规范)，由于各种原因、后续版本不再支持此规范。
-因此，从`0.5.0`版本开始，切换到支持 Ant Design 设计规范上去。
+请查看 [Changelog](/changelog#0.6.0)。
--- site/theme/index.js
+++ site/theme/index.js
@@ -1,23 +1,12 @@
 const contentTmpl = './template/Content/index';
-// 不要加外部 plugin
-// import injectTapEventPlugin from 'react-tap-event-plugin';
-// injectTapEventPlugin(); // fix Warning: Unknown prop `onTouchTap`
 
 module.exports = {
-  categoryOrder: {
-    组件: 0,
-    十大原则: 0,
-    设计基础: 1,
-    动画: 2,
-  },
+  categoryOrder: {},
   typeOrder: {
     APIS: 0,
     COMPONENTS: 1,
   },
   docVersions: {
-    '0.1.x': 'http://01x.antm.alipay.net',
-    '0.5.x': 'http://05x.antm.alipay.net',
-    '0.6.x': 'http://06x.antm.alipay.net',
   },
   routes: [{
     path: '/',
