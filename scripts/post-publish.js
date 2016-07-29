c4d623b88495849d3ed3ae2c268a3164141e5c79
yiminghe <yiminghe@gmail.com>
2016-07-29 12:04:04 +0800

    clean

--- scripts/post-publish.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-if (fs.existsSync(path.join(__dirname, '../package.json.bak'))) {
-  fs.renameSync(path.join(__dirname, '../package.json.bak'), path.join(__dirname, '../package.json'));
-}
--- scripts/pre-publish.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const packageInfo = require('../package.json');
-packageInfo.name = '@alipay/antd-mobile';
-fs.renameSync(path.join(__dirname, '../package.json'), path.join(__dirname, '../package.json.bak'));
-fs.writeFileSync(path.join(__dirname, '../package.json'), JSON.stringify(packageInfo, null, 2));
