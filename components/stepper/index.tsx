b7843a83374311ed7746a5bb9ad7913ba976d896
silentcloud <rjmuqiang@gmail.com>
2016-08-02 17:16:11 +0800

    fix android stepper

--- components/stepper/index.tsx
+++ components/stepper/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import RcInputNumber from 'rc-input-number/lib';
 import StepProps from './StepPropTypes';
 import styles from 'rc-input-number/lib/styles';
+import { Platform } from 'react-native';
 
 export default class Stepper extends React.Component<StepProps, any> {
   static defaultProps = {
@@ -12,8 +13,16 @@ export default class Stepper extends React.Component<StepProps, any> {
   };
 
   render() {
+    const restProps = Object.assign({}, this.props);
+    const inputAndroidStyle = Platform.OS === 'android' ? {
+      top: 6,
+      paddingTop: 0,
+    } : {};
+    const inputStyle = Object.assign({}, inputAndroidStyle, this.props.inputStyle);
+    delete restProps.inputStyle;
+
     return (
-      <RcInputNumber {...this.props} />
+      <RcInputNumber {...restProps} inputStyle={inputStyle} />
     );
   }
 }
