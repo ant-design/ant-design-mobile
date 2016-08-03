da7125df1b76bf59e3a848c7f4087005c3827f46
然则 <hualei.hl@alibaba-inc.com>
2016-08-03 10:58:55 +0800

    fix list-view indexed list

--- components/list-view/MyList.tsx
+++ components/list-view/MyList.tsx
@@ -3,8 +3,8 @@ import List from '../list';
 
 export default React.createClass({
   render() {
-    const { children, style, onScroll } = this.props;
-    const divProps = { style, onScroll };
+    const { children, className, style, onScroll } = this.props;
+    const divProps = { className, style, onScroll };
     return React.cloneElement(<List />, divProps, children);
   },
 });
