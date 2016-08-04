b322d853f4129b5f04263e521a0795191d95c9ab
afc163 <afc163@gmail.com>
2016-08-04 16:57:00 +0800

    fix tsc

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Text } from 'react-native';
+import { MenuProps, MenuState } from './propTypes';
 
-export default class AntMenu extends React.Component {
+export default class AntMenu extends React.Component<MenuProps, MenuState> {
   static defaultProps = {
   };
   render() {
