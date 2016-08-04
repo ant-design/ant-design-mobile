0d72fe9f63241c5e21bdfb5852a84d50917d8583
silentcloud <rjmuqiang@gmail.com>
2016-08-04 16:34:42 +0800

    fix backandroid

--- rn-kitchen-sink/index.js
+++ rn-kitchen-sink/index.js
@@ -5,9 +5,10 @@ import {
   View,
   StatusBar,
   Platform,
+  BackAndroid,
 } from 'react-native';
 import codePush from 'react-native-code-push';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Reducer, Actions, ActionConst } from 'react-native-router-flux';
 
 import Home from './components/Home';
 import RnIndex from './components/RnIndex';
@@ -38,7 +39,30 @@ const styles = StyleSheet.create({
   },
 });
 
+let isMainScreen = false;
+const reducerCreate = params => {
+  const defaultReducer = new Reducer(params);
+  return (state, action) => {
+    if (action.type === ActionConst.FOCUS && action.scene && action.scene.initial) {
+      isMainScreen = true;
+    } else {
+      isMainScreen = false;
+    }
+    return defaultReducer(state, action);
+  };
+};
+
 class AntmRnApp extends React.Component {
+  componentWillMount() {
+    BackAndroid.addEventListener('hardwareBackPress', () => {
+      if (!isMainScreen) {
+        Actions.pop();
+        return true;
+      }
+      return false;
+    });
+  }
+
   componentDidMount() {
     codePush.sync({
       updateDialog: {
@@ -85,7 +109,7 @@ class AntmRnApp extends React.Component {
     return (
       <View style={{ flex: 1 }}>
         <StatusBar barStyle="light-content" />
-        <Router>
+        <Router createReducer={reducerCreate}>
           <Scene key="root" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle}>
             <Scene key="home" component={Home} title="Ant Design Mobile" initial />
             <Scene key="web" component={WebIndex} title="Antm Web Component" />
