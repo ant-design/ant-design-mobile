15c2882bc36ccd8852680b563ab3f019331a6595
afc163 <afc163@gmail.com>
2016-08-04 14:41:52 +0800

    code style

--- components/float-menu/index.tsx
+++ components/float-menu/index.tsx
@@ -2,32 +2,35 @@ import * as React from 'react';
 import { PropTypes } from 'react';
 import Menu, { MenuContext, MenuOptions, MenuOption, MenuTrigger } from 'react-native-menu';
 import tsPropsType from './PropsType';
-// import splitObject from '../_util/splitObject';
 
 export default class FloatMenu extends React.Component<tsPropsType, any> {
   static propTypes = {
     children: PropTypes.any,
   };
   static defaultProps = {
-    onSelect: () => { },
+    onSelect: () => {},
   };
   static Item = MenuOption;
   render() {
-    const { children, onSelect, overlay, disabled,
-      name, style, triggerStyle, overlayStyle, contextStyle, renderOverlayComponent } = this.props;
+    const {
+      children, onSelect, overlay, disabled, contextStyle,
+      name, style, triggerStyle, overlayStyle, renderOverlayComponent,
+    } = this.props;
     const menuOptionsProp = {
       optionsContainerStyle: overlayStyle,
       renderOptionsContainer: renderOverlayComponent,
     };
-    return (<MenuContext ref="menuContext" style={contextStyle}>
-      <Menu name={name} onSelect={onSelect} style={style}>
-        <MenuTrigger disabled={disabled} style={triggerStyle}>
-          {children}
-        </MenuTrigger>
-        <MenuOptions {...menuOptionsProp}>
-          {overlay}
-        </MenuOptions>
-      </Menu>
-    </MenuContext>);
+    return (
+      <MenuContext ref="menuContext" style={contextStyle}>
+        <Menu name={name} onSelect={onSelect} style={style}>
+          <MenuTrigger disabled={disabled} style={triggerStyle}>
+            {children}
+          </MenuTrigger>
+          <MenuOptions {...menuOptionsProp}>
+            {overlay}
+          </MenuOptions>
+        </Menu>
+      </MenuContext>
+    );
   }
 }
