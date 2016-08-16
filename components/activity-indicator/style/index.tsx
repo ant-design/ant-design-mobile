9d2d0d892038cd267c35aa8c217efdaa4a252575
观通 <lijin.jlj@alipay.com>
2016-08-16 11:09:02 +0800

    fix toast vertical centering.

--- components/activity-indicator/style/index.tsx
+++ components/activity-indicator/style/index.tsx
@@ -2,6 +2,28 @@ import variables from '../../style/variables';
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'transparent',
+  },
+  innerContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  wrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 89,
+    height: 89,
+    borderRadius: 6,
+    backgroundColor: 'rgba(34,34,34,0.6)',
+  },
   tip: {
     color: '#999',
     fontSize: 14,
