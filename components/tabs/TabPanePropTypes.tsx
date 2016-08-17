41f83688d21dee4c4fe1d40acc03420a2a5918af
silentcloud <rjmuqiang@gmail.com>
2016-08-17 14:15:51 +0800

    code back

--- components/tabs/TabPanePropTypes.tsx
+++ components/tabs/TabPanePropTypes.tsx
@@ -1,16 +1,17 @@
 interface TabPaneProps {
   tab: string;
   key: string;
-
-  // itemKey: string;
-  // activeKey: string;
-  // onTabClick: (key: string) => void;
-  // underlineColor?: string;
-  // activeUnderlineColor?: string;
-  // textColor?: string;
-  // activeTextColor?: string;
-  // /*web only*/
-  // rootPrefixCls?: string;
+  /*private*/
+  itemKey?: string;
+  activeKey?: string;
+  onTabClick?: (key: string) => void;
+  underlineColor?: string;
+  activeUnderlineColor?: string;
+  textColor?: string;
+  activeTextColor?: string;
+  animation?: boolean;
+  /*web only*/
+  rootPrefixCls?: string;
 }
 
 export default TabPaneProps;
