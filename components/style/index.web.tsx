e02b73480ab871497191c33c6f72214bb1cdde33
pingan1927 <pengsencai1986@gmail.com>
2016-08-19 17:03:29 +0800

    update

--- components/style/index.web.tsx
+++ components/style/index.web.tsx
@@ -1,6 +1,2 @@
-const pixelRatio = Math.floor(window.devicePixelRatio || 1);
-if (pixelRatio >= 2) {
-  document.querySelector('html').classList.add(`pixel-ratio-${pixelRatio}`);
-}
 import 'normalize.css/normalize.css';
 import './index.less';
