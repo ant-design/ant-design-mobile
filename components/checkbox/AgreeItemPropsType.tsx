4db3cdefc355ccdc20ba0b1760143bd7e8c289a0
法新 <linlinfaxin@163.com>
2016-08-24 21:25:12 +0800

    support tint color & AgreeItem support TextNode child

--- components/checkbox/AgreeItemPropsType.tsx
+++ components/checkbox/AgreeItemPropsType.tsx
@@ -2,6 +2,7 @@ import CheckboxProps from './CheckboxPropsType';
 
 interface AgreeItemPropsType extends CheckboxProps {
   children?: any;
+  checkboxStyle?: any; // rn only
 }
 
 export default AgreeItemPropsType;
