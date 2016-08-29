d8fcf55b3af4b17f3cdb32ede9471d76ce5f564c
silentcloud <rjmuqiang@gmail.com>
2016-08-29 18:14:21 +0800

    fix swipeaction modal

--- components/modal/style/index.tsx
+++ components/modal/style/index.tsx
@@ -12,6 +12,7 @@ export default StyleSheet.create({
     borderRadius: variables.radius_md,
     width: 286,
     paddingTop: variables.v_spacing_xl,
+    overflow: 'hidden',
   },
   header: {
     fontSize: variables.font_size_heading,
@@ -41,5 +42,31 @@ export default StyleSheet.create({
     fontSize: 40,
     fontWeight: '200',
     color: '#bcbcbc',
+  },
+  buttnGroupH: {
+    flexDirection: 'row',
+    borderTopWidth: variables.border_width_sm,
+    borderColor: variables.border_color_base,
+  },
+  buttnGroupV: {
+    flexDirection: 'column',
+
+  },
+  buttnWrapH: {
+    flex: 1,
+    borderColor: variables.border_color_base,
+    borderRightWidth: variables.border_width_sm,
+    paddingVertical: 11,
+  },
+  buttnWrapV: {
+    flex: 1,
+    borderTopWidth: variables.border_width_sm,
+    borderColor: variables.border_color_base,
+    paddingVertical: 11,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: variables.color_link,
+    fontSize: variables.link_button_font_size,
   },
 });
