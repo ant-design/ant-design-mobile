4f380b0185d3e4d1e33dfcd204a262df015f8231
yiminghe <yiminghe@gmail.com>
2016-08-31 19:16:23 +0800

    fix ts error

--- components/_util/touchableFeedback.tsx
+++ components/_util/touchableFeedback.tsx
@@ -4,52 +4,54 @@
  */
 import * as React from 'react';
 
-export interface TouchableFeedbackProps {
-  onTouchStart?: Function;
-  onTouchEnd?: Function;
-  onTouchCancel?: Function;
-}
-export interface TouchableFeedbackState {
-  touchFeedback: boolean;
-}
+export default function touchableFeedBack(ComposedComponent) {
+  const TouchableFeedbackComponent = React.createClass<{
+    onTouchStart?: Function;
+    onTouchEnd?: Function;
+    onTouchCancel?: Function;
+  }, any>({
+    getInitialState() {
+      return {
+        touchFeedback: false,
+      };
+    },
 
-export default ComposedComponent => class extends React.Component<TouchableFeedbackProps, TouchableFeedbackState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      touchFeedback: false,
-    };
-  }
-  setTouchFeedbackState = (touchFeedback) => {
-    this.setState({
-      touchFeedback,
-    });
-  };
-  onTouchStart = (e) => {
-    if (this.props.onTouchStart) {
-      this.props.onTouchStart(e);
-    }
-    this.setTouchFeedbackState(true);
-  };
-  onTouchEnd = (e) => {
-    if (this.props.onTouchEnd) {
-      this.props.onTouchEnd(e);
-    }
-    this.setTouchFeedbackState(false);
-  };
-  onTouchCancel = (e) => {
-    if (this.props.onTouchCancel) {
-      this.props.onTouchCancel(e);
-    }
-    this.setTouchFeedbackState(false);
-  };
-  render() {
-    return <ComposedComponent
-      {...this.props}
-      touchFeedback={this.state.touchFeedback}
-      onTouchStart={this.onTouchStart}
-      onTouchEnd={this.onTouchEnd}
-      onTouchCancel={this.onTouchCancel}
+    setTouchFeedbackState(touchFeedback) {
+      this.setState({
+        touchFeedback,
+      });
+    },
+
+    onTouchStart(e) {
+      if (this.props.onTouchStart) {
+        this.props.onTouchStart(e);
+      }
+      this.setTouchFeedbackState(true);
+    },
+
+    onTouchEnd(e) {
+      if (this.props.onTouchEnd) {
+        this.props.onTouchEnd(e);
+      }
+      this.setTouchFeedbackState(false);
+    },
+
+    onTouchCancel(e) {
+      if (this.props.onTouchCancel) {
+        this.props.onTouchCancel(e);
+      }
+      this.setTouchFeedbackState(false);
+    },
+
+    render() {
+      return <ComposedComponent
+        {...this.props}
+        touchFeedback={this.state.touchFeedback}
+        onTouchStart={this.onTouchStart}
+        onTouchEnd={this.onTouchEnd}
+        onTouchCancel={this.onTouchCancel}
       />;
-  }
-};
+    },
+  });
+  return TouchableFeedbackComponent;
+}
