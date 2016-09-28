68a7f2b32d9cfd868234b2317964dd58800d98ab
左宜 <qi.lqlq@alipay.com>
2016-09-28 10:36:10 +0800

    try svg-symbols

--- components/icon/index.web.tsx
+++ components/icon/index.web.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
-import classNames from 'classnames';
+// import classNames from 'classnames';
 
-export interface IconPropType {
-  type: string;
-  className?: string;
-  style?: React.CSSProperties;
-  size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg';
-}
+// export interface IconPropType {
+//   type: string;
+//   className?: string;
+//   style?: React.CSSProperties;
+//   size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg';
+// }
 
 export default class Icon extends React.Component<IconPropType, any> {
-  static defaultProps = {
-    size: 'md',
-  };
+  // static defaultProps = {
+  //   size: 'md',
+  // };
   render() {
+    /*
     const { type, className, style, size } = this.props;
     const iconClassName = classNames({
       'am-icon': true,
@@ -20,6 +21,13 @@ export default class Icon extends React.Component<IconPropType, any> {
       [`am-icon-${size}`]: true,
       [className]: !!className,
     });
+
     return <i className={iconClassName} style={style} />;
+    */
+
+    const svgSymbols = require('./style/assets/svg-symbols.svg');
+    return <svg>
+      <use xlinkHref={`${svgSymbols}#aliwangwang`} />
+    </svg>;
   }
 }
