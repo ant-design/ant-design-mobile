f826a31ad0a7a2e538cd20745a0d490f6cc2c4f6
然则 <hualei.hl@alibaba-inc.com>
2016-10-09 16:06:58 +0800

    add onPickerChange api #345

---
+++ site/data/province-lite.js
@@ -0,0 +1,25 @@
+/* eslint comma-dangle:0 */
+/**
+ * data for Picker async col
+ */
+const data = [{
+  value: 'bj',
+  label: '北京市',
+}, {
+  value: 'zj',
+  label: '浙江省',
+}, {
+  value: 'gd',
+  label: '广东省',
+}, {
+  value: 'hn',
+  label: '海南省',
+}, {
+  value: 'cq',
+  label: '重庆市',
+}, {
+  value: 'sc',
+  label: '四川省',
+}];
+
+export default data;
