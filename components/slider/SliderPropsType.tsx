7facb1116af23e5ba2bd922a3df62c4e2e4cfb34
silentcloud <rjmuqiang@gmail.com>
2016-10-21 17:37:11 +0800

    fix: slider tipFormatter ts

--- components/slider/SliderPropsType.tsx
+++ components/slider/SliderPropsType.tsx
@@ -2,7 +2,7 @@ interface SliderProps {
   onChange?: (value?: number) => void;
   onAfterChange?: (value?: number) => void;
   defaultValue?: number;
-  tipFormatter?: Function;
+  tipFormatter?: Function | null;
   value?: number;
   min?: number;
   max?: number;
