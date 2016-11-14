4d8bfca268f3de253ed6b8896704a16e34e02c8e
silentcloud <rjmuqiang@gmail.com>
2016-11-14 17:08:06 +0800

    update rn carousel demo

--- components/carousel/demo/basic.tsx
+++ components/carousel/demo/basic.tsx
@@ -9,34 +9,34 @@ export default class BasicCarouselExample extends React.Component<any, any> {
   }
   render() {
     return (
-      <View>
-      <View style={{ paddingHorizontal: 15 }}>
-        <Carousel
-          style={styles.wrapper}
-          autoplayTimeout={1}
-          selectedIndex={2}
-          autoplay={true}
-          infinite={true}
-          afterChange={this.onselectedIndexChange}
-        >
-          <View style={[styles.container, { backgroundColor: 'red' }]}>
-            <Text>Carousel 1</Text>
-          </View>
-          <View style={[styles.container, , { backgroundColor: 'blue' }]}>
-            <Text>Carousel 2</Text>
-          </View>
-          <View style={[styles.container, { backgroundColor: 'yellow' }]}>
-            <Text>Carousel 3</Text>
-          </View>
-          <View style={[styles.container, { backgroundColor: 'black' }]}>
-            <Text>Carousel 4</Text>
-          </View>
-          <View style={[styles.container, { backgroundColor: '#ccc' }]}>
-            <Text>Carousel 5</Text>
-          </View>
-        </Carousel>
-        <Text>Carousel 根据内容自适应高度</Text>
-      </View>
+      <View style={{ marginTop: 20 }}>
+        <View style={{ paddingHorizontal: 15 }}>
+          <Carousel
+            style={styles.wrapper}
+            autoplayTimeout={2}
+            selectedIndex={2}
+            autoplay={true}
+            infinite={true}
+            afterChange={this.onselectedIndexChange}
+          >
+            <View style={[styles.container, { backgroundColor: 'red' }]}>
+              <Text>Carousel 1</Text>
+            </View>
+            <View style={[styles.container, , { backgroundColor: 'blue' }]}>
+              <Text>Carousel 2</Text>
+            </View>
+            <View style={[styles.container, { backgroundColor: 'yellow' }]}>
+              <Text>Carousel 3</Text>
+            </View>
+            <View style={[styles.container, { backgroundColor: 'black' }]}>
+              <Text>Carousel 4</Text>
+            </View>
+            <View style={[styles.container, { backgroundColor: '#ccc' }]}>
+              <Text>Carousel 5</Text>
+            </View>
+          </Carousel>
+          <Text>Carousel 根据内容自适应高度</Text>
+        </View>
       </View>
     );
   }
