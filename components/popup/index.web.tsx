e252aa29baa5e0f87fc67f251159a0c305e6cf6a
Zhang Weijie <email@zhangweijie.cn>
2016-11-22 14:38:56 +0800

    fix popup when call hide before show (#556)

--- components/popup/index.web.tsx
+++ components/popup/index.web.tsx
@@ -84,6 +84,8 @@ export default class Popup {
     });
   }
   static hide = () => {
-    ins.defaultInstance.close();
+    if (ins.defaultInstance) {
+      ins.defaultInstance.close();
+    }
   }
 }
