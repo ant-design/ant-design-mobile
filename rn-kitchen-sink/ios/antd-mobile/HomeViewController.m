a09d04f941c2313d9196ea3432cd6b2f16c72071
silentcloud <rjmuqiang@gmail.com>
2016-11-30 16:46:57 +0800

    update rn 0.38

--- rn-kitchen-sink/ios/antd-mobile/HomeViewController.m
+++ rn-kitchen-sink/ios/antd-mobile/HomeViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "HomeViewController.h"
+#import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
 #import "CodePush.h"
 #import "ScanViewController.h"
@@ -21,34 +22,34 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  
+
   self.navigationController.navigationBarHidden = YES;
-  
+
   NSURL *jsCodeLocation;
-  
+
   #ifdef DEBUG
-    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/rn-kitchen-sink/index.ios.bundle?platform=ios&dev=true"];
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"rn-kitchen-sink/index.ios" fallbackResource:nil];
   #else
     jsCodeLocation = [CodePush bundleURLForResource:@"index.ios" withExtension:@"bundle"];
   #endif
-  
+
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"kitchen-sink"
                                                initialProperties:nil
                                                    launchOptions:nil];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
   rootView.frame = self.view.bounds;
-  
+
   UIButton *button=[UIButton buttonWithType:UIButtonTypeCustom];
   [button setImage:[UIImage imageNamed:@"scan"] forState:UIControlStateNormal];
   button.hidden = YES;
   button.frame = CGRectMake([UIScreen mainScreen].bounds.size.width - 42, 31, 22, 22);
   [button addTarget:self action:@selector(pushView) forControlEvents:UIControlEventTouchUpInside];
   _scanButton = button;
-  
+
   [self.view addSubview:rootView];
   [self.view addSubview:button];
-  
+
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showScanButton) name:RCTJavaScriptDidLoadNotification object:nil];
 }
 
@@ -67,10 +68,10 @@
                                           otherButtonTitles:@"Ok",nil];
     alert.tag = 100;
     [alert setAlertViewStyle:UIAlertViewStyleLoginAndPasswordInput];
-    
+
     UITextField *nameField = [alert textFieldAtIndex:0];
     nameField.placeholder =  @"moduleName";
-    
+
     UITextField *urlField = [alert textFieldAtIndex:1];
     [urlField setSecureTextEntry:NO];
     urlField.placeholder = @"bundleURL";
@@ -97,10 +98,10 @@
       self.bundleURL = urlField.text;
     }
     // 先根据 url 判断项目类型
-    
+
     NSURL *url = [NSURL URLWithString:self.bundleURL];
     NSString *filePath = url.path;
-    
+
     NSString *moduleName = nil;
     if ([filePath hasSuffix:@"bundle"] || [filePath hasSuffix:@"jsbundle"] || [filePath hasSuffix:@"js"] ) {
       UITextField *moduleNameField = [alertView textFieldAtIndex:0];
@@ -143,7 +144,7 @@
   @try {
     NSURL *url = [NSURL URLWithString:string];
     NSString *filePath = url.path;
-  
+
     if ([filePath hasSuffix:@"bundle"] || [filePath hasSuffix:@"jsbundle"] || [filePath hasSuffix:@"js"] ) {
       // 弹框输入 moduleName
       UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"请输入 moduleName"
@@ -168,9 +169,9 @@
                                 alertControllerWithTitle:nil
                                 message:@"该二维码不是一个有效的项目入口文件地址"
                                 preferredStyle:UIAlertControllerStyleAlert];
-    
+
     UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
-    
+
     [alert addAction:defaultAction];
   }
 }
