6c9fa3e41679ee3c3f2db861ff612689a7940983
yiminghe <yiminghe@gmail.com>
2016-12-28 14:47:35 +0800

    fix style

--- index.web.js
+++ index.web.js
@@ -1,24 +1,13 @@
 /* eslint no-console:0 */
 // export components to browser's window for `dist/antd-mobile.js`
 
-function camelCase(name) {
-  return name.charAt(0).toUpperCase() +
-    name.slice(1).replace(/-(\w)/g, (m, n) => n.toUpperCase());
-}
-
-var req = require.context('./components', true, /^\.\/[^_][\w-]+\/(style\/)?index\.web\.tsx?$/);
+var req = require.context('./components', true, /^\.\/[^_][\w-]+\/style\/index\.web\.tsx?$/);
 
 req.keys().forEach((mod) => {
-  var v = req(mod);
-  if (v && v.default) {
-    v = v.default;
-  }
-  var match = mod.match(/^\.\/([^_][\w-]+)\/index\.web\.tsx?$/);
-  if (match && match[1]) {
-    exports[camelCase(match[1])] = v;
-  }
+  req(mod);
 });
 
+module.exports = require('./components');
 
 if (typeof console !== 'undefined' && console.warn) {
   console.warn(`you are using prebuild antd-mobile,
