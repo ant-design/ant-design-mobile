e96d09574b7e882b5b31c21aa3c70800d2bfaa1e
silentcloud <rjmuqiang@gmail.com>
2017-01-09 16:48:43 +0800

    improve rn-actionsheet, move android ui code to android container

--- components/action-sheet/AndroidContainer.tsx
+++ components/action-sheet/AndroidContainer.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { DeviceEventEmitter } from 'react-native';
-import styles from './style/index';
+import { DeviceEventEmitter, View, Text, TouchableHighlight } from 'react-native';
+import styles, { vars as variables } from './style/index';
 import Modal from 'rc-dialog/lib/Modal';
 
 export interface Props {
   onAnimationEnd?: (visibile: boolean) => void;
   visible?: boolean;
+  share?: boolean;
+  config: any;
+  callback?: (index: number) => void;
 }
 
 export default class ActionSheetAndroid extends React.Component<Props, any> {
+  static defaultProps = {
+    share: false,
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -33,18 +40,54 @@ export default class ActionSheetAndroid extends React.Component<Props, any> {
   }
 
   render() {
+    const { config, share, callback, onAnimationEnd} = this.props;
+    const {
+      title, message, url, options, destructiveButtonIndex, cancelButtonIndex, excludedActivityTypes,
+    } = config;
+    const titleMsg = share ? (
+      url && <View style={styles.title} key="0"><Text>{url}</Text></View>
+    ) : (
+      title && <View style={styles.title} key="0"><Text style={styles.titleText}>{title}</Text></View>
+    );
+    const cb = (index) => {
+      (DeviceEventEmitter as any).emit('antActionSheetHide');
+      if (callback) {
+        callback(index);
+      }
+    };
     return (
       <Modal
         animationDuration={200}
         animateAppear
         visible={this.state.visible}
-        onAnimationEnd={this.props.onAnimationEnd}
+        onAnimationEnd={onAnimationEnd}
         style={styles.content}
         animationType="slide-up"
         maskClosable
         onClose={this.close}
       >
-        {this.props.children}
+        <View>
+          {titleMsg}
+          {message && <View style={styles.message} key="1"><Text>{message}</Text></View> }
+          <View>
+            {share ? (
+              excludedActivityTypes.map((item, index) => <View key={index}>{item}</View>)
+            ) : (options as Array<string>).map((item, index) => (
+              <View key={index} style={[cancelButtonIndex === index ? styles.cancelBtn : null]}>
+                <TouchableHighlight
+                  style={[ styles.btn ]}
+                  underlayColor={variables.fill_tap}
+                  onPress={() => cb(index) }
+                >
+                  <Text style={[ destructiveButtonIndex === index ? styles.destructiveBtn : null ]}>
+                    {item}
+                  </Text>
+                </TouchableHighlight>
+                {cancelButtonIndex === index ? <View style={styles.cancelBtnMask} /> : null}
+              </View>
+            ))}
+          </View>
+        </View>
       </Modal>
     );
   }
--- components/action-sheet/index.tsx
+++ components/action-sheet/index.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
-import {
-  View,
-  DeviceEventEmitter,
-  ActionSheetIOS,
-  Platform,
-  Text,
-  TouchableHighlight,
-} from 'react-native';
-import styles, { vars as variables } from './style/index';
+import { DeviceEventEmitter, ActionSheetIOS, Platform } from 'react-native';
 import topView from 'rn-topview';
 import ActionSheetAndroidContainer from './AndroidContainer';
 
@@ -16,77 +8,21 @@ let ActionSheet = ActionSheetIOS as any;
 if (Platform.OS !== 'ios') {
   ActionSheet = {
     showActionSheetWithOptions(config, callback) {
-      const { title, message, options, destructiveButtonIndex, cancelButtonIndex } = config;
-      const titleMsg = [
-        title ? <View style={styles.title} key="0"><Text style={styles.titleText}>{title}</Text></View> : null,
-        message ? <View style={styles.message} key="1"><Text>{message}</Text></View> : null,
-      ];
-      const cb = (index) => {
-        (DeviceEventEmitter as any).emit('antActionSheetHide');
-        if (callback) {
-          callback(index);
-        }
-      };
-      const children = (
-        <View>
-          {titleMsg}
-          <View>
-            {(options as Array<string>).map((item, index) => (
-              <View key={index} style={[cancelButtonIndex === index ? styles.cancelBtn : null]}>
-                <TouchableHighlight
-                  style={[
-                    styles.btn,
-                  ]}
-                  underlayColor={variables.fill_tap}
-                  onPress={() => cb(index) }
-                >
-                  <Text
-                    style={[
-                      destructiveButtonIndex === index ? styles.destructiveBtn : null,
-                    ]}
-                  >
-                    {item}
-                  </Text>
-                </TouchableHighlight>
-                {cancelButtonIndex === index ? <View style={styles.cancelBtnMask} /> : null}
-              </View>
-            ))}
-          </View>
-        </View>
-      );
-
       topView.set(
         <ActionSheetAndroidContainer visible onAnimationEnd={visible => {
           if(!visible) {
             topView.remove();
           }
-        }}>
-          {children}
-        </ActionSheetAndroidContainer>
+        }} config={config} callback={callback} />
       );
     },
     showShareActionSheetWithOptions(config: any) {
-      const { url, message, excludedActivityTypes } = config;
-      const titleMsg = [
-        url ? <View style={styles.title} key="0"><Text>{url}</Text></View> : null,
-        message ? <View style={styles.message} key="1"><Text>{message}</Text></View> : null,
-      ];
-      const children = (
-        <View>
-          {titleMsg}
-          <View>
-            {excludedActivityTypes.map((item, index) => <View key={index}>{item}</View>)}
-          </View>
-        </View>
-      );
       topView.set(
         <ActionSheetAndroidContainer visible onAnimationEnd={visible => {
           if(!visible) {
             topView.remove();
           }
-        }}>
-          {children}
-        </ActionSheetAndroidContainer>
+        }} config={config} share />
       );
     },
     close() {
