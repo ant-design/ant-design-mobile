40bcaeeb3ed894d3f6c2266f866a9976699c128a
然则 <hualei.hl@alibaba-inc.com>
2017-01-12 14:55:58 +0800

    add package.json main field, add error warn ##602

---
+++ components/warn.tsx
@@ -0,0 +1,5 @@
+if (typeof console !== 'undefined' && console.error) {
+  console.error(`Note:
+please use https://github.com/ant-design/babel-plugin-import to reduce app bundle size.`);
+}
+export default {};
