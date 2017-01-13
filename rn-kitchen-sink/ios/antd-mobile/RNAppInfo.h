1d63948b6955415ce584dde767fff298b9e490a5
silentcloud <rjmuqiang@gmail.com>
2017-01-13 17:19:51 +0800

    update demo app rn version

---
+++ rn-kitchen-sink/ios/antd-mobile/RNAppInfo.h
@@ -0,0 +1,15 @@
+//
+//  RNAppInfo.h
+//  antd-mobile
+//
+//  Created by silentcloud on 2016/11/8.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+#import "RCTLog.h"
+
+@interface RNAppInfo : NSObject <RCTBridgeModule>
+@end
+
