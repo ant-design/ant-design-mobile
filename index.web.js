0b17fef2e3c5fdc10777a8669e0f3d316c519f84
然则 <hualei.hl@alibaba-inc.com>
2017-01-19 18:12:54 +0800

    fix spelling mistake

--- index.web.js
+++ index.web.js
@@ -21,6 +21,6 @@ req.keys().forEach((mod) => {
 
 
 if (typeof console !== 'undefined' && console.warn) {
-  console.warn(`you are using prebuild antd-mobile,
+  console.warn(`You are using prebuilt antd-mobile,
 please use https://github.com/ant-design/babel-plugin-import to reduce app bundle size.`);
 }
