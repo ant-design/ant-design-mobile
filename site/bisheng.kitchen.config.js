034074cc0f160789a07d8a7a66f11a69548f1cb0
Benjy Cui <benjytrys@gmail.com>
2017-01-20 16:41:46 +0800

    deps: upgrade bisheng

--- site/bisheng.kitchen.config.js
+++ site/bisheng.kitchen.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const pxtorem = require('postcss-pxtorem');
 const commonConfig = require('./bisheng.common.config');
-const indexDemos = require('./mobile/').indexDemos;
 
 module.exports = Object.assign({}, commonConfig, {
   port: 8002,
@@ -13,24 +12,6 @@ module.exports = Object.assign({}, commonConfig, {
   entryName: 'kitchen-sink',
   theme: './site/mobile',
   htmlTemplate: path.join(__dirname, './mobile/static/template.html'),
-  pick: {
-    components: commonConfig.pick.components,
-    /* eslint-disable consistent-return */
-    indexDemos(markdownData) {
-      const paths = markdownData.meta.filename.split('/');
-      // add demos to index page, e.g. "components/drawer/demo/basic.md"
-      if (paths[1] && indexDemos.indexOf(paths[1]) > -1 && paths[2] && paths[2] === 'demo') {
-        return {
-          component: paths[1],
-          meta: markdownData.meta,
-        };
-      }
-    },
-  },
-  plugins: [
-    'bisheng-plugin-antd',
-    'bisheng-plugin-react?lang=__react',
-  ],
   doraConfig: {
     verbose: true,
     plugins: ['dora-plugin-upload'],
--- site/mobile/index.js
+++ site/mobile/index.js
@@ -1,3 +1,7 @@
+const path = require('path');
+
+const indexDemos = ['drawer', 'list-view'];
+
 module.exports = {
   siteTitle: 'AlipayUI',
   siteSubTitle: '支付宝移动端组件库',
@@ -21,6 +25,39 @@ module.exports = {
     Combination: 6,
     Gesture: 5,
   },
+  lazyLoad(nodePath, nodeValue) {
+    if (typeof nodeValue === 'string') {
+      return true;
+    }
+    return nodePath.endsWith('/demo');
+  },
+  pick: {
+    components(markdownData) {
+      const filename = markdownData.meta.filename;
+      if (!/^components/.test(filename) ||
+          /\/demo$/.test(path.dirname(filename))) return;
+      /* eslint-disable consistent-return */
+      return {
+        meta: markdownData.meta,
+      };
+      /* eslint-enable consistent-return */
+    },
+    /* eslint-disable consistent-return */
+    indexDemos(markdownData) {
+      const paths = markdownData.meta.filename.split('/');
+      // add demos to index page, e.g. "components/drawer/demo/basic.md"
+      if (paths[1] && indexDemos.indexOf(paths[1]) > -1 && paths[2] && paths[2] === 'demo') {
+        return {
+          component: paths[1],
+          meta: markdownData.meta,
+        };
+      }
+    },
+  },
+  plugins: [
+    'bisheng-plugin-antd',
+    'bisheng-plugin-react?lang=__react',
+  ],
   routes: [{
     path: '/',
     component: './template/KitchenSink/index',
