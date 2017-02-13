3b05bf9fda0fe35b23691e410323174c2522d8e2
BccSafe <bccsafe5988@gmail.com>
2017-02-13 22:49:17 +0800

    补充list-view类型声明

--- components/list-view/PropsType.tsx
+++ components/list-view/PropsType.tsx
@@ -1,12 +1,39 @@
+import React from 'react';
+
 interface Props {
   children?: any;
   refreshControl?: any;
+  onScroll?: Function;
+  scrollEventThrottle?: number;
+  removeClippedSubviews?: boolean; // offscreen views are removed
+  dataSource: any;
+  renderSeparator?: Function;
+  renderRow: Function;
+  initialListSize?: number;
+  onEndReached?: Function;
+  onEndReachedThreshold?: number;
+  pageSize?: number;
+  renderFooter?: Function;
+  renderHeader?: Function;
+  renderSectionHeader?: Function;
+  renderScrollComponent?: Function;
+  scrollRenderAheadDistance?: number;
+  onChangeVisibleRows?: Function;
   /** web only */
   className?: string;
   prefixCls?: string;
   listPrefixCls?: string;
   listViewPrefixCls?: string;
+  style?: React.CSSProperties;
+  contentContainerStyle?: React.CSSProperties;
+  renderBodyComponent?: Function;
+  renderSectionBodyWrapper?: Function;
+  sectionBodyClassName?: string;
   useZscroller?: boolean;
+  useBodyScroll?: boolean;
+  stickyHeader?: boolean;
+  stickyProps?: any; // https://github.com/captivationsoftware/react-sticky/blob/master/README.md#sticky--props
+  stickyContainerProps?: any;
 }
 
 export default Props;
