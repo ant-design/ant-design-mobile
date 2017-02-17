29e30938fe7f212277261f34e6469589b4570347
paranoidjk <hust2012jiangkai@gmail.com>
2017-02-17 20:45:16 +0800

    chore(site): auto redirect to mobile demo

--- site/desktop/src/template/Layout/index.jsx
+++ site/desktop/src/template/Layout/index.jsx
@@ -23,7 +23,18 @@ export default class Layout extends React.Component {
     router: React.PropTypes.object.isRequired,
   };
 
+  checkIfMobile = () => {
+    const ua = window.navigator.userAgent.toLowerCase();
+    if (ua.indexOf('android') !== -1 || ua.indexOf('iphone') !== -1) {
+      return true;
+    }
+    return false;
+  }
+
   componentDidMount() {
+    if (this.checkIfMobile()) {
+      location.href = '/kitchen-sink/';
+    }
     if (typeof ga !== 'undefined' && !gaListenerSetted) {
       this.context.router.listen((loc) => {
         window.ga('send', 'pageview', loc.pathname + loc.search);
