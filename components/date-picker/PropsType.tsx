38c70d6f33befc99938b31995a6595a7c58770cc
BccSafe <bccsafe5988@gmail.com>
2017-02-22 23:34:53 +0800

    fix DatePicker tsx err

--- components/date-picker/PropsType.tsx
+++ components/date-picker/PropsType.tsx
@@ -12,11 +12,13 @@ interface DatePickerProps {
   minDate?: any;
   maxDate?: any;
   locale?: any;
+  disabled?: boolean;
   /** rn only */
   triggerTypes?: string;
   styles?: any;
   /** web only */
   prefixCls?: string;
+  className?: string;
   pickerPrefixCls?: string;
   popupPrefixCls?: string;
   dismissText?: string|React.ReactElement<any>; // React.ReactElement only for web
