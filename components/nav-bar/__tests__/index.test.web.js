7e5d43eae2ec76dce5dcf16180af96828d790e98
然则 <hualei.hl@alibaba-inc.com>
2017-03-23 18:41:52 +0800

    add ActionSheet/NavBar test

---
+++ components/nav-bar/__tests__/index.test.web.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, mount } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
+import NavBar from '../index.web';
+
+describe('NavBar', () => {
+  it('renders correctly', () => {
+    const wrapper = render(
+      <NavBar leftContent="返回" mode="light" rightContent={'i'}>NavBar</NavBar>,
+    );
+    expect(renderToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('trigger event correctly', () => {
+    const onLeftClick = (e) => {
+      // console.log('onLeftClick');
+      expect(e.target.className).toBe('am-navbar-left');
+    };
+    const wrapper = mount(
+      <NavBar onLeftClick={onLeftClick}>NavBar</NavBar>,
+    );
+    wrapper.find('.am-navbar-left').simulate('click');
+  });
+});
