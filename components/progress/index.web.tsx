beb369c001da728eade2f56fd3fc68ae3ad9f1f1
Colder Xihk <cncolder@users.noreply.github.com>
2017-04-11 02:31:22 -0500

    add progress className prop (#1126)

--- components/progress/index.web.tsx
+++ components/progress/index.web.tsx
@@ -24,13 +24,14 @@ export default class Progress extends React.Component<ProgressProps, any> {
     }
   }
   render() {
-    const { prefixCls, position, unfilled, style = {} } = this.props;
+    const { className, prefixCls, position, unfilled, style = {} } = this.props;
     const percentStyle = {
       width: this.noAppearTransition || !this.props.appearTransition ? `${this.props.percent}%` : 0,
       height: 0,
     };
 
     const wrapCls = classNames({
+      [className as string]: className,
       [`${prefixCls}-outer`]: true,
       [`${prefixCls}-fixed-outer`]: position === 'fixed',
       [`${prefixCls}-hide-outer`]: unfilled === 'hide',
