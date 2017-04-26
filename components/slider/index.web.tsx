15e6ab9dbdae93aecc1157c8e948d9d87668596b
silentcloud <rjmuqiang@gmail.com>
2017-04-26 18:04:19 +0800

    revert slider

--- components/slider/index.web.tsx
+++ components/slider/index.web.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import RcSlider from 'rc-slider/lib/Slider';
-import Handle from 'rc-slider/lib/Handle';
 import SliderProps from './PropsType';
 
 export default class Slider extends React.Component<SliderProps, any> {
@@ -8,28 +7,8 @@ export default class Slider extends React.Component<SliderProps, any> {
     prefixCls: 'am-slider',
   };
   render() {
-    const { min, max } = this.props;
-    let handle = this.props.handle;
-    if (!handle) {
-      handle = (props) => {
-        const { disabled, value, ...restProps } = props;
-        delete restProps.dragging;
-        return (
-          <Handle
-            role="slider"
-            aria-valuenow={value}
-            aria-disabled={disabled}
-            aria-valuemax={max}
-            aria-valuemin={min}
-            {...restProps}
-          />
-        );
-      };
-    }
     return (
-      <div className={`${this.props.prefixCls}-wrapper`}>
-        <RcSlider {...this.props} handle={handle}  />
-      </div>
+      <div className={`${this.props.prefixCls}-wrapper`}><RcSlider {...this.props} /></div>
     );
   }
 }
