da16e0d4392d8fb7685b41bd7b0f7b726a124138
silentcloud <rjmuqiang@gmail.com>
2017-05-09 14:26:31 +0800

    update rn-result demo, ref #1199

--- components/result/demo/basic.tsx
+++ components/result/demo/basic.tsx
@@ -5,28 +5,28 @@ import { Result } from 'antd-mobile';
 export default class ResultExample extends React.Component<any, any> {
   render() {
     return (<ScrollView style={{ backgroundColor: '#F5F5F9', flex: 1 }}>
-      <Text style={{ margin: 10, color: '#999' }}>uri</Text>
+      <Text style={{ margin: 10, color: '#999' }}>URI 图片</Text>
       <Result
         imgUrl={{ uri: 'https://zos.alipayobjects.com/rmsportal/GcBguhrOdlYvGfnsXgrE.png' }}
         title="验证成功"
         message="所提交内容已成功完成验证"
       />
 
-      <Text style={{ margin: 10, color: '#999' }}>require</Text>
+      <Text style={{ margin: 10, color: '#999' }}>Image source</Text>
       <Result
         imgUrl={require('./alipay@3x.png')}
         title="验证成功"
         message="所提交内容已成功完成验证"
       />
 
-      <Text style={{ margin: 10, color: '#999' }}>img</Text>
+      <Text style={{ margin: 10, color: '#999' }}>Image element</Text>
       <Result
         img={<Image source={require('./alipay@3x.png')} style={{ width: 60, height: 60 }} />}
         title="验证成功"
         message={<Text>所提交内容已成功完成验证</Text>}
       />
 
-      <Text style={{ margin: 10, color: '#999' }}>button</Text>
+      <Text style={{ margin: 10, color: '#999' }}>含 button 操作</Text>
       <Result
         img={<Image source={require('./alipay@3x.png')} style={{ width: 60, height: 60 }} />}
         title="验证成功"
