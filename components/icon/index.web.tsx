3b29b344857d920be6547ecf9eb7c4ddd1d7709e
paranoidjk <hust2012jiangkai@gmail.com>
2017-05-18 13:32:02 +0800

    fix: PropType warning caused by react _interopRequireWildcard

--- components/icon/index.web.tsx
+++ components/icon/index.web.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import classNames from 'classnames';
 
 // http://stackoverflow.com/questions/29891458/webpack-require-every-file-in-directory
