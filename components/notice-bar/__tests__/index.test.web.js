5f02d7f965e7489eb4ecf32b38705e84c39bd971
然则 <hualei.hl@alibaba-inc.com>
2017-06-13 13:27:22 +0800

    add NoticeBar test, ref #1274

---
+++ components/notice-bar/__tests__/index.test.web.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, shallow } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
+import NoticeBar from '../index.web';
+
+describe('NoticeBar', () => {
+  it('renders correctly', () => {
+    const wrapper = render(<NoticeBar>foo</NoticeBar>);
+    expect(renderToJson(wrapper)).toMatchSnapshot();
+  });
+
+  describe('onClick', () => {
+    let handleClick;
+    let wrapper;
+
+    beforeEach(() => {
+      handleClick = jest.fn();
+      wrapper = shallow(
+        <NoticeBar mode="closable" onClick={handleClick}>
+          Notice: The arrival time of incomes and
+        </NoticeBar>,
+      );
+      wrapper.find('.am-notice-bar-operation').simulate('click');
+    });
+
+    it('fires onClick event', () => {
+      expect(handleClick).toBeCalledWith();
+    });
+
+    it('change state', () => {
+      expect(wrapper.state('show')).toBe(false);
+    });
+  });
+});
