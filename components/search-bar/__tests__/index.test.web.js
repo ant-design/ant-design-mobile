78f9fe4e21b9f613894890b341f797a972d6b74b
然则 <hualei.hl@alibaba-inc.com>
2017-06-13 14:39:22 +0800

    add SearchBar test, ref #1274

---
+++ components/search-bar/__tests__/index.test.web.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, mount } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
+import SearchBar from '../index.web';
+
+describe('SearchBar', () => {
+  it('renders correctly', () => {
+    const wrapper = render(<SearchBar placeholder="搜索" />);
+    expect(renderToJson(wrapper)).toMatchSnapshot();
+  });
+
+  describe('test some events', () => {
+    let handler;
+    let wrapper;
+
+    beforeEach(() => {
+      handler = jest.fn();
+    });
+
+    it('fires onChange event', () => {
+      wrapper = mount(<SearchBar onChange={handler} />);
+      wrapper.find('input').simulate('change', { target: { value: 'foo' } });
+      expect(handler).toBeCalledWith('foo');
+    });
+
+    it('fires onFocus event', () => {
+      wrapper = mount(<SearchBar onFocus={handler} />);
+      wrapper.find('input').simulate('focus');
+      expect(handler).toBeCalledWith();
+    });
+
+    it('fires onBlur event', () => {
+      wrapper = mount(<SearchBar onBlur={handler} />);
+      wrapper.find('input').simulate('blur');
+      expect(handler).toBeCalledWith();
+    });
+
+    it('fires onCancel event', () => {
+      wrapper = mount(<SearchBar value="test" onCancel={handler} />);
+      wrapper.find('.am-search-cancel').simulate('click');
+      expect(handler).toBeCalledWith('test');
+    });
+
+    it('fires onClear event', () => {
+      wrapper = mount(<SearchBar onClear={handler} />);
+      wrapper.find('a').simulate('click');
+      expect(handler).toBeCalledWith('');
+    });
+  });
+});
