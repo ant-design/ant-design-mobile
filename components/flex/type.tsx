7b2f28189c9f3ff0d1db9544303283558f7312f1
BccSafe <bccsafe5988@gmail.com>
2017-06-16 11:06:11 +0800

    fix item tsx

--- components/flex/type.tsx
+++ components/flex/type.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FlexWebProps, FlexNativeProps, FlexItemWebProps, FlexItemNativeProps } from './PropsType';
 
+export class FlexItem extends React.Component<FlexItemWebProps | FlexItemNativeProps, any> {}
+
 export default class Flex extends React.Component<FlexWebProps | FlexNativeProps, any> {
-  static Item: React.Component<FlexItemWebProps | FlexItemNativeProps, any>;
+  static Item: FlexItem;
 }
--- components/list/type.tsx
+++ components/list/type.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ListWebProps, ListNativeProps, ListItemWebProps, ListItemNativeProps } from './PropsType';
 
+export class ListItem extends React.Component<ListItemWebProps | ListItemNativeProps, any> {}
+
 export default class List extends React.Component<ListWebProps | ListNativeProps, any> {
-  static Item: React.Component<ListItemWebProps | ListItemNativeProps, any>;
+  static Item: ListItem;
 }
