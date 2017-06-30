815afc19ac64d68c926848682b4a78a71dc41ea2
silentcloud <rjmuqiang@gmail.com>
2017-06-30 16:47:21 +0800

    add issue url

--- components/switch/__tests__/index.test.web.js
+++ components/switch/__tests__/index.test.web.js
@@ -14,6 +14,7 @@ describe('SegmentedControl', () => {
     const wrapper = shallow(<Switch checked onChange={onChange} />);
     expect(wrapper.find('input').is('input[value="on"]')).toBeTruthy();
     // I don't know why click/change doesn't change the value to `off`
+    // ref https://github.com/facebook/react/issues/961 https://github.com/airbnb/enzyme/issues/216
     // wrapper.find('input').simulate('click', { target: { checked: false } });
     // expect(wrapper.find('input').is('input[value="off"]')).toBeTruthy();
   });
