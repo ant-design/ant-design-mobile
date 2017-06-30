1d0de70abb4cc66fefb33b7b4d2681fb0c4c6f99
paranoidjk <hust2012jiangkai@gmail.com>
2017-06-30 18:59:37 +0800

    fix: test

--- components/switch/__tests__/index.test.web.js
+++ components/switch/__tests__/index.test.web.js
@@ -12,10 +12,7 @@ describe('SegmentedControl', () => {
   it('check api', () => {
     const onChange = jest.fn();
     const wrapper = shallow(<Switch checked onChange={onChange} />);
-    expect(wrapper.find('input').is('input[value="on"]')).toBeTruthy();
-    // I don't know why click/change doesn't change the value to `off`
-    // ref https://github.com/facebook/react/issues/961 https://github.com/airbnb/enzyme/issues/216
-    // wrapper.find('input').simulate('click', { target: { checked: false } });
-    // expect(wrapper.find('input').is('input[value="off"]')).toBeTruthy();
+    wrapper.find('input').simulate('change', { target: { checked: false } });
+    expect(onChange).toHaveBeenCalledWith(false);
   });
 });
