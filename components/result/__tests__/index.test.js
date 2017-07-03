4ffe391612750aed90a8137d84901312fd0e3794
silentcloud <rjmuqiang@gmail.com>
2017-07-03 15:14:10 +0800

    fix result-rn test

--- components/result/__tests__/index.test.js
+++ components/result/__tests__/index.test.js
@@ -8,7 +8,7 @@ describe('Result', () => {
   it('renders correctly', () => {
     const wrapper = renderer.create(
       <Result
-        imgUrl="https://zos.alipayobjects.com/rmsportal/qlMpMyJTcSjMpKAgtcEt.png"
+        imgUrl={{ uri: 'https://zos.alipayobjects.com/rmsportal/qlMpMyJTcSjMpKAgtcEt.png' }}
         title="验证成功"
         message="所提交内容已成功完成验证"
       />,
