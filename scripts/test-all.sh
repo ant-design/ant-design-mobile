5e53f76030612ef50712fcf8b0bf788fefdec489
silentcloud <rjmuqiang@gmail.com>
2017-07-05 15:38:14 +0800

    chore: fix test:all test with no-cache, close #1529

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -2,11 +2,10 @@
 
 npm run lint && \
 npm run dist && \
-LIB_DIR=dist npm run test:web && \
+LIB_DIR=dist npm run test:web -- --no-cache && \
 npm run compile && \
-LIB_DIR=lib npm run test:web && \
-LIB_DIR=lib npm run test:rn && \
-LIB_DIR=es npm run test:web && \
-LIB_DIR=es npm run test:rn && \
-npm run test:web && \
-npm run test:rn
+LIB_DIR=lib npm run test:web -- --no-cache && \
+LIB_DIR=lib npm run test:rn -- --no-cache && \
+LIB_DIR=es npm run test:web -- --no-cache && \
+LIB_DIR=es npm run test:rn -- --no-cache && \
+npm run test
