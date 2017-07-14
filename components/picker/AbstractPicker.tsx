6c722c80a942891df95568101a475dbc7e2563a8
colder <colder@vitarn.com>
2017-07-14 15:48:26 +0800

    refactor picker add web suffix
    split picker into two rn/web files
    
    extends abstract class with different props or styles

---
+++ components/picker/AbstractPicker.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import PopupCascader from 'rmc-cascader/lib/Popup';
+import Cascader from 'rmc-cascader/lib/Cascader';
+import MultiPicker from 'rmc-picker/lib/MultiPicker';
+import treeFilter from 'array-tree-filter';
+import tsPropsType from './PropsType';
+
+export function getDefaultProps() {
+  const defaultFormat = (values) => {
+    return values.join(',');
+  };
+  return {
+    triggerType: 'onClick',
+    prefixCls: 'am-picker',
+    pickerPrefixCls: 'am-picker-col',
+    popupPrefixCls: 'am-picker-popup',
+    format: defaultFormat,
+    cols: 3,
+    cascade: true,
+    extra: '请选择',
+    okText: '确定',
+    dismissText: '取消',
+    title: '',
+  };
+}
+
+export default abstract class AbstractPicker extends React.Component<tsPropsType, any> {
+  protected abstract popupProps: {};
+
+  getSel = () => {
+    const value = this.props.value || [];
+    let treeChildren;
+    if (this.props.cascade) {
+      treeChildren = treeFilter(this.props.data, (c, level) => {
+        return c.value === value[level];
+      });
+    } else {
+      treeChildren = value.map((v, i) => {
+        return this.props.data[i].filter(d => d.value === v)[0];
+      });
+    }
+    return this.props.format && this.props.format(treeChildren.map((v) => {
+        return v.label;
+      }));
+  }
+
+  render() {
+    const { props } = this;
+    const { children, value = [], extra, okText, itemStyle, dismissText, popupPrefixCls, cascade } = props;
+    let cascader;
+    let popupMoreProps = {};
+    if (cascade) {
+      cascader = (
+        <Cascader
+          prefixCls={props.prefixCls}
+          pickerPrefixCls={props.pickerPrefixCls}
+          data={props.data}
+          cols={props.cols}
+          onChange={props.onPickerChange}
+          pickerItemStyle={itemStyle}
+        />
+      );
+    } else {
+      cascader = (
+        <MultiPicker
+          prefixCls={props.prefixCls}
+          pickerPrefixCls={props.pickerPrefixCls}
+          pickerItemStyle={itemStyle}
+        >
+          {props.data.map(d => { return { props: { children: d } }; })}
+        </MultiPicker>
+      );
+      popupMoreProps = {
+        pickerValueProp: 'selectedValue',
+        pickerValueChangeProp: 'onValueChange',
+      };
+    }
+    return (
+      <PopupCascader
+        cascader={cascader}
+        {...this.popupProps}
+        {...props}
+        prefixCls={popupPrefixCls}
+        value={value}
+        dismissText={dismissText}
+        okText={okText}
+        {...popupMoreProps}
+      >
+        {React.cloneElement(children, { extra: this.getSel() || extra })}
+      </PopupCascader>
+    );
+  }
+}
