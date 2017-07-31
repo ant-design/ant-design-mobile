9405490c639f31822fde1831d159222c2f9ed0e4
paranoidjk <hust2012jiangkai@gmail.com>
2017-07-31 17:32:51 +0800

    feat: change .tsx to .native.tsx

--- components/pagination/style/index.tsx
+++
@@ -1,4 +0,0 @@
-import '../../style/';
-import '../../button/style/';
-import '../../flex/style/';
-import './index.less';
---
+++ components/warn.native.tsx
@@ -0,0 +1,5 @@
+if (typeof console !== 'undefined' && console.error) {
+  console.error(`Note: must use https://github.com/ant-design/babel-plugin-import .
+For more information, please see https://github.com/ant-design/ant-design-mobile/issues/602 `);
+}
+export default {};
