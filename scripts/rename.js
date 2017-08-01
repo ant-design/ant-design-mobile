46caece96b3132e98ef6d85fc2381d1e9a3887d0
paranoidjk <hust2012jiangkai@gmail.com>
2017-08-01 10:14:29 +0800

    feat: add rename scripts

---
+++ scripts/rename.js
@@ -0,0 +1,39 @@
+var path = require("path");
+var fs = require("fs");
+
+var rootPath = '/Users/jiangkai/github/ant-design-mobile/components';
+renameFilesInDir(rootPath);
+
+function changeFileName(filepath){
+    fs.stat(filepath, function(error, stats){
+        if (error) {
+          cosole.error(error);
+          return;
+        }
+        if(stats.isFile()) {
+            console.log(filepath);
+            if (/\.jsx?$/.test(filepath)) {
+              fs.unlinkSync(filepath);
+            }
+        } else if(stats.isDirectory()) {
+            renameFilesInDir(filepath);
+        }
+    });
+}
+
+function renameFilesInDir(dir) {
+    fs.readdir(dir,function(error,files){
+        if (error) {
+          console.error(error);
+          return;
+        }
+        var len = files.length;
+        var file = null;
+        for(var i=0; i<len; i++){
+            file = files[i];
+            if (!/^(\.|_)/.test(file)) {
+              changeFileName(path.join(dir, file));
+            }
+        }
+    });
+}
