6c82544d9b6201de4c8ff02950d3eb592fa23995
silentcloud <rjmuqiang@gmail.com>
2017-08-10 11:12:04 +0800

    fix: rn swipe-action, close #1655

--- components/swipe-action/index.tsx
+++ components/swipe-action/index.tsx
@@ -3,15 +3,6 @@ import Swipeout from 'rc-swipeout/lib/Swipeout';
 import SwipeActionProps from './PropsType';
 
 class SwipeAction extends React.Component<SwipeActionProps, any> {
-  static defaultProps = {
-    autoClose: false,
-    disabled: false,
-    left: [],
-    right: [],
-    onOpen() {},
-    onClose() {},
-  };
-
   render() {
     return (
       <Swipeout {...this.props} />
