f44e1b07d8b0fab70c24f3b6d41f2e7f0b85cf92
ustccjw <317713370@qq.com>
2017-08-18 12:04:05 +0800

    add data-scale in kitchen

---
+++ replace.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="jiawei.cjw@alibaba-inc.com"
+CORRECT_NAME="ustccjw"
+CORRECT_EMAIL="317713370@qq.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
