0db95ff66a14f9f674e2a9d896b3cb85b41309ec
SuperEVO <zhang740@vmzclub.com>
2017-08-25 16:10:30 +0800

    add calendar component. #1610

--- components/index.tsx
+++ components/index.tsx
@@ -1,10 +1,10 @@
 // this file is not used if use https://github.com/ant-design/babel-plugin-import
 const ENV = process.env.NODE_ENV;
 if (ENV !== 'production' &&
-    ENV !== 'test' &&
-    typeof console !== 'undefined' &&
-    console.warn &&
-    typeof window !== 'undefined') {
+  ENV !== 'test' &&
+  typeof console !== 'undefined' &&
+  console.warn &&
+  typeof window !== 'undefined') {
   console.warn(
     'You are using a whole package of antd-mobile, ' +
     'please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.',
@@ -16,6 +16,7 @@ export { default as ActionSheet } from './action-sheet/index';
 export { default as ActivityIndicator } from './activity-indicator/index';
 export { default as Badge } from './badge/index';
 export { default as Button } from './button/index';
+export { default as Calendar } from './calendar/index';
 export { default as Card } from './card/index';
 export { default as Carousel } from './carousel/index';
 export { default as Checkbox } from './checkbox/index';
