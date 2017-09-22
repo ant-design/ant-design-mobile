901d356bc72cba88d70b3ea380daf1e9716e0c66
然则 <hualei.hl@alibaba-inc.com>
2017-09-22 10:41:06 +0800

    chore: improve index.js comment

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// Just import style for https://github.com/ant-design/ant-design/issues/3745
-const req = require.context('./components', true, /^\.\/[^_][\w-]+\/style\/index\.tsx?$/);
+/**
+ * This is the entry file for `npm run dist`
+ */
 
-// build for `dist/antd-mobile.css`
+/**
+ * build for `dist/antd-mobile.css`
+ * Note: Just import style. The pattern is different in `v0.9`. The difference is `(style\/)?`
+ */
+const req = require.context('./components', true, /^\.\/[^_][\w-]+\/style\/index\.tsx?$/);
 req.keys().forEach((mod) => {
   req(mod);
 });
 
-// build for `dist/antd-mobile.js`
-module.exports = require('./components/index');
+/**
+ * build for `dist/antd-mobile.js`
+ */
+// module.exports = require('./components/index');
