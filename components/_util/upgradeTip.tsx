cb9cb4d10c6bf422c3b2256cb454639e051b1cd5
zhang740 <zhang740@vmzclub.com>
2017-09-25 13:35:19 +0800

    chore: fix tests when dist.

--- components/_util/upgradeTip.tsx
+++ components/_util/upgradeTip.tsx
@@ -1,56 +1,60 @@
 if (process.env.NODE_ENV === 'development') {
-  const localVersion = require('./version.json');
-  if (!localVersion._disable && fetch) {
-    const server = 'http://alipay-rmsdeploy-image.cn-hangzhou.alipay.aliyun-inc.com';
-    fetch(`${server}/filesync/prod/z/fengdie/antd-mobile-upgrade-tip/upgrade-tip-h5data.json`)
-      .then(data => data.json())
-      .then((data) => {
-        let notice = '';
-        const tipComponents = data.filter(item => {
-          const key = item.name;
-          if (key === '[notice]') {
-            notice = item.tip;
-          }
-          if (!localVersion[key]) {
-            return false;
-          }
-          const oldVer = localVersion[key].split('.') as string[];
-          const newVer = item.version.split('.') as string[];
-          let result = false;
-          const length = Math.max(oldVer.length, newVer.length);
-          for (let i = 0; i < length; i++) {
-            if (oldVer[i] !== newVer[i]) {
-              result = result || oldVer[i] === undefined || oldVer[i] < newVer[i];
+  try {
+    const localVersion = require('./version.json');
+    if (!localVersion._disable && fetch) {
+      const server = 'http://alipay-rmsdeploy-image.cn-hangzhou.alipay.aliyun-inc.com';
+      fetch(`${server}/filesync/prod/z/fengdie/antd-mobile-upgrade-tip/upgrade-tip-h5data.json`)
+        .then(data => data.json())
+        .then((data) => {
+          let notice = '';
+          const tipComponents = data.filter(item => {
+            const key = item.name;
+            if (key === '[notice]') {
+              notice = item.tip;
             }
+            if (!localVersion[key]) {
+              return false;
+            }
+            const oldVer = localVersion[key].split('.') as string[];
+            const newVer = item.version.split('.') as string[];
+            let result = false;
+            const length = Math.max(oldVer.length, newVer.length);
+            for (let i = 0; i < length; i++) {
+              if (oldVer[i] !== newVer[i]) {
+                result = result || oldVer[i] === undefined || oldVer[i] < newVer[i];
+              }
+            }
+            return result;
+          });
+
+          if (notice) {
+            console.warn(
+              '--------------------------------------------------------\n',
+              '[antd-mobile-upgrade-tip] Notice:\n',
+              `${notice}\n`,
+              '--------------------------------------------------------',
+            );
           }
-          return result;
-        });
 
-        if (notice) {
-          console.warn(
-            '--------------------------------------------------------\n',
-            '[antd-mobile-upgrade-tip] Notice:\n',
-            `${notice}\n`,
-            '--------------------------------------------------------',
-          );
-        }
+          if (tipComponents.length > 0) {
+            console.warn(
+              '--------------------------------------------------------\n',
+              '[antd-mobile-upgrade-tip] some components is expired:\n',
+              '\n',
+              ...tipComponents
+                .map(newData => {
+                  const key = newData.name;
+                  return `${key}: ${localVersion[key]} => ${newData.version} ${newData.tip} ${newData.url}\n`;
+                }),
+              '\n',
+              '[you can reinstall node_modules to upgrade all of them.]\n',
+              '[about this] http://beta.mobile.ant.design/docs/react/upgrade-tip-cn\n',
+              '--------------------------------------------------------',
+            );
+          }
+        }).catch(() => { });
+    }
+  } catch (error) {
 
-        if (tipComponents.length > 0) {
-          console.warn(
-            '--------------------------------------------------------\n',
-            '[antd-mobile-upgrade-tip] some components is expired:\n',
-            '\n',
-            ...tipComponents
-              .map(newData => {
-                const key = newData.name;
-                return `${key}: ${localVersion[key]} => ${newData.version} ${newData.tip} ${newData.url}\n`;
-              }),
-            '\n',
-            '[you can reinstall node_modules to upgrade all of them.]\n',
-            '[about this] http://beta.mobile.ant.design/docs/react/upgrade-tip-cn\n',
-            '--------------------------------------------------------',
-          );
-        }
-      }).catch(() => { });
   }
 }
