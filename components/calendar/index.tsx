3e43729b44fae513f3e38a30ee27bb9b417f5134
zhang740 <zhang740@vmzclub.com>
2017-09-26 12:16:48 +0800

    update calendar header.

--- components/calendar/index.tsx
+++ components/calendar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icon from '../icon';
 import PropTypes from 'prop-types';
 import tsPropsType from './PropsType';
 import { Calendar as RMCalendar } from 'rmc-calendar';
@@ -18,10 +19,12 @@ export default class Calendar extends React.Component<tsPropsType, any> {
   render() {
     const { props, context } = this;
     const locale = getComponentLocale(props, context, 'Calendar', () => require('./locale/zh_CN'));
+    const Header = RMCalendar.DefaultHeader;
 
     return (
       <RMCalendar
         locale={locale}
+        renderHeader={headerProps => <Header {...headerProps} closeIcon={<Icon type="cross"/>} />}
         {...props}
       />
     );
