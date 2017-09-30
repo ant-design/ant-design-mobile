ec7015c84675fbc9979e60d617820eca421e30e9
zhang740 <zhang740@vmzclub.com>
2017-09-30 12:25:19 +0800

    chore: update script for upgrade-tip.

---
+++ scripts/cdn-config.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+
+const baseDir = './_site';
+const filelist = [];
+let dir = `${baseDir}`;
+fs.readdirSync(dir)
+  .filter(file => path.parse(file).ext === '.html')
+  .forEach((doc) => {
+    filelist.push(`${dir}/${doc}`);
+  });
+
+dir = `${baseDir}/docs/pattern`;
+fs.readdirSync(dir)
+  .filter(file => path.parse(file).ext === '.html')
+  .forEach((doc) => {
+    filelist.push(`${dir}/${doc}`);
+  });
+
+dir = `${baseDir}/docs/react`;
+fs.readdirSync(dir)
+  .filter(file => path.parse(file).ext === '.html')
+  .forEach((doc) => {
+    filelist.push(`${dir}/${doc}`);
+  });
+
+module.exports = {
+  secret: 'mXvdTx0LJjKDCb2Wqsq5jKpiMEvk2SL1ocVESHCfn5iHb4RWOB+34ihM2e+Rc6SZssxX1vQ041Q2OIg+j6uy7/mrTp/TvDOqORHzLGtLnSA=',
+  fileList: [
+    {
+      template: filelist,
+      data: [
+        {
+          regex: /"\/index.css"/g,
+          file: `${baseDir}/index.css`,
+          template: '"{url}"',
+        },
+        {
+          regex: /"\/index.js"/g,
+          file: `${baseDir}/index.js`,
+          template: '"{url}"',
+        },
+        {
+          regex: /"\/common.js"/g,
+          file: `${baseDir}/common.js`,
+          template: '"{url}"',
+        },
+      ],
+      options: {
+        mode: 'public',
+      },
+    },
+    {
+      template: [
+        `${baseDir}/kitchen-sink/index.html`,
+        `${baseDir}/kitchen-sink/404.html`,
+      ],
+      data: [
+        {
+          regex: '/kitchen-sink/kitchen-sink.css',
+          file: `${baseDir}/kitchen-sink/kitchen-sink.css`,
+        },
+        {
+          regex: '/kitchen-sink/common.js',
+          file: `${baseDir}/kitchen-sink/common.js`,
+        },
+        {
+          regex: '/kitchen-sink/kitchen-sink.js',
+          file: `${baseDir}/kitchen-sink/kitchen-sink.js`,
+        },
+      ],
+      options: {
+        mode: 'public',
+      },
+    },
+  ],
+};
--- scripts/update-tip.js
+++ scripts/update-tip.js
@@ -1,28 +1,28 @@
 /* eslint-disable */
-const fs = require('fs');
-const debug = (...args) => {
-  // console.log(...args);
+var fs = require('fs');
+function debug() {
+  // console.log.apply(this, arguments);
 }
-const projectRoot = getProjectRoot();
-const nodeModulesRoot = `${projectRoot}/node_modules`;
-const antdMobileRoot = `${nodeModulesRoot}/antd-mobile`;
+var projectRoot = getProjectRoot();
+var nodeModulesRoot = projectRoot + '/node_modules';
+var antdMobileRoot = nodeModulesRoot + '/antd-mobile';
 
 debug('\n------------------------------------');
-debug(`[antd-mobile-update-tip] projectRoot: ${projectRoot}`);
-debug(`[antd-mobile-update-tip] nodeModulesRoot: ${nodeModulesRoot}`);
-debug(`[antd-mobile-update-tip] antdMobileRoot: ${antdMobileRoot}`);
+debug('[antd-mobile-update-tip] projectRoot: ' + projectRoot);
+debug('[antd-mobile-update-tip] nodeModulesRoot: ' + nodeModulesRoot);
+debug('[antd-mobile-update-tip] antdMobileRoot: ' + antdMobileRoot);
 
-if (!fs.existsSync(`${projectRoot}/package.json`) || !fs.existsSync(`${antdMobileRoot}/package.json`)) {
+if (!fs.existsSync(projectRoot + '/package.json') || !fs.existsSync(antdMobileRoot + '/package.json')) {
   debug('[antd-mobile-update-tip] Not found package.json.');
   debug('------------------------------------');
   return;
 }
 
-const projectInfo = require(`${projectRoot}/package.json`);
+var projectInfo = require(projectRoot + '/package.json');
 /** version.json 保存目录 */
-const targetDir = `${antdMobileRoot}/lib/_util`;
+var targetDir = antdMobileRoot + '/lib/_util';
 /** version.json 路径 */
-const targetFile = `${targetDir}/version.json`;
+var targetFile = targetDir + '/version.json';
 
 if (projectInfo['antd-mobile'] && projectInfo['antd-mobile']['upgrade-tip'] === false) {
   debug('[antd-mobile-update-tip] upgrade-tip disabled.');
@@ -33,23 +33,23 @@ if (projectInfo['antd-mobile'] && projectInfo['antd-mobile']['upgrade-tip'] === 
   return;
 }
 
-const versionList = {};
-const antdMobileInfo = require(`${antdMobileRoot}/package.json`);
+var versionList = {};
+var antdMobileInfo = require(antdMobileRoot + '/package.json');
 versionList[antdMobileInfo.name] = antdMobileInfo.version;
 
-debug(`\nantd-mobile: ${antdMobileInfo.version}\n`, 'dependencies:');
+debug('\nantd-mobile: ' + antdMobileInfo.version + '\n', 'dependencies:');
 
-Object.keys(antdMobileInfo.dependencies)
-  .filter(depName => depName.indexOf('rc-') === 0 || depName.indexOf('rmc-') === 0)
-  .forEach(depName => {
-    let version = '';
-    packagePath = `${nodeModulesRoot}/${depName}/package.json`;
-    if (fs.existsSync(packagePath)) {
-      version = require(packagePath).version;
-    }
-    debug(`    ${depName}\t: ${version}`);
-    versionList[depName] = version;
-  });
+Object.keys(antdMobileInfo.dependencies).filter(function (depName) {
+  return depName.indexOf('rc-') === 0 || depName.indexOf('rmc-') === 0;
+}).forEach(function (depName) {
+  var version = '';
+  packagePath = nodeModulesRoot + '/' + depName + '/package.json';
+  if (fs.existsSync(packagePath)) {
+    version = require(packagePath).version;
+  }
+  debug('    ' + depName + '\t: ' + version);
+  versionList[depName] = version;
+});
 
 if (fs.existsSync(targetDir)) {
   fs.writeFileSync(targetFile, JSON.stringify(versionList));
@@ -60,8 +60,8 @@ if (fs.existsSync(targetDir)) {
 debug('------------------------------------\n');
 
 function getProjectRoot() {
-  const cwd = process.cwd();
-  const index = cwd.lastIndexOf('/node_modules/');
+  var cwd = process.cwd();
+  var index = cwd.lastIndexOf('/node_modules/');
   if (index >= 0) {
     return cwd.slice(0, index);
   } else {
