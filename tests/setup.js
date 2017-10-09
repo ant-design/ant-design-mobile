3cab410d6e1a0ca275d558e7f8fc13dbbe43f4fe
silentcloud <rjmuqiang@gmail.com>
2017-10-09 16:34:32 +0800

    fix createPortal mock

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,7 @@
 const Enzyme = require('enzyme');
+const ReactDOM = require('react-dom');
 const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
+
+ReactDOM.createPortal = jest.fn().mockReturnValue(null);
