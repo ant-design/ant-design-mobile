5e2ecccff6d98a27738fe5df623fc21d5a009279
zhang740 <zhang740@vmzclub.com>
2017-10-10 16:37:01 +0800

    chore: update cdn config.

--- scripts/cdn-config.js
+++ scripts/cdn-config.js
@@ -1,30 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
-const baseDir = './_site';
+const projectDir = path.join(__dirname, '../');
+
+const baseDir = path.join(projectDir, '_site');
 const filelist = [];
+
 let dir = `${baseDir}`;
 fs.readdirSync(dir)
   .filter(file => path.parse(file).ext === '.html')
   .forEach((doc) => {
-    filelist.push(`${dir}/${doc}`);
+    filelist.push(path.join(dir, doc));
   });
 
 dir = `${baseDir}/docs/pattern`;
 fs.readdirSync(dir)
   .filter(file => path.parse(file).ext === '.html')
   .forEach((doc) => {
-    filelist.push(`${dir}/${doc}`);
+    filelist.push(path.join(dir, doc));
   });
 
 dir = `${baseDir}/docs/react`;
 fs.readdirSync(dir)
   .filter(file => path.parse(file).ext === '.html')
   .forEach((doc) => {
-    filelist.push(`${dir}/${doc}`);
+    filelist.push(path.join(dir, doc));
   });
 
 module.exports = {
+  cwd: projectDir,
   secret: 'mXvdTx0LJjKDCb2Wqsq5jKpiMEvk2SL1ocVESHCfn5iHb4RWOB+34ihM2e+Rc6SZssxX1vQ041Q2OIg+j6uy7/mrTp/TvDOqORHzLGtLnSA=',
   fileList: [
     {
