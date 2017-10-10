4ff52e29c56760ce0dc1c8e21fbca72f26fde1b4
silentcloud <rjmuqiang@gmail.com>
2017-10-10 18:22:08 +0800

    fix: popover arrow background color for 1.x, ref #1643

--- components/popover/index.web.tsx
+++ components/popover/index.web.tsx
@@ -34,6 +34,11 @@ export default class Popover extends React.Component<tsPropsType, any> {
       }
       return child;
     });
-    return <Tooltip {...restProps} overlay={overlayNode} />;
+    const wrapperNode = (
+      <div className={`${this.props.prefixCls}-inner-wrapper`}>
+        {overlayNode}
+      </div>
+    );
+    return <Tooltip {...restProps} overlay={wrapperNode} />;
   }
 }
