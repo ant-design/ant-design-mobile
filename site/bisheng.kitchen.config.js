f8f7f4c4fe1d70fef537bf6b7525639285c1d122
然则 <hualei.hl@alibaba-inc.com>
2017-10-17 13:00:54 +0800

    add start-hd script for site

--- site/bisheng.kitchen.config.js
+++ site/bisheng.kitchen.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const pxtorem = require('postcss-pxtorem');
 const commonConfig = require('./bisheng.common.config');
 
-module.exports = Object.assign({}, commonConfig, {
+const kitchenConfig = {
   port: 8002,
   source: {
     components: './components',
@@ -15,4 +16,18 @@ module.exports = Object.assign({}, commonConfig, {
     verbose: true,
     plugins: ['dora-plugin-upload'],
   },
-});
+};
+
+if (process.env.HD_ENV === 'hd') {
+  kitchenConfig.webpackConfig = function (config) {
+    config = commonConfig.webpackConfig(config);
+    config.postcss.push(pxtorem({
+      rootValue: 100,
+      propWhiteList: [],
+      // selectorBlackList: [/^html$/, /^\.ant-/, /^\.github-/, /^\.gh-/], // does't exist these class now.
+    }));
+    return config;
+  };
+}
+
+module.exports = Object.assign({}, commonConfig, kitchenConfig);
