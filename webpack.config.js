3916f5bafb1338ce6eae850542f8c2374c0bc24b
Wei Zhu <yesmeck@gmail.com>
2017-10-19 05:54:52 -0500

    Upgrade to antd-tools@4.0.0 (#1960)

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,14 @@ const getWebpackConfig = require('antd-tools/lib/getWebpackConfig');
 const Visualizer = require('webpack-visualizer-plugin');
 const pkg = require('./package.json');
 
-module.exports = function (webpackConfig) {
-  // fix `npm run dist` sourceMap error, do not know the reason
-  delete webpackConfig.ts.compilerOptions.sourceMap;
+const webpackConfig = getWebpackConfig(false);
 
-  webpackConfig = getWebpackConfig(webpackConfig, true);
-  if (!Array.isArray(webpackConfig)) {
-    webpackConfig = [webpackConfig, webpackConfig];
+webpackConfig.forEach((config, index) => {
+  if (index === 0) {
+    config.plugins.push(new Visualizer({
+      filename: `../ant-design-analysis/${pkg.name}@${pkg.version}-stats.html`,
+    }));
   }
-  webpackConfig.forEach((config, index) => {
-    if (index === 0) {
-      config.plugins.push(new Visualizer({
-        filename: `../ant-design-analysis/${pkg.name}@${pkg.version}-stats.html`,
-      }));
-    }
-  });
+});
 
-  return webpackConfig;
-};
+module.exports = webpackConfig;
