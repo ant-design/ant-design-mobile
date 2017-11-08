1bf36291ba4781ad8dbdb658097c736453315d46
zhang740 <zhang740@vmzclub.com>
2017-11-08 12:32:36 +0800

    chore: code format.

--- components/tab-bar/index.tsx
+++ components/tab-bar/index.tsx
@@ -5,92 +5,92 @@ import getDataAttr from '../_util/getDataAttr';
 import { TabBarProps, TabBarItemProps } from './PropsType';
 
 export class Item extends React.Component<TabBarItemProps, any> {
-    static defaultProps = {
-        prefixCls: 'am-tab-bar-item',
-    } as TabBarItemProps;
+  static defaultProps = {
+    prefixCls: 'am-tab-bar-item',
+  } as TabBarItemProps;
 
-    render() {
-        const { prefixCls, style } = this.props;
+  render() {
+    const { prefixCls, style } = this.props;
 
-        return <div className={prefixCls} style={style}>
-            {this.props.children}
-        </div>;
-    }
+    return <div className={prefixCls} style={style}>
+      {this.props.children}
+    </div>;
+  }
 }
 
 class AntTabBar extends React.Component<TabBarProps, any> {
-    static defaultProps = {
-        prefixCls: 'am-tab-bar',
-        barTintColor: 'white',
-        tintColor: '#108ee9',
-        hidden: false,
-        unselectedTintColor: '#888',
-        placeholder: '正在加载',
-        animated: false,
-        swipeable: false,
-    } as TabBarProps;
+  static defaultProps = {
+    prefixCls: 'am-tab-bar',
+    barTintColor: 'white',
+    tintColor: '#108ee9',
+    hidden: false,
+    unselectedTintColor: '#888',
+    placeholder: '正在加载',
+    animated: false,
+    swipeable: false,
+  } as TabBarProps;
 
-    public static Item = Item;
+  public static Item = Item;
 
-    getTabs = () => {
-        return React.Children.map(this.props.children, (c: any) => {
-            return {
-                ...c.props as TabBarItemProps,
-            };
-        });
-    }
+  getTabs = () => {
+    return React.Children.map(this.props.children, (c: any) => {
+      return {
+        ...c.props as TabBarItemProps,
+      };
+    });
+  }
 
-    renderTabBar = () => {
-        const { barTintColor, prefixCls, tintColor, unselectedTintColor } = this.props;
-        const tabsData = this.getTabs();
+  renderTabBar = () => {
+    const { barTintColor, prefixCls, tintColor, unselectedTintColor } = this.props;
+    const tabsData = this.getTabs();
 
-        const content = tabsData.map((cProps, index) => {
-            return <Tab
-                key={index}
-                prefixCls={`${this.props.prefixCls}-tab`}
-                badge={cProps.badge}
-                dot={cProps.dot}
-                selected={cProps.selected}
-                icon={cProps.icon}
-                selectedIcon={cProps.selectedIcon}
-                title={cProps.title}
-                tintColor={tintColor}
-                unselectedTintColor={unselectedTintColor}
-                dataAttrs={getDataAttr(cProps)}
-                onClick={() => cProps.onPress && cProps.onPress()}
-            />;
-        });
-        return <div className={`${prefixCls}-bar`} style={{ backgroundColor: barTintColor }}>
-            {content}
-        </div>;
-    }
+    const content = tabsData.map((cProps, index) => {
+      return <Tab
+        key={index}
+        prefixCls={`${this.props.prefixCls}-tab`}
+        badge={cProps.badge}
+        dot={cProps.dot}
+        selected={cProps.selected}
+        icon={cProps.icon}
+        selectedIcon={cProps.selectedIcon}
+        title={cProps.title}
+        tintColor={tintColor}
+        unselectedTintColor={unselectedTintColor}
+        dataAttrs={getDataAttr(cProps)}
+        onClick={() => cProps.onPress && cProps.onPress()}
+      />;
+    });
+    return <div className={`${prefixCls}-bar`} style={{ backgroundColor: barTintColor }}>
+      {content}
+    </div>;
+  }
 
-    render() {
-        const { prefixCls, children, hidden, animated, swipeable, noRenderContent } = this.props;
-        const tabs = this.getTabs();
-        let activeIndex = 0;
-        tabs.forEach((tab, index) => {
-            if (tab.selected) {
-                activeIndex = index;
-            }
-        });
+  render() {
+    const { prefixCls, children, hidden, animated, swipeable, noRenderContent } = this.props;
+    const tabs = this.getTabs();
+    let activeIndex = 0;
+    tabs.forEach((tab, index) => {
+      if (tab.selected) {
+        activeIndex = index;
+      }
+    });
 
-        return (
-            <div className={prefixCls}>
-                <Tabs
-                    tabs={tabs}
-                    renderTabBar={hidden ? false : this.renderTabBar}
-                    tabBarPosition="bottom"
-                    page={activeIndex < 0 ? undefined : activeIndex}
-                    animated={animated}
-                    swipeable={swipeable}
-                    noRenderContent={noRenderContent}
-                >
-                    {children}
-                </Tabs>
-            </div>
-        );
-    }
+    return (
+      <div className={prefixCls}>
+        <Tabs
+          tabs={tabs}
+          renderTabBar={hidden ? false : this.renderTabBar}
+          tabBarPosition="bottom"
+          page={activeIndex < 0 ? undefined : activeIndex}
+          animated={animated}
+          swipeable={swipeable}
+          noRenderContent={noRenderContent}
+        >
+          {children}
+        </Tabs>
+      </div>
+    );
+  }
 }
 
 export default AntTabBar;
