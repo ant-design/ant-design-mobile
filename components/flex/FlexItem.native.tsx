29b52deea6cc001d39d816d7acc04f6c2cdeb618
Dog <>
2017-11-19 13:33:33 +0800

    Optional use TouchableWithoutFeedback component in Flex/FlexItem component

--- components/flex/FlexItem.native.tsx
+++ components/flex/FlexItem.native.tsx
@@ -10,21 +10,42 @@ export interface FlexItemProps extends BasePropsType {
   onPressOut?: any;
 }
 
-export default class FlexItem extends React.Component<FlexItemProps, any> {
+export default class FlexItem extends React.Component < FlexItemProps, any > {
   static defaultProps = {
     flex: 1,
   };
   render() {
-    let { style, children, flex, ...restProps } = this.props;
+    let {
+      style,
+      children,
+      flex,
+      ...restProps,
+    } = this.props;
     const flexItemStyle = {
       flex: flex || 1,
     };
     // support other touchablewithoutfeedback props
     // TODO  support TouchableHighlight
-    return (
-      <View style={[flexItemStyle, style]} {...restProps }>
+    const inner = (
+      <View style={[flexItemStyle, style]} {...restProps}>
         {children}
       </View>
     );
+
+    const shouldWrapInTouchableComponent =
+      restProps.onPress
+      || restProps.onLongPress
+      || restProps.onPressIn
+      || restProps.onPressOut;
+
+    if (!!shouldWrapInTouchableComponent) {
+      return (
+        <TouchableWithoutFeedback {...restProps}>
+          {inner}
+        </TouchableWithoutFeedback>
+      );
+    } else {
+      return inner;
+    }
   }
 }
