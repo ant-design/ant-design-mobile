9e9789e8c48cdce621dc79797b54be434e38c87e
Andreas Cederström <andriijas@users.noreply.github.com>
2017-12-01 04:17:16 +0100

    add swedish locale 🇸🇪 (#2132)

---
+++ components/locale-provider/sv_SE.tsx
@@ -0,0 +1,16 @@
+import Pagination from '../pagination/locale/sv_Se';
+import DatePicker from '../date-picker/locale/sv_Se';
+import DatePickerView from '../date-picker-view/locale/sv_Se';
+import InputItem from '../input-item/locale/sv_Se';
+import Picker from '../picker/locale/sv_Se';
+import SearchBar from '../search-bar/locale/sv_Se';
+
+export default {
+  locale: 'sv',
+  Pagination,
+  DatePicker,
+  DatePickerView,
+  InputItem,
+  Picker,
+  SearchBar,
+};
---
+++ components/pagination/locale/sv_SE.tsx
@@ -0,0 +1,4 @@
+export default {
+  prevText: 'Föreg',
+  nextText: 'Nästa',
+};
