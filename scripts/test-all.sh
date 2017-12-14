905ea2687c2a76dbf7f6fba782cc24e71b10a13d
paranoidjk <hust2012jiangkai@gmail.com>
2017-12-14 13:56:06 +0800

    chore: disable rn test

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -7,7 +7,7 @@ npm run dist && \
 LIB_DIR=dist npm run test:web -- --no-cache && \
 npm run compile && \
 LIB_DIR=lib npm run test:web -- --no-cache && \
-LIB_DIR=lib npm run test:rn -- --no-cache && \
+# LIB_DIR=lib npm run test:rn -- --no-cache && \
 LIB_DIR=es npm run test:web -- --no-cache && \
-LIB_DIR=es npm run test:rn -- --no-cache && \
+# LIB_DIR=es npm run test:rn -- --no-cache && \
 npm run test
