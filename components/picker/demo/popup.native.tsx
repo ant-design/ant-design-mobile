1ca24875551ed14420d2d851305f657049c043d6
bang <sqibang@gmail.com>
2018-02-06 12:13:50 +0800

    refactor(picker): type definitions

--- components/picker/demo/popup.native.tsx
+++ components/picker/demo/popup.native.tsx
@@ -1,6 +1,6 @@
-import { View } from 'react-native';
-import { Picker, List } from 'antd-mobile';
+import { List, Picker } from 'antd-mobile';
 import React from 'react';
+import { View } from 'react-native';
 
 import { district } from 'antd-mobile-demo-data';
 
@@ -19,23 +19,27 @@ export default class PopupExample extends React.Component<any, any> {
         data: district,
       });
     }, 500);
-  }
-  onChange = (value) => {
+  };
+  onChange = value => {
     // console.log(value);
     this.setState({ value });
-  }
+  };
   render() {
-    return (<View style={{ marginTop: 30 }}>
-      <List>
-        <Picker
-          data={this.state.data}
-          cols={2}
-          value={this.state.value}
-          onChange={this.onChange}
-        >
-          <List.Item arrow="horizontal" last onClick={this.onClick}>省市选择(异步加载)</List.Item>
-        </Picker>
-      </List>
-    </View>);
+    return (
+      <View style={{ marginTop: 30 }}>
+        <List>
+          <Picker
+            data={this.state.data}
+            cols={2}
+            value={this.state.value}
+            onChange={this.onChange}
+          >
+            <List.Item arrow="horizontal" last onClick={this.onClick}>
+              省市选择(异步加载)
+            </List.Item>
+          </Picker>
+        </List>
+      </View>
+    );
   }
 }
