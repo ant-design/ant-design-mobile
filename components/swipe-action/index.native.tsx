640e23787a42c5eceb5d0ca630ebacff27b9e867
bang <sqibang@gmail.com>
2018-02-06 17:44:33 +0800

    refactor(swipe-action): type definitions

--- components/swipe-action/index.native.tsx
+++ components/swipe-action/index.native.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import Swipeout from 'rc-swipeout/lib/Swipeout';
-import BasePropsType from './PropsType';
+import React from 'react';
+import { StyleProp, TextStyle, ViewStyle } from 'react-native';
+import { SwipeActionPropsType } from './PropsType';
 
-export interface SwipeActionProps extends BasePropsType {
+export interface SwipeActionProps extends SwipeActionPropsType<TextStyle> {
   styles?: any;
+  style?: StyleProp<ViewStyle>;
 }
 
 class SwipeAction extends React.Component<SwipeActionProps, any> {
   render() {
-    return (
-      <Swipeout {...this.props} />
-    );
+    return <Swipeout {...this.props} />;
   }
 }
 
