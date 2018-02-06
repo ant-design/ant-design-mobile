0f9c573feef624dabc8e66f11623d2b945d6a4f6
bang <sqibang@gmail.com>
2018-02-06 10:08:47 +0800

    refactor(nav-bar): type definitions

--- components/nav-bar/index.tsx
+++ components/nav-bar/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import classnames from 'classnames';
-import NavBarProps from './PropsType';
+import React from 'react';
+import { NavBarProps } from './PropsType';
 
 export default class NavBar extends React.Component<NavBarProps, any> {
   static defaultProps = {
@@ -11,14 +11,32 @@ export default class NavBar extends React.Component<NavBarProps, any> {
 
   render() {
     const {
-      prefixCls, className, children, mode, icon, onLeftClick, leftContent, rightContent,
-      ...restProps,
+      prefixCls,
+      className,
+      children,
+      mode,
+      icon,
+      onLeftClick,
+      leftContent,
+      rightContent,
+      ...restProps
     } = this.props;
 
     return (
-      <div {...restProps} className={classnames(className, prefixCls, `${prefixCls}-${mode}`)}>
-        <div className={`${prefixCls}-left`} role="button" onClick={onLeftClick}>
-          {icon ? <span className={`${prefixCls}-left-icon`} aria-hidden="true">{icon}</span> : null}
+      <div
+        {...restProps}
+        className={classnames(className, prefixCls, `${prefixCls}-${mode}`)}
+      >
+        <div
+          className={`${prefixCls}-left`}
+          role="button"
+          onClick={onLeftClick}
+        >
+          {icon ? (
+            <span className={`${prefixCls}-left-icon`} aria-hidden="true">
+              {icon}
+            </span>
+          ) : null}
           {leftContent}
         </div>
         <div className={`${prefixCls}-title`}>{children}</div>
