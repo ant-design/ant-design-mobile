874c1533033734c36489f64b2f43bf2569f3e87b
bang <sqibang@gmail.com>
2018-02-07 12:12:22 +0800

    refactor(list-view): type definitions

--- components/list-view/Indexed.tsx
+++ components/list-view/Indexed.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import MListView from 'rmc-list-view';
-import tsPropsType from './PropsType';
 import handleProps from './handleProps';
+import { ListViewPropsType } from './PropsType';
 const IndexedList = MListView.IndexedList;
 
-export interface ListViewPropsType extends tsPropsType {
-  onQuickSearch?: Function;
+export interface MIndexedListProps extends ListViewPropsType {
+  onQuickSearch?: (sectionID: any, topId?: any) => void;
   quickSearchBarStyle?: React.CSSProperties;
-  quickSearchBarTop?: Object;
+  quickSearchBarTop?: {
+    value: string;
+    label: string;
+  };
   delayTime?: number;
-  delayActivityIndicator?: any;
+  delayActivityIndicator?: React.ReactNode;
 }
 
-export default class MIndexedList extends React.Component<ListViewPropsType, any> {
+export default class MIndexedList extends React.Component<
+  MIndexedListProps,
+  any
+> {
   static defaultProps = {
     prefixCls: 'am-indexed-list',
     listPrefixCls: 'am-list',
@@ -26,7 +32,7 @@ export default class MIndexedList extends React.Component<ListViewPropsType, any
     const { restProps, extraProps } = handleProps(this.props, true);
     return (
       <IndexedList
-        ref={el => this.indexedListRef = el}
+        ref={(el: any) => (this.indexedListRef = el)}
         sectionHeaderClassName={`${prefixCls}-section-header ${listPrefixCls}-body`}
         sectionBodyClassName={`${prefixCls}-section-body ${listPrefixCls}-body`}
         {...restProps}
