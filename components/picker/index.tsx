425098705ae5d7bc60f2750159e7569d842a9dd2
yuanfei.gyf <yuanfei.gyf@alibaba-inc.com>
2018-05-04 16:43:55 +0800

    fix: Picker && SearchBar LocalProvider Context

--- components/picker/index.tsx
+++ components/picker/index.tsx
@@ -1,8 +1,13 @@
 import AbstractPicker, { getDefaultProps } from './AbstractPicker';
+import PropTypes from 'prop-types';
 import popupProps from './popupProps';
 
 export default class Picker extends AbstractPicker {
   static defaultProps = getDefaultProps();
+
+  static contextTypes = {
+    antLocale: PropTypes.object,
+  };
 
   protected popupProps = popupProps;
 }
