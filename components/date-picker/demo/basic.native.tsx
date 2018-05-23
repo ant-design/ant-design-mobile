0a701dd205c636804f7e63d7b7d3a2283aad293b
然则 <hualei.hl@alibaba-inc.com>
2018-05-23 14:53:42 +0800

    RN(DatePicker): fix mode month, close #2499

--- components/date-picker/demo/basic.native.tsx
+++ components/date-picker/demo/basic.native.tsx
@@ -4,12 +4,9 @@ import { DatePicker, List } from 'antd-mobile';
 import React from 'react';
 import { View } from 'react-native';
 
-const now = new Date();
+// const now = new Date();
 
 export default class PopupExample extends React.Component<any, any> {
-  date1MinDate: any;
-  date1MaxDate: any;
-
   constructor(props: any) {
     super(props);
     this.state = {
@@ -26,15 +23,10 @@ export default class PopupExample extends React.Component<any, any> {
       <View>
         <List>
           <DatePicker
-            defaultDate={now}
             value={this.state.value}
             mode="date"
-            minDate={
-              this.date1MinDate || (this.date1MinDate = new Date(2015, 7, 6))
-            }
-            maxDate={
-              this.date1MaxDate || (this.date1MaxDate = new Date(2016, 11, 3))
-            }
+            minDate={new Date(2015, 7, 6)}
+            maxDate={new Date(2026, 11, 3)}
             onChange={this.onChange}
             format="YYYY-MM-DD"
           >
