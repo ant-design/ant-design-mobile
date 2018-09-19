ad3547194e7bade8525a24b54f8df01c7b0c2c7d
xushao <shaopeng.xsp@alibaba-inc.com>
2018-09-19 14:08:50 +0800

    LocaleProvider add zh_hk support

---
+++ components/input-item/locale/zh_HK.tsx
@@ -0,0 +1,5 @@
+export default {
+  confirmLabel: '確定',
+  backspaceLabel: '退格',
+  cancelKeyboardLabel: '收起鍵盤',
+};
---
+++ components/locale-provider/zh_HK.tsx
@@ -0,0 +1,20 @@
+import DatePickerView from '../date-picker-view/locale/en_US';
+import DatePicker from '../date-picker/locale/zh_HK';
+import InputItem from '../input-item/locale/zh_HK';
+import Pagination from '../pagination/locale/zh_HK';
+import Picker from '../picker/locale/zh_HK';
+import SearchBar from '../search-bar/locale/zh_HK';
+import Menu from '../menu/locale/zh_HK';
+import PullToRefresh from '../pull-to-refresh/locale/zh_HK';
+
+export default {
+  locale: 'zh_HK',
+  Pagination,
+  DatePicker,
+  DatePickerView,
+  InputItem,
+  Picker,
+  SearchBar,
+  Menu,
+  PullToRefresh,
+};
