f91a70b6370705f33dd2993f58ab8a9b5d4e9386
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-07-22 12:06:32 +0800

    chore: upgrade dependencies and add 750 dev config

--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,5 @@
+const pxToRem = require('postcss-pxtorem')
+
 export default {
   mode: 'site',
   title: 'Ant Design Mobile',
@@ -102,9 +104,17 @@ export default {
       background: #f5f5f5;
     }`,
   ],
+  // extraPostCSSPlugins: [
+  //   pxToRem({
+  //     rootValue: 50,
+  //     propList: ['*'],
+  //   }),
+  // ],
   themeConfig: {
     hd: {
-      rules: [],
+      rules: [
+        // {mode: 'vw', options: [100, 750]}
+      ],
     },
   },
 }
