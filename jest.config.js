46d74141dce448055a437d32a13c323cdcf824d9
xiaohuoni <448627663@qq.com>
2021-07-29 16:38:47 +0800

    feat: add test-utils and button tests

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  modulePathIgnorePatterns: [],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest/dist',
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+}
