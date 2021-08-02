e091c1426da7d16096fa15e8482c9f6dd838517a
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-08-02 15:08:15 +0800

    feat: Popup支持mask和afterShow属性

--- src/components/popup/index.tsx
+++ src/components/popup/index.tsx
@@ -1,9 +1,9 @@
 import classNames from 'classnames'
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, FC } from 'react'
 import { CSSTransition } from 'react-transition-group'
 import { ElementProps } from '../../utils/element-props'
 import { useInitialized } from '../../utils/use-initialized'
-import { withDefaultProps } from '../../utils/with-default-props'
+import { mergeProps } from '../../utils/with-default-props'
 import Mask from '../mask'
 import { useLockScroll } from '../../utils/use-lock-scroll'
 import {
@@ -15,10 +15,12 @@ const classPrefix = `am-popup`
 
 export type PopupProps = {
   visible?: boolean
+  mask?: boolean
   onMaskClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
   destroyOnClose?: boolean
   forceRender?: boolean
   getContainer?: GetContainer
+  afterShow?: () => void
   afterClose?: () => void
   position?: 'bottom' | 'top' | 'left' | 'right'
   bodyClassName?: string
@@ -31,9 +33,11 @@ const defaultProps = {
   position: 'bottom',
   visible: false,
   getContainer: () => document.body,
+  mask: true,
 }
 
-const Popup = withDefaultProps(defaultProps)<PopupProps>(props => {
+const Popup: FC<PopupProps> = p => {
+  const props = mergeProps(defaultProps, p)
   // 动画执行完，才隐藏最外层
   const [hidden, setHidden] = useState(!props.visible)
 
@@ -69,17 +73,20 @@ const Popup = withDefaultProps(defaultProps)<PopupProps>(props => {
 
   const node = (
     <div className={cls} style={props.style}>
-      <Mask
-        visible={props.visible}
-        onMaskClick={props.onMaskClick}
-        className={props.maskClassName}
-        style={props.maskStyle}
-        disableBodyScroll={false}
-      />
+      {props.mask && (
+        <Mask
+          visible={props.visible}
+          onMaskClick={props.onMaskClick}
+          className={props.maskClassName}
+          style={props.maskStyle}
+          disableBodyScroll={false}
+        />
+      )}
       <CSSTransition
         classNames={`${classPrefix}-body`}
         in={props.visible}
         timeout={200}
+        onEntered={props.afterShow}
         onExited={afterClose}
         unmountOnExit={props.destroyOnClose}
       >
@@ -91,6 +98,6 @@ const Popup = withDefaultProps(defaultProps)<PopupProps>(props => {
   )
 
   return renderToContainer(props.getContainer, node)
-})
+}
 
 export default Popup
