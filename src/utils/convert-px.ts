95c71231d319d7b9be0d8881a30a7e386664c21e
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-08-10 16:33:30 +0800

    fix: convertPx 的计算精度问题

--- src/utils/convert-px.ts
+++ src/utils/convert-px.ts
@@ -3,6 +3,6 @@ tester.className = 'am-one-px-tester'
 document.body.appendChild(tester)
 
 export function convertPx(px: number) {
-  const ratio = tester.clientHeight || 1
+  const ratio = tester.getBoundingClientRect().height || 1
   return px * ratio
 }
