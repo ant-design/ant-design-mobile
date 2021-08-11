358ae60cc45c4476ff1b923aef494e0b1e8f4944
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-08-11 17:06:47 +0800

    chore: lint 规则调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   'plugins': ['@typescript-eslint'],
   'rules': {
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
