e5ce97f171533fa653b6bab3d6d3945f80e7b857
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-08-12 11:55:22 +0800

    feat: Input 支持 --font-size

--- src/demos/demo-block/index.tsx
+++ src/demos/demo-block/index.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export const DemoBlock: FC<Props> = props => {
   return (
-    <div>
+    <div className={styles.demoBlock}>
       <div className={styles.title}>{props.title}</div>
       <div
         className={styles.content}
