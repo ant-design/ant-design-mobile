da1f4d55323ed4bcc5d6d18fd4aa7a95385cf53b
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-09-01 18:02:23 +0800

    enhance: Swiper 增加 count 校验

--- src/components/swiper/swiper.tsx
+++ src/components/swiper/swiper.tsx
@@ -8,6 +8,7 @@ import { useSpring, animated } from '@react-spring/web'
 import { useDrag } from 'react-use-gesture'
 import { bound } from '../../utils/rubberband'
 import { PageIndicator } from './page-indicator'
+import { staged } from 'staged-components'
 
 export type SwiperProps = {
   defaultIndex?: number
@@ -26,101 +27,112 @@ const defaultProps = {
   loop: true,
 }
 
-export const Swiper: FC<SwiperProps> = p => {
+export const Swiper: FC<SwiperProps> = staged(p => {
   const props = mergeProps(defaultProps, p)
 
   const count = React.Children.count(props.children)
 
-  const trackRef = useRef<HTMLDivElement>(null)
-  function getWidth() {
-    const track = trackRef.current
-    if (!track) return 0
-    return track.offsetWidth
+  if (count === 0) {
+    devWarning('Swiper', '`Swiper` needs at least one child.')
+    return null
   }
 
-  const [current, setCurrent] = useState(props.defaultIndex)
+  return () => {
+    let loop = props.loop
+    if (count === 1 && loop) {
+      devWarning(
+        'Swiper',
+        '`Swiper` needs at least two children to enable loop.'
+      )
+      loop = false
+    }
+    const trackRef = useRef<HTMLDivElement>(null)
+    function getWidth() {
+      const track = trackRef.current
+      if (!track) return 0
+      return track.offsetWidth
+    }
 
-  const [{ x }, api] = useSpring(() => ({
-    x: bound(current, 0, count - 1) * -100,
-    config: { tension: 200, friction: 30 },
-  }))
+    const [current, setCurrent] = useState(props.defaultIndex)
 
-  const bind = useDrag(
-    state => {
-      const width = getWidth()
-      if (!width) return
-      const [mx] = state.movement
-      if (state.last) {
-        const index = bound(
-          -Math.round((mx + state.vxvy[0] * 100) / width),
-          current - 1,
-          current + 1
-        )
-        setCurrent(index)
-        props.onIndexChange?.(index)
-        api.start({
-          x: index * -100,
-        })
-      } else {
-        api.start({
-          x: (mx * 100) / width,
-          immediate: true,
-        })
-      }
-    },
-    {
-      initial: () => {
+    const [{ x }, api] = useSpring(() => ({
+      x: bound(current, 0, count - 1) * -100,
+      config: { tension: 200, friction: 30 },
+    }))
+
+    const bind = useDrag(
+      state => {
         const width = getWidth()
-        return [(x.get() / 100) * width, 0]
-      },
-      bounds: () => {
-        const width = getWidth()
-        const index: number = -Math.round(x.get() / 100)
-        return {
-          right: Math.max(index - 1, 0) * -width,
-          left: Math.min(index + 1, count - 1) * -width,
+        if (!width) return
+        const [mx] = state.movement
+        if (state.last) {
+          const index = bound(
+            -Math.round((mx + state.vxvy[0] * 100) / width),
+            current - 1,
+            current + 1
+          )
+          setCurrent(index)
+          props.onIndexChange?.(index)
+          api.start({
+            x: index * -100,
+          })
+        } else {
+          api.start({
+            x: (mx * 100) / width,
+            immediate: true,
+          })
         }
       },
-      rubberband: true,
-      axis: 'x',
-      experimental_preventWindowScrollY: true,
-    }
-  )
+      {
+        initial: () => {
+          const width = getWidth()
+          return [(x.get() / 100) * width, 0]
+        },
+        bounds: () => {
+          if (loop) return {}
+          const width = getWidth()
+          return {
+            right: 0,
+            left: (count - 1) * -width,
+          }
+        },
+        rubberband: !loop,
+        axis: 'x',
+        experimental_preventWindowScrollY: true,
+      }
+    )
 
-  return (
-    <div
-      className={classNames('adm-swiper', props.className)}
-      style={props.style}
-    >
+    return (
       <div
-        className='adm-swiper-track'
-        ref={trackRef}
-        {...(props.allowTouchMove ? bind() : {})}
+        className={classNames('adm-swiper', props.className)}
+        style={props.style}
       >
-        <animated.div
-          className='adm-swiper-track-inner'
-          style={{ x: x.to(x => `${x}%`) }}
+        <div
+          className='adm-swiper-track'
+          ref={trackRef}
+          {...(props.allowTouchMove ? bind() : {})}
         >
-          {React.Children.map(props.children, child => {
-            if (!React.isValidElement(child)) return null
-            if (child.type !== SwiperItem) {
-              devWarning(
-                'Swiper',
-                'The children of `Swiper` must be `Swiper.Item` components.'
-              )
-              return null
-            }
-            return (
-              <div className='adm-swiper-slide' key={child.key}>
-                {child}
-              </div>
-            )
-          })}
-        </animated.div>
+          <animated.div
+            className='adm-swiper-track-inner'
+            style={{ x: x.to(x => `${x}%`) }}
+          >
+            {React.Children.map(props.children, child => {
+              if (!React.isValidElement(child)) return null
+              if (child.type !== SwiperItem) {
+                devWarning(
+                  'Swiper',
+                  'The children of `Swiper` must be `Swiper.Item` components.'
+                )
+                return null
+              }
+              return <div className='adm-swiper-slide'>{child}</div>
+            })}
+          </animated.div>
+        </div>
+        <div className='adm-swiper-indicator'>
+          <PageIndicator total={count} current={current} />
+        </div>
       </div>
-      <div className='adm-swiper-indicator'>
-        <PageIndicator total={count} current={current} />
-      </div>
-    </div>
-  )
-}
+    )
+  }
+})
