890a5fc974291a4b49f6dd757c2edb1ccc2bd02f
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-09-05 00:24:10 +0800

    doc: adjust CheckList demo

--- src/components/check-list/demos/demo1.tsx
+++ src/components/check-list/demos/demo1.tsx
@@ -6,14 +6,14 @@ export default () => {
   return (
     <>
       <DemoBlock title='基础用法' padding='0' border='none'>
-        <CheckList>
+        <CheckList defaultValue={['B']}>
           <CheckList.Item value='A'>A</CheckList.Item>
           <CheckList.Item value='B'>B</CheckList.Item>
           <CheckList.Item value='C'>C</CheckList.Item>
         </CheckList>
       </DemoBlock>
       <DemoBlock title='多选' padding='0' border='none'>
-        <CheckList multiple>
+        <CheckList multiple defaultValue={['B', 'C']}>
           <CheckList.Item value='A'>A</CheckList.Item>
           <CheckList.Item value='B'>B</CheckList.Item>
           <CheckList.Item value='C'>C</CheckList.Item>
