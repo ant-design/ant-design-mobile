ede83af39e43a5c659e513b2b0ad6163d1ce8d1d
jm <604096529@qq.com>
2021-09-15 11:04:18 +0800

    feat: Result icon

--- src/components/result/result.tsx
+++ src/components/result/result.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, ReactNode } from 'react'
 import classNames from 'classnames'
 import {
   CheckCircleFilled,
@@ -22,22 +22,21 @@ export interface ResultProps {
   status: 'success' | 'error' | 'info' | 'waiting' | 'warning'
   title: string
   description?: string
+  icon?: ReactNode
   className?: string
   style?: React.CSSProperties
 }
 
 export const Result: FC<ResultProps> = props => {
-  const { className, style, status, title, description } = props
-  const ResultIcon = ICONS[status]
+  const { className, style, status, title, description, icon } = props
+  const resultIcon = icon || React.createElement(ICONS[status])
 
   return (
     <div
       className={classNames(classPrefix, `${classPrefix}-${status}`, className)}
       style={style}
     >
-      <div className={`${classPrefix}-icon`}>
-        <ResultIcon />
-      </div>
+      <div className={`${classPrefix}-icon`}>{resultIcon}</div>
       <div className={`${classPrefix}-title`}>{title}</div>
       {description ? (
         <div className={`${classPrefix}-description`}>{description}</div>
