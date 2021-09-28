60b34c09f01693980f2a07a5103032748728cfde
lize.yc <lize.yc@alibaba-inc.com>
2021-09-28 20:16:37 +0800

    fix:修复lint错误

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -7,7 +7,7 @@
 export function shuffle(array: any[]) {
   const result = [...array]
   for (let i = result.length; i > 0; i--) {
-    let j = Math.floor(Math.random() * i)
+    const j = Math.floor(Math.random() * i)
     ;[result[i - 1], result[j]] = [result[j], result[i - 1]]
   }
   return result
