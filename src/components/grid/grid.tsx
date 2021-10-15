9de66d26816d14c0fefa64f141fe0f09805c71c0
zqran <uuxnet@gmail.com>
2021-10-15 18:29:12 +0800

    feat: (Grid) remove `any` type

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -9,12 +9,15 @@ export type GridProps = {
   gap?: number | number[] | string | string[]
 } & NativeProps
 
+type GapStyle = Partial<Record<'--vertical-gap' | '--horizontal-gap', string>>
+type GridStyle = React.CSSProperties & Record<'--columns', GridProps['columns']>
+
 const defaultProps = {
   gap: 0,
 }
 
 export const Grid = withDefaultProps(defaultProps)<GridProps>(props => {
-  let gapStyle: any = {}
+  let gapStyle: GapStyle = {}
   const { gap } = props
   if (gap) {
     const [horizontalGap, verticalGap] = Array.isArray(gap) ? gap : [gap, gap]
@@ -28,15 +31,14 @@ export const Grid = withDefaultProps(defaultProps)<GridProps>(props => {
           : horizontalGap,
     }
   }
+  const style: GridStyle = {
+    ...gapStyle,
+    '--columns': props.columns,
+  }
+
   return withNativeProps(
     props,
-    <div
-      className={classPrefix}
-      style={{
-        ...gapStyle,
-        '--columns': props.columns,
-      }}
-    >
+    <div className={classPrefix} style={style}>
       {props.children}
     </div>
   )
@@ -46,10 +48,13 @@ export type GridItemProps = {
   span?: number
 } & NativeProps
 
+type GridItemStyle = React.CSSProperties &
+  Record<'--item-span', GridItemProps['span']>
+
 export const GridItem = withDefaultProps({
   span: 1,
 })<GridItemProps>(props => {
-  const itemStyle: any = {
+  const itemStyle: GridItemStyle = {
     '--item-span': props.span,
   }
   return withNativeProps(
