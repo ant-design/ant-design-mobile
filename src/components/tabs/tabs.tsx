e1db02ab9259e4787dd0c8d5e8e969d5592c38ca
GyDi <segydi@foxmail.com>
2021-10-16 03:52:51 +0800

    enhance(tabs): auto scroll and switch animation

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -1,8 +1,19 @@
-import { FC, ReactNode, ReactElement, ComponentProps } from 'react'
+import {
+  FC,
+  ReactNode,
+  ReactElement,
+  ComponentProps,
+  useState,
+  useRef,
+  useLayoutEffect,
+} from 'react'
 import React from 'react'
 import classNames from 'classnames'
+import { useSpring, animated } from '@react-spring/web'
+import { useDrag } from '@use-gesture/react'
 import { NativeProps, withNativeProps } from '../../utils/native-props'
-import { useNewControllableValue } from '../../utils/use-controllable-value'
+import { supportsPassive } from '../../utils/supports-passive'
+import { bound } from '../../utils/bound'
 
 const classPrefix = `adm-tabs`
 
@@ -23,58 +34,143 @@ export type TabsProps = {
 
 export const Tabs: FC<TabsProps> = props => {
   const childrenRecord: Record<string, ReactNode> = {}
-  let firstActiveKey: string | null = null
 
   const panes: ReactElement<ComponentProps<typeof TabPane>>[] = []
 
-  React.Children.forEach(props.children, (child, index) => {
+  React.Children.forEach(props.children, child => {
     if (!React.isValidElement(child)) return
-    const key = child.key
+    const { key } = child
     if (typeof key !== 'string') return
-    if (index === 0) {
-      firstActiveKey = key
-    }
+
     childrenRecord[key] = child.props.children
     panes.push(child)
   })
 
-  const [activeKey, setActiveKey] = useNewControllableValue({
-    value: props.activeKey,
-    defaultValue: props.defaultActiveKey ?? firstActiveKey,
-    onChange: props.onChange,
-  })
+  let defaultCurrent = 0
+  if (props.defaultActiveKey !== undefined) {
+    panes.forEach((child, index) => {
+      if (child.key === props.defaultActiveKey) {
+        defaultCurrent = index
+      }
+    })
+  }
+
+  const [current, setCurrent] = useState(defaultCurrent)
+
+  const trackRef = useRef<HTMLDivElement>(null)
+
+  const [{ x }, api] = useSpring(() => ({
+    x: 0,
+    config: {
+      tension: 200,
+      friction: 30,
+    },
+  }))
+
+  const [barStyle, barApi] = useSpring(() => ({
+    x: 0,
+    width: 0,
+  }))
+
+  const bind = useDrag(
+    state => {
+      if (!trackRef.current) return
+      const tracker = trackRef.current
+
+      const [offsetX] = state.offset
+
+      if (!state.last) {
+        api.start({
+          x: offsetX,
+          immediate: true,
+        })
+      } else {
+        const { clientWidth, scrollWidth } = tracker
+        api.start({
+          x: bound(offsetX, clientWidth - scrollWidth, 0),
+        })
+      }
+    },
+    {
+      from: () => [x.get(), 0],
+      axis: 'x',
+      preventScroll: true,
+      pointer: {
+        touch: true,
+      },
+      eventOptions: supportsPassive ? { passive: false } : false,
+    }
+  )
+
+  useLayoutEffect(() => {
+    if (!trackRef.current) return
+    const tracker = trackRef.current
+
+    const widthSum = Array.from(tracker.children)
+      .slice(0, current)
+      .reduce((p, c) => p + c.clientWidth, 0)
+
+    const { clientWidth, scrollWidth } = tracker
+    const { clientWidth: childWidth } = tracker.children[current]
+
+    const offsetX = Math.max(0, (clientWidth - childWidth) / 2) - widthSum
+
+    api.start({
+      x: bound(offsetX, clientWidth - scrollWidth, 0),
+    })
+
+    barApi.start({
+      x: widthSum,
+      width: tracker.children[current].clientWidth,
+      immediate: barStyle.width.get() === 0,
+    })
+  }, [current])
 
   return withNativeProps(
     props,
     <div className={classPrefix}>
-      <div className={`${classPrefix}-tab-list`}>
-        {panes.map(pane =>
-          withNativeProps(
-            pane.props,
-            <div key={pane.key} className={`${classPrefix}-tab-wrapper`}>
-              <div
-                onClick={() => {
-                  const { key } = pane
-                  if (key === undefined || key === null) {
-                    return
-                  }
-                  setActiveKey(key.toString())
-                }}
-                className={classNames(`${classPrefix}-tab`, {
-                  [`${classPrefix}-tab-active`]: pane.key === activeKey,
-                })}
-              >
-                {pane.props.title}
+      <div className={`${classPrefix}-list`} {...bind()}>
+        <animated.div
+          className={`${classPrefix}-list-track`}
+          ref={trackRef}
+          style={{ x }}
+        >
+          {panes.map((pane, index) =>
+            withNativeProps(
+              pane.props,
+              <div key={pane.key} className={`${classPrefix}-tab-wrapper`}>
+                <div
+                  onClick={() => {
+                    const { key } = pane
+                    if (key === undefined || key === null) {
+                      return
+                    }
+
+                    props.onChange?.(key as string)
+                    setCurrent(index)
+                  }}
+                  className={classNames(`${classPrefix}-tab`, {
+                    [`${classPrefix}-tab-active`]: index === current,
+                  })}
+                >
+                  {pane.props.title}
+                </div>
               </div>
-            </div>
-          )
-        )}
+            )
+          )}
+
+          <animated.div
+            className={`${classPrefix}-list-bar`}
+            style={barStyle}
+          />
+        </animated.div>
       </div>
-      {panes.map(pane => {
+
+      {panes.map((pane, index) => {
         if (pane.props.children === undefined) {
           return null
         }
-        if (pane.key === activeKey) {
+        if (index === current) {
           return (
             <div key={pane.key} className={`${classPrefix}-content`}>
               {pane.props.children}
