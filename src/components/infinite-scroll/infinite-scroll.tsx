c21794994c116f87615f7c712af9ae1799057c60
zqran <uuxnet@gmail.com>
2021-10-28 17:42:24 +0800

    fix: (InfiniteScroll) prevent `doLoadMore` when hidden (#4276)
    * feat: (InfiniteScroll) prevent `doLoadMore` when hidden
    
    * fix: (InfiniteScroll) with display none parent
    
    Co-authored-by: 卡晨 <guangbo.hgb@alibaba-inc.com>

--- src/components/infinite-scroll/infinite-scroll.tsx
+++ src/components/infinite-scroll/infinite-scroll.tsx
@@ -45,12 +45,12 @@ export const InfiniteScroll = withDefaultProps({
       if (!props.hasMore) return
       const element = elementRef.current
       if (!element) return
+      if (!element.offsetParent) return
       const parent = getScrollParent(element)
       if (!parent) return
-      const elementTop = element.getBoundingClientRect().top
-      const current = isWindow(parent)
-        ? window.innerHeight
-        : parent.getBoundingClientRect().bottom
+      const rect = element.getBoundingClientRect()
+      const elementTop = rect.top
+      const current = isWindow(parent) ? window.innerHeight : rect.bottom
       if (current >= elementTop - props.threshold) {
         doLoadMore()
       }
