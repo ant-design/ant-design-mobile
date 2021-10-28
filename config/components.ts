3104de539a661383356a9e4641875d13fb159211
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-10-28 11:48:15 +0800

    feat: add SafeArea component (experimental)

--- config/components.ts
+++ config/components.ts
@@ -69,6 +69,7 @@ export const components = {
     '/components/image-uploader',
     '/components/number-keyboard',
     '/components/picker-view',
+    '/components/safe-area',
     '/components/tree-select',
   ],
 }
---
+++ src/components/safe-area/index.tsx
@@ -0,0 +1,6 @@
+import './safe-area.less'
+import { SafeArea } from './safe-area'
+
+export type { SafeAreaProps } from './safe-area'
+
+export default SafeArea
