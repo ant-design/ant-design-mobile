275da2771e35c053b5d41126b7ece2b3ebca9796
say-hai <mhlqian@qq.com>
2021-11-02 22:08:25 +0800

    refactor:(action-sheet) use mergeProps replace withDefaultProps

--- src/components/action-sheet/action-sheet.tsx
+++ src/components/action-sheet/action-sheet.tsx
@@ -1,12 +1,13 @@
 import React, {
-  forwardRef,
-  useState,
-  useImperativeHandle,
   createRef,
+  FC,
+  forwardRef,
   useEffect,
+  useImperativeHandle,
+  useState,
 } from 'react'
 import { NativeProps, withNativeProps } from '../../utils/native-props'
-import { withDefaultProps } from '../../utils/with-default-props'
+import { mergeProps } from '../../utils/with-default-props'
 import classNames from 'classnames'
 import Popup from '../popup'
 import Button from '../button'
@@ -46,85 +47,85 @@ const defaultProps = {
   closeOnMaskClick: true,
 }
 
-export const ActionSheet = withDefaultProps(defaultProps)<ActionSheetProps>(
-  props => {
-    return (
-      <Popup
-        visible={props.visible}
-        onMaskClick={() => {
-          props.onMaskClick?.()
-          if (props.closeOnMaskClick) {
-            props.onClose?.()
-          }
-        }}
-        afterClose={props.afterClose}
-        className={`${classPrefix}-popup`}
-        getContainer={props.getContainer}
-      >
-        {withNativeProps(
-          props,
-          <div className={classPrefix}>
-            {props.extra && (
-              <div className={`${classPrefix}-extra`}>{props.extra}</div>
-            )}
-            <div className={`${classPrefix}-button-list`}>
-              {props.actions.map((action, index) => (
-                <div
-                  key={action.key}
-                  className={`${classPrefix}-button-item-wrapper`}
-                >
-                  <Button
-                    block
-                    fill='none'
-                    disabled={action.disabled}
-                    onClick={() => {
-                      action.onClick?.()
-                      props.onAction?.(action, index)
-                      if (props.closeOnAction) {
-                        props.onClose?.()
-                      }
-                    }}
-                    className={classNames(`${classPrefix}-button-item`, {
-                      [`${classPrefix}-button-item-danger`]: action.danger,
-                    })}
-                  >
-                    <div className={`${classPrefix}-button-item-name`}>
-                      {action.text}
-                    </div>
-                    {action.description && (
-                      <div className={`${classPrefix}-button-item-description`}>
-                        {action.description}
-                      </div>
-                    )}
-                  </Button>
-                </div>
-              ))}
-            </div>
+export const ActionSheet: FC<ActionSheetProps> = p => {
+  const props = mergeProps(defaultProps, p)
 
-            {props.cancelText && (
-              <div className={`${classPrefix}-cancel`}>
-                <div className={`${classPrefix}-button-item-wrapper`}>
-                  <Button
-                    block
-                    fill='none'
-                    className={`${classPrefix}-button-item`}
-                    onClick={() => {
+  return (
+    <Popup
+      visible={props.visible}
+      onMaskClick={() => {
+        props.onMaskClick?.()
+        if (props.closeOnMaskClick) {
+          props.onClose?.()
+        }
+      }}
+      afterClose={props.afterClose}
+      className={`${classPrefix}-popup`}
+      getContainer={props.getContainer}
+    >
+      {withNativeProps(
+        props,
+        <div className={classPrefix}>
+          {props.extra && (
+            <div className={`${classPrefix}-extra`}>{props.extra}</div>
+          )}
+          <div className={`${classPrefix}-button-list`}>
+            {props.actions.map((action, index) => (
+              <div
+                key={action.key}
+                className={`${classPrefix}-button-item-wrapper`}
+              >
+                <Button
+                  block
+                  fill='none'
+                  disabled={action.disabled}
+                  onClick={() => {
+                    action.onClick?.()
+                    props.onAction?.(action, index)
+                    if (props.closeOnAction) {
                       props.onClose?.()
-                    }}
-                  >
-                    <div className={`${classPrefix}-button-item-name`}>
-                      {props.cancelText}
+                    }
+                  }}
+                  className={classNames(`${classPrefix}-button-item`, {
+                    [`${classPrefix}-button-item-danger`]: action.danger,
+                  })}
+                >
+                  <div className={`${classPrefix}-button-item-name`}>
+                    {action.text}
+                  </div>
+                  {action.description && (
+                    <div className={`${classPrefix}-button-item-description`}>
+                      {action.description}
                     </div>
-                  </Button>
-                </div>
+                  )}
+                </Button>
               </div>
-            )}
+            ))}
           </div>
-        )}
-      </Popup>
-    )
-  }
-)
+
+          {props.cancelText && (
+            <div className={`${classPrefix}-cancel`}>
+              <div className={`${classPrefix}-button-item-wrapper`}>
+                <Button
+                  block
+                  fill='none'
+                  className={`${classPrefix}-button-item`}
+                  onClick={() => {
+                    props.onClose?.()
+                  }}
+                >
+                  <div className={`${classPrefix}-button-item-name`}>
+                    {props.cancelText}
+                  </div>
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </Popup>
+  )
+}
 
 export type ActionSheetRef = {
   close: () => void
