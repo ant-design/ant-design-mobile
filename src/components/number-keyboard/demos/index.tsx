f2a469a59a803da2f493e7f716591998777f9d24
lize.yc <lize.yc@alibaba-inc.com>
2021-11-09 10:50:52 +0800

    fix:修复弹窗demo交互和弹窗键盘被遮挡问题

--- src/components/number-keyboard/demos/index.tsx
+++ src/components/number-keyboard/demos/index.tsx
@@ -64,6 +64,7 @@ export default () => {
                   </Button>
                 ),
               })
+              setVisible('')
               e.stopPropagation()
             }}
           >
