507653bb309e2bb5ed1031fb2a6b872ab739610d
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-11-15 16:26:01 +0800

    enhance: add px test validating logic (only in dev)

--- src/utils/convert-px.ts
+++ src/utils/convert-px.ts
@@ -1,4 +1,6 @@
 import { canUseDom } from './can-use-dom'
+import { isDev } from './is-dev'
+import { devError } from './dev-log'
 
 let onePxTest: HTMLDivElement | null = null
 let tester: HTMLDivElement | null = null
@@ -10,6 +12,14 @@ if (canUseDom) {
   tester = document.createElement('div')
   tester.className = 'adm-px-tester'
   document.body.appendChild(tester)
+  if (isDev) {
+    if (window.getComputedStyle(tester).position !== 'fixed') {
+      devError(
+        'Global',
+        'The px tester is not rendering properly. Please make sure you have imported `antd-mobile/es/global`.'
+      )
+    }
+  }
 }
 
 export function convertPx(px: number) {
