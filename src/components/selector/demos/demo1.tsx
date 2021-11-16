1a4b1c6ade7d54a9c797d454c844b907edb46792
Cavano <51314472+p697@users.noreply.github.com>
2021-11-16 17:28:46 +0800

    enhance: rename Selector's context to extend

--- src/components/selector/demos/demo1.tsx
+++ src/components/selector/demos/demo1.tsx
@@ -45,7 +45,7 @@ export default () => {
         <Selector
           options={ItemList}
           defaultValue={['1']}
-          onChange={(arr, context) => console.log(arr, context.items)}
+          onChange={(arr, extend) => console.log(arr, extend.items)}
         />
       </DemoBlock>
       <DemoBlock title='多选'>
@@ -53,7 +53,7 @@ export default () => {
           options={ItemList}
           defaultValue={['2', '3']}
           multiple={true}
-          onChange={(arr, context) => console.log(arr, context.items)}
+          onChange={(arr, extend) => console.log(arr, extend.items)}
         />
       </DemoBlock>
       <DemoBlock title='全局禁止'>
