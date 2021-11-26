122fecbc2e94a0968f7b6f1d0b9e77963c421094
Cavano <51314472+p697@users.noreply.github.com>
2021-11-26 11:01:26 +0800

    fix: (CascadePicker) onSelect accidentally triggered

--- src/components/cascade-picker-view/cascade-picker-view.tsx
+++ src/components/cascade-picker-view/cascade-picker-view.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import PickerView from '../picker-view'
+import type { PickerValue, PickerValueExtend } from '../picker-view'
+import { mergeProps } from '../../utils/with-default-props'
 import { NativeProps } from '../../utils/native-props'
 import type { PickerViewProps } from '../picker-view'
 import type { CascadePickerOption } from '../cascade-picker'
@@ -10,13 +12,32 @@ export type CascadePickerViewProps = Omit<PickerViewProps, 'columns'> & {
   options: CascadePickerOption[]
 } & NativeProps<'--height'>
 
-export const CascadePickerView: FC<CascadePickerViewProps> = props => {
+const defaultProps = {
+  defaultValue: [],
+}
+
+export const CascadePickerView: FC<CascadePickerViewProps> = p => {
+  const props = mergeProps(defaultProps, p)
   const { options, ...pickerProps } = props
   const { depth, subOptionsRecord } = useCascadePickerOptions(options)
+
+  const [value, setValue] = useState<PickerValue[]>(props.defaultValue)
+
+  const onChange = (val: PickerValue[], ext: PickerValueExtend) => {
+    setValue(val)
+    // 如果上一次的 value 与本次 val 的最后一个值一样，说明一定是非最后一列发生了变化，跳过本次 onChange
+    if (value[value.length - 1] === val[val.length - 1]) return
+    // 正在初始化，跳过 onChange
+    if (val.length < depth) return
+    if (val[0] === undefined) return
+    pickerProps.onChange?.(val, ext)
+  }
 
   return (
     <PickerView
       {...pickerProps}
+      value={value}
+      onChange={(val, ext) => onChange(val, ext)}
       columns={selected =>
         generateCascadePickerColumns(
           selected as string[],
--- src/components/cascade-picker/cascade-picker.tsx
+++ src/components/cascade-picker/cascade-picker.tsx
@@ -1,6 +1,8 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import Picker from '../picker'
 import type { PickerProps } from '../picker'
+import type { PickerValue, PickerValueExtend } from '../picker-view'
+import { mergeProps } from '../../utils/with-default-props'
 import { useCascadePickerOptions } from './use-cascade-picker-options'
 import { generateCascadePickerColumns } from './cascade-picker-utils'
 
@@ -14,13 +16,32 @@ export type CascadePickerProps = Omit<PickerProps, 'columns'> & {
   options: CascadePickerOption[]
 }
 
-export const CascadePicker: FC<CascadePickerProps> = props => {
+const defaultProps = {
+  defaultValue: [],
+}
+
+export const CascadePicker: FC<CascadePickerProps> = p => {
+  const props = mergeProps(defaultProps, p)
   const { options, ...pickerProps } = props
   const { depth, subOptionsRecord } = useCascadePickerOptions(options)
+
+  const [value, setValue] = useState<PickerValue[]>(props.defaultValue)
+
+  const onSelect = (val: PickerValue[], ext: PickerValueExtend) => {
+    setValue(val)
+    // 如果上一次的 value 与本次 val 的最后一个值一样，说明一定是非最后一列发生了变化，跳过本次 onSelect
+    if (value[value.length - 1] === val[val.length - 1]) return
+    // 正在初始化，跳过 onSelect
+    if (val.length < depth) return
+    if (val[0] === undefined) return
+    pickerProps.onSelect?.(val, ext)
+  }
 
   return (
     <Picker
       {...pickerProps}
+      value={value}
+      onSelect={onSelect}
       columns={selected =>
         generateCascadePickerColumns(
           selected as string[],
