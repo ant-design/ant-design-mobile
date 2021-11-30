1d145f383a6fdb12487bcb067593f6211f31845f
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-11-30 17:52:37 +0800

    enhance: (Picker) improve the performance of Picker series components

--- src/components/date-picker/date-picker.tsx
+++ src/components/date-picker/date-picker.tsx
@@ -10,6 +10,7 @@ import {
   generateDatePickerColumns,
   defaultRenderLabel,
 } from './date-picker-utils'
+import { usePersistFn } from 'ahooks'
 
 export type DatePickerProps = Pick<
   PickerProps,
@@ -63,28 +64,29 @@ export const DatePicker: FC<DatePickerProps> = p => {
     [setValue]
   )
 
-  const onSelect = useCallback(
-    (val: string[]) => {
-      const date = convertStringArrayToDate(val)
-      if (date) {
-        props.onSelect?.(date)
-      }
-    },
-    [props.onSelect]
+  const onSelect = usePersistFn((val: string[]) => {
+    const date = convertStringArrayToDate(val)
+    if (date) {
+      props.onSelect?.(date)
+    }
+  })
+
+  const columns = useCallback(
+    selected =>
+      generateDatePickerColumns(
+        selected as string[],
+        props.min,
+        props.max,
+        props.precision,
+        props.renderLabel
+      ),
+    [props.min, props.max, props.precision, props.renderLabel]
   )
 
   return withNativeProps(
     props,
     <Picker
-      columns={selected =>
-        generateDatePickerColumns(
-          selected as string[],
-          props.min,
-          props.max,
-          props.precision,
-          props.renderLabel
-        )
-      }
+      columns={columns}
       value={pickerValue}
       onCancel={props.onCancel}
       onClose={props.onClose}
