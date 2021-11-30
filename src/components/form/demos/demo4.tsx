f714994d7bf86c4b25e2dfa893208e3863dd3cd3
yangdepp <yangdepp@163.com>
2021-11-30 11:13:08 +0800

    doc: (Form) add customized form controls example (#4463)
    * doc: (Form)增加自定义表单控件的例子
    
    * doc: (Form) add customized form controls document
    
    * doc: (Form) fix customized form controls example code
    
    * doc: (Form) add customized form controls example code
    
    * doc: Form demo4
    
    Co-authored-by: 卡晨 <guangbo.hgb@alibaba-inc.com>

---
+++ src/components/form/demos/demo4.tsx
@@ -0,0 +1,114 @@
+import React, { useState, FC } from 'react'
+import { Form, Input, Button, Picker, Switch, Space } from 'antd-mobile'
+import { DownOutline } from 'antd-mobile-icons'
+import { DemoBlock } from 'demos'
+
+const columns = [['86', '01', '02', '03']]
+
+interface MobileValue {
+  preValue: string
+  realValue: string
+}
+
+interface MobileFieldProps {
+  value?: MobileValue
+  onChange?: (value: MobileValue) => void
+}
+
+const MobileField: FC<MobileFieldProps> = ({
+  value = { preValue: '86', realValue: '' },
+  onChange,
+}) => {
+  const [visible, setVisible] = useState(false)
+
+  const triggerValue = (changedValue: Partial<MobileValue>) => {
+    onChange?.({ ...value, ...changedValue })
+  }
+
+  const onRealValueChange = (value: string) => {
+    triggerValue({ realValue: value })
+  }
+
+  const onPreValueChange = (value: string[]) => {
+    triggerValue({ preValue: value[0] })
+  }
+  return (
+    <>
+      <Space align='center'>
+        <div onClick={() => setVisible(true)}>
+          <Space align='center'>
+            <div>+{value.preValue}</div>
+            <DownOutline />
+          </Space>
+        </div>
+        <Input
+          placeholder='请输入手机号'
+          value={value.realValue}
+          onChange={onRealValueChange}
+        />
+      </Space>
+      <Picker
+        columns={columns}
+        visible={visible}
+        onClose={() => {
+          setVisible(false)
+        }}
+        value={[value.preValue]}
+        onConfirm={onPreValueChange}
+      />
+    </>
+  )
+}
+
+export default () => {
+  const onFinish = (values: any) => {
+    console.log(values)
+  }
+
+  const checkMobile = (_: any, value: MobileValue) => {
+    if (value.realValue) {
+      return Promise.resolve()
+    }
+    return Promise.reject(new Error('手机号不能为空!'))
+  }
+
+  return (
+    <>
+      <DemoBlock
+        title='自定义表单控件'
+        padding='0'
+        border='none'
+        background='transparent'
+      >
+        <Form
+          layout='vertical'
+          onFinish={onFinish}
+          initialValues={{
+            mobile: { preValue: '86', realValue: '' },
+            status: '0',
+          }}
+          footer={
+            <Button block type='submit' color='primary'>
+              提交
+            </Button>
+          }
+        >
+          <Form.Item
+            label='姓名'
+            name='name'
+            rules={[{ required: true, message: '姓名不能为空!' }]}
+          >
+            <Input placeholder='请输入姓名' />
+          </Form.Item>
+          <Form.Item
+            label='手机号'
+            name='mobile'
+            rules={[{ required: true }, { validator: checkMobile }]}
+          >
+            <MobileField />
+          </Form.Item>
+        </Form>
+      </DemoBlock>
+    </>
+  )
+}
