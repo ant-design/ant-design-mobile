20adbcdf4d21506ec269cb6dc8dd0d718d01a495
WBH <48749219+wbh1328551759@users.noreply.github.com>
2021-12-02 19:31:18 +0800

    feat(getTreeDeep): Replace if else with trot operator
    I think you can use a more concise ternary operator here

--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -3,11 +3,7 @@ export function getTreeDeep(treeData: any[], childrenName = 'children') {
   const walker = (tree: any[]) => {
     let deep = 0
     tree.forEach(item => {
-      if (item[childrenName]) {
-        deep = Math.max(deep, walker(item[childrenName]) + 1)
-      } else {
-        deep = Math.max(deep, 1)
-      }
+      deep = item[childrenName] ? Math.max(deep, walker(item[childrenName]) + 1) : Math.max(deep, 1)
     })
     return deep
   }
