11d72f8b18b06f5477c198f685b3df329d0c1b2a
awmleer <awmleer@icloud.com>
2021-12-28 15:40:03 +0800

    doc: Calendar

--- src/components/calendar/demos/demo1.tsx
+++ src/components/calendar/demos/demo1.tsx
@@ -1,14 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Calendar } from 'antd-mobile'
 import { DemoBlock, DemoDescription } from 'demos'
-import dayjs from 'dayjs'
 
 export default () => {
-  const today = dayjs()
-  const [val, setVal] = useState<[Date, Date] | null>(() => [
-    today.subtract(2, 'day').toDate(),
-    today.add(2, 'day').toDate(),
-  ])
   return (
     <>
       <DemoBlock title='仅展示'>
@@ -30,26 +24,6 @@ export default () => {
           selectionMode='range'
           onChange={val => {
             console.log(val)
-          }}
-        />
-      </DemoBlock>
-      <DemoBlock title='受控模式'>
-        <Calendar
-          selectionMode='range'
-          value={val}
-          onChange={val => {
-            setVal(val)
-          }}
-        />
-      </DemoBlock>
-      <DemoBlock title='给日期添加一些标记'>
-        <Calendar
-          selectionMode='range'
-          renderLabel={date => {
-            if (dayjs(date).isSame(today, 'day')) return '今天'
-            if (date.getDay() === 0 || date.getDay() === 6) {
-              return '周末'
-            }
           }}
         />
       </DemoBlock>
