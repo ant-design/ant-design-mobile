159040fc74eac15328c7d58132d38b647394bade
awmleer <awmleer@icloud.com>
2022-01-17 09:48:49 +0800

    doc: update ding group link

--- docs/components/home-page.tsx
+++ docs/components/home-page.tsx
@@ -265,7 +265,7 @@ export default () => {
             <li>钉钉交流反馈群</li>
             <li>
               <img
-                src='https://gw.alipayobjects.com/mdn/rms_25513e/afts/img/A*fo-tQ5wRk5QAAAAAAAAAAAAAARQnAQ'
+                src='https://gw.alipayobjects.com/mdn/rms_25513e/afts/img/A*wy8nQLwxbKYAAAAAAAAAAAAAARQnAQ'
                 alt='ding-group'
                 width={240}
               />
