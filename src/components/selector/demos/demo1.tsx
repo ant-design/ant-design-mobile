213427de3a3662e89c6b6b356fb15701682bd344
awmleer <awmleer@icloud.com>
2022-01-21 17:04:21 +0800

    enhance: (Selector) adjust style

--- src/components/selector/demos/demo1.tsx
+++ src/components/selector/demos/demo1.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import { Selector } from 'antd-mobile'
+import { Selector, Space } from 'antd-mobile'
 import { DemoBlock } from 'demos'
 
 import './demo1.less'
 
-const ItemList = [
+const options = [
   {
     label: '选项一',
     value: '1',
@@ -12,15 +12,10 @@ const ItemList = [
   {
     label: '选项二',
     value: '2',
-    disabled: true,
   },
   {
     label: '选项三',
     value: '3',
-  },
-  {
-    label: '选项四',
-    value: '4',
   },
 ]
 
@@ -29,7 +24,7 @@ const RadioMode = () => {
   const [value, setValue] = useState('1')
   return (
     <Selector
-      options={ItemList}
+      options={options}
       value={[value]}
       onChange={v => {
         if (v.length) {
@@ -45,7 +40,7 @@ export default () => {
     <>
       <DemoBlock title='单选'>
         <Selector
-          options={ItemList}
+          options={options}
           defaultValue={['1']}
           onChange={(arr, extend) => console.log(arr, extend.items)}
         />
@@ -53,39 +48,52 @@ export default () => {
 
       <DemoBlock title='多选'>
         <Selector
-          options={ItemList}
+          options={options}
           defaultValue={['2', '3']}
           multiple={true}
           onChange={(arr, extend) => console.log(arr, extend.items)}
         />
       </DemoBlock>
 
-      <DemoBlock title='全局禁止'>
-        <Selector
-          options={ItemList}
-          defaultValue={['1']}
-          disabled={true}
-          onChange={arr => console.log(arr)}
-        />
+      <DemoBlock title='禁用'>
+        <Space block direction='vertical'>
+          <Selector options={options} defaultValue={['1']} disabled={true} />
+          <Selector
+            options={[
+              {
+                label: '选项一',
+                value: '1',
+              },
+              {
+                label: '选项二',
+                value: '2',
+                disabled: true,
+              },
+              {
+                label: '选项三',
+                value: '3',
+              },
+            ]}
+            defaultValue={['3']}
+          />
+        </Space>
       </DemoBlock>
 
       <DemoBlock title='固定两列'>
         <Selector
           columns={2}
-          options={ItemList}
+          options={options}
           defaultValue={['2', '3']}
           multiple={true}
-          onChange={arr => console.log(arr)}
         />
       </DemoBlock>
 
       <DemoBlock title='固定三列'>
         <Selector
           columns={3}
-          options={ItemList}
+          options={options}
           defaultValue={['2', '3']}
           multiple={true}
-          onChange={arr => console.log(arr)}
         />
       </DemoBlock>
 
@@ -115,7 +123,7 @@ export default () => {
       <DemoBlock title='自定义样式（通过 style）'>
         <Selector
           style={{ '--checked-color': '#ffe2e5' }}
-          options={ItemList}
+          options={options}
           defaultValue={['1', '2']}
           multiple={true}
         />
@@ -124,7 +132,7 @@ export default () => {
       <DemoBlock title='自定义样式（通过 className）'>
         <Selector
           className='my-selector'
-          options={ItemList}
+          options={options}
           defaultValue={['1', '2']}
           multiple={true}
         />
