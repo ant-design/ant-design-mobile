d4aa8a83e3f5d97f5dedabd49ad25fb4d136d33a
awmleer <awmleer@icloud.com>
2022-02-10 16:14:45 +0800

    fix: (Popover.Menu) fix type definitions about `actions`

--- src/components/popover/popover-menu.tsx
+++ src/components/popover/popover-menu.tsx
@@ -16,17 +16,16 @@ export type Action = {
   text: React.ReactNode
   icon?: React.ReactNode
   disabled?: boolean
-  key?: string
+  key?: string | number
   onClick?: () => void
-  [key: string]: any
 }
 
-export type PopoverMenuProps<T> = Omit<PopoverProps, 'content'> & {
-  actions: T[]
-  onAction?: (text: T) => void
+export type PopoverMenuProps = Omit<PopoverProps, 'content'> & {
+  actions: Action[]
+  onAction?: (item: Action) => void
 }
 
-export const PopoverMenu = forwardRef<PopoverRef, PopoverMenuProps<Action>>(
+export const PopoverMenu = forwardRef<PopoverRef, PopoverMenuProps>(
   (props, ref) => {
     const innerRef = useRef<PopoverRef>(null)
     useImperativeHandle(ref, () => innerRef.current!, [])
@@ -84,6 +83,4 @@ export const PopoverMenu = forwardRef<PopoverRef, PopoverMenuProps<Action>>(
       </Popover>
     )
   }
-) as <T extends Action = Action>(
-  props: PopoverMenuProps<T> & { ref?: Ref<PopoverRef> }
-) => ReactElement
+)
