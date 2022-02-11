7f876941fcddb157d0a503ff8a5325b36af5077d
awmleer <awmleer@icloud.com>
2022-02-11 16:18:26 +0800

    doc: (Form) update demo

--- src/components/form/demos/demo3.tsx
+++ src/components/form/demos/demo3.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Form, Input, Button } from 'antd-mobile'
-import { AddSquareOutline } from 'antd-mobile-icons'
 
 export default function () {
   const [form] = Form.useForm()
@@ -15,19 +14,8 @@ export default function () {
       }
     >
       <Form.Header>在表单字段内增加额外内容</Form.Header>
-      <Form.Item label='运输车辆'>
-        <Form.Item
-          name='cph'
-          noStyle
-          rules={[{ required: true, message: '请选择运输车辆' }]}
-        >
-          <Input />
-        </Form.Item>
-        <AddSquareOutline />
-      </Form.Item>
-
       <Form.Item label='短信验证码' extra={<a>发送验证码</a>}>
-        <Input />
+        <Input placeholder='请输入' />
       </Form.Item>
     </Form>
   )
