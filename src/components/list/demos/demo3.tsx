b08c5ad1a5a7d7c13e3a67606e2f77a0aa3ee8be
awmleer <awmleer@icloud.com>
2022-02-14 11:56:32 +0800

    doc: (List) update demo

--- src/components/list/demos/demo3.tsx
+++ src/components/list/demos/demo3.tsx
@@ -2,10 +2,19 @@ import { List } from 'antd-mobile'
 import React from 'react'
 
 export default () => {
+  function handleClick() {
+    // ...
+  }
   return (
     <List mode='card' header='卡片模式'>
-      <List.Item title='这里是标题'>这里是主信息</List.Item>
-      <List.Item title='这里是标题'>这里是主信息</List.Item>
+      <List.Item extra='按照支付设置的顺序扣款' onClick={handleClick}>
+        扣款方式
+      </List.Item>
+      <List.Item extra='200元' onClick={handleClick}>
+        月限额
+      </List.Item>
+      <List.Item onClick={handleClick}>帮助中心</List.Item>
+      <List.Item onClick={handleClick}>关闭服务</List.Item>
     </List>
   )
 }
