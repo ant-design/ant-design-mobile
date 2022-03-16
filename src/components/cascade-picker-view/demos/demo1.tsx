ee8cfcf8f5dedf69ad9c28e0d001adc3bff1bcf8
lujie <lujie@yuantutech.com>
2022-03-16 12:25:43 +0800

    doc: (CascadePickerView) update demo

--- src/components/cascade-picker-view/demos/demo1.tsx
+++ src/components/cascade-picker-view/demos/demo1.tsx
@@ -1,39 +1,8 @@
 import React, { useState } from 'react'
 import { CascadePickerView } from 'antd-mobile'
 import { DemoBlock } from 'demos'
-
+import { options } from './options-data'
 import { AsyncDemo } from './async-demo'
-
-const options = [
-  {
-    label: '浙江',
-    value: '浙江',
-    children: [
-      {
-        label: '杭州',
-        value: '杭州',
-      },
-      {
-        label: '宁波',
-        value: '宁波',
-      },
-    ],
-  },
-  {
-    label: '江苏',
-    value: '江苏',
-    children: [
-      {
-        label: '南京',
-        value: '南京',
-      },
-      {
-        label: '苏州',
-        value: '苏州',
-      },
-    ],
-  },
-]
 
 export default () => {
   const [value, setValue] = useState<(string | null)[]>(['浙江', '杭州'])
