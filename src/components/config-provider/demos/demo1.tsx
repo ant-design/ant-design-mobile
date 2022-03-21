18760cfb0e2b22ecd34115c95ae9fd8b9eefb5e3
lize.yc <lize.yc@alibaba-inc.com>
2022-03-21 12:21:51 +0800

    feat:新增香港繁体和台湾繁体

--- src/components/config-provider/demos/demo1.tsx
+++ src/components/config-provider/demos/demo1.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { ConfigProvider, ErrorBlock } from 'antd-mobile'
 import { DemoBlock } from 'demos'
 import zhCN from 'antd-mobile/es/locales/zh-CN'
+import zhTW from 'antd-mobile/es/locales/zh-TW'
+import zhHK from 'antd-mobile/es/locales/zh-HK'
 import enUS from 'antd-mobile/es/locales/en-US'
 
 export default () => {
@@ -9,13 +11,25 @@ export default () => {
     <>
       <DemoBlock title='中文'>
         <ConfigProvider locale={zhCN}>
-          <ErrorBlock />
+          <ErrorBlock status='disconnected' />
         </ConfigProvider>
       </DemoBlock>
 
       <DemoBlock title='英文'>
         <ConfigProvider locale={enUS}>
-          <ErrorBlock />
+          <ErrorBlock status='disconnected' />
+        </ConfigProvider>
+      </DemoBlock>
+
+      <DemoBlock title='中文(台湾繁体)'>
+        <ConfigProvider locale={zhTW}>
+          <ErrorBlock status='disconnected' />
+        </ConfigProvider>
+      </DemoBlock>
+
+      <DemoBlock title='中文(香港繁体)'>
+        <ConfigProvider locale={zhHK}>
+          <ErrorBlock status='disconnected' />
         </ConfigProvider>
       </DemoBlock>
     </>
