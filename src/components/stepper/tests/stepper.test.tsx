26a64af3cf6ec11571bf4089ad6b517c15b83125
dengqing <qing.deng@goldenpig.com.cn>
2022-03-29 17:27:20 +0800

    fix: add test case

--- src/components/stepper/tests/stepper.test.tsx
+++ src/components/stepper/tests/stepper.test.tsx
@@ -1,6 +1,5 @@
 import { fireEvent, render, waitFor } from '@testing-library/react'
-import { wait } from '@testing-library/user-event/dist/utils'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Stepper } from '../stepper'
 
 describe('stepper', () => {
@@ -32,15 +31,21 @@ describe('stepper', () => {
     container.getElementsByTagName('button')[1].click()
     expect(container.getElementsByTagName('input')[0].value).toBe('2')
 
+    const input = container.getElementsByTagName('input')[0]
+
+    fireEvent.focus(input)
+
     // input change
     fireEvent.change(container.getElementsByTagName('input')[0], {
       target: { value: 1000 },
     })
 
+    fireEvent.blur(input)
+
     expect(container.getElementsByTagName('input')[0].value).toBe('1000')
   })
 
-  test('defaultValue works', () => {
+  test('defaultValue works', async () => {
     const onChange = jest.fn()
     const { container } = render(
       <Stepper defaultValue={100} onChange={onChange} />
@@ -48,7 +53,7 @@ describe('stepper', () => {
 
     const input = container.getElementsByTagName('input')[0]
 
-    expect(input.value).toEqual('100')
+    expect(input.value).toBe('100')
 
     fireEvent.change(input, {
       target: {
@@ -56,7 +61,9 @@ describe('stepper', () => {
       },
     })
 
-    waitFor(() => {
+    fireEvent.blur(input)
+
+    await waitFor(() => {
       expect(onChange).toHaveBeenLastCalledWith(100)
     })
   })
@@ -78,10 +85,10 @@ describe('stepper', () => {
   test('digits works', () => {
     const { container } = render(<Stepper value={0.1} digits={2} />)
 
-    expect(container.getElementsByTagName('input')[0].value).toEqual('0.10')
+    expect(container.getElementsByTagName('input')[0].value).toBe('0.10')
   })
 
-  test('min and max works', () => {
+  test('min and max works', async () => {
     const onChange = jest.fn()
     const { container } = render(
       <Stepper
@@ -107,12 +114,15 @@ describe('stepper', () => {
       expect((onChange.mock as any).lastCall[0]).toBeGreaterThanOrEqual(0)
     }
 
+    const input = container.getElementsByTagName('input')[0]
     // input change
-    fireEvent.change(container.getElementsByTagName('input')[0], {
+    fireEvent.change(input, {
       target: { value: 1000 },
     })
 
-    waitFor(() => {
+    fireEvent.blur(input)
+
+    await waitFor(() => {
       expect(onChange).toHaveBeenLastCalledWith(0.2)
     })
   })
@@ -166,5 +176,51 @@ describe('stepper', () => {
 
     expect(onFocus).toHaveBeenCalledTimes(1)
     expect(onBlur).toHaveBeenCalledTimes(1)
+  })
+
+  test('value as NaN works', async () => {
+    const { container } = render(<Stepper defaultValue={1000} />, {})
+
+    const input = container.getElementsByTagName('input')[0]
+
+    fireEvent.focus(input)
+
+    fireEvent.compositionStart(input)
+
+    fireEvent.change(input, {
+      target: {
+        value: '中文',
+      },
+    })
+
+    fireEvent.compositionEnd(input)
+
+    fireEvent.blur(input)
+    await waitFor(() => {
+      expect(input.value).toBe('1000')
+    })
+  })
+
+  test('dynamic digits works', async () => {
+    const Demo = () => {
+      const [digits, setDigits] = useState(2)
+
+      useEffect(() => {
+        setTimeout(() => {
+          setDigits(0)
+        })
+      }, [])
+
+      return <Stepper digits={digits} defaultValue={1.23}></Stepper>
+    }
+    const { container } = render(<Demo />)
+
+    const input = container.getElementsByTagName('input')[0]
+
+    expect(input.value).toBe('1.23')
+
+    await waitFor(() => {
+      expect(input.value).toBe('1')
+    })
   })
 })
