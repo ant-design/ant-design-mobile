acccde5cd716cbf6eec8c65af04eba7426eca2b4
dengqing <1247748612@qq.com>
2022-04-02 12:46:08 +0800

    fix: type error

--- src/components/collapse/index.ts
+++ src/components/collapse/index.ts
@@ -3,7 +3,7 @@ import { attachPropertiesToComponent } from '../../utils/attach-properties-to-co
 import { Collapse } from './collapse'
 import { CollapsePanel } from './collapse-panel'
 
-export { CollapsePanelProps } from './collapse-panel'
+export type { CollapsePanelProps } from './collapse-panel'
 export type { CollapseProps } from './collapse'
 
 export default attachPropertiesToComponent(Collapse, {
