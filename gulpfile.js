79bbab7a97480a3952b52c62ca2160cebf14053b
叶枫 <7971419+crazyair@users.noreply.github.com>
2022-04-02 11:40:20 +0800

    fix: adjust umd external config for stackblitz (#5016)

--- gulpfile.js
+++ gulpfile.js
@@ -202,8 +202,18 @@ function umdWebpack() {
           },
           externals: [
             {
-              'react': 'React',
-              'react-dom': 'ReactDOM',
+              react: {
+                commonjs: 'react',
+                commonjs2: 'react',
+                amd: 'react',
+                root: 'React',
+              },
+              'react-dom': {
+                commonjs: 'react-dom',
+                commonjs2: 'react-dom',
+                amd: 'react-dom',
+                root: 'ReactDOM',
+              },
             },
           ],
         },
