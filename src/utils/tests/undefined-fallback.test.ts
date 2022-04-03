cdf58148bf3457d568f61dde1a3a5b947014d620
awmleer <awmleer@icloud.com>
2022-04-03 10:42:34 +0800

    refactor: (Form.Item) undefinedFallback

---
+++ src/utils/tests/undefined-fallback.test.ts
@@ -0,0 +1,17 @@
+import { undefinedFallback } from '../undefined-fallback'
+
+describe('undefinedFallback', () => {
+  test('basic', () => {
+    expect(undefinedFallback('foo', undefined)).toBe('foo')
+    expect(undefinedFallback(undefined, 'foo')).toBe('foo')
+    expect(undefinedFallback(undefined, undefined)).toBe(undefined)
+    expect(undefinedFallback('foo', 'bar')).toBe('foo')
+    expect(undefinedFallback('foo', undefined, 'bar')).toBe('foo')
+    expect(undefinedFallback(undefined, 'foo', 'bar')).toBe('foo')
+    expect(undefinedFallback(undefined, undefined, 'foo', 'bar')).toBe('foo')
+    expect(undefinedFallback(undefined, 'foo', undefined, 'bar')).toBe('foo')
+  })
+})
+
+const a = undefinedFallback('foo', undefined)
+console.log(a)
