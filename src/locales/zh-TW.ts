349d9b46661f496738418c787e920a96a4b0ac9d
Faremax <faremax@aliyun.com>
2022-04-12 19:29:28 +0800

    enhance: supplement zh-HK and zh-TW (#5059)
    * feat:supplement zh-HK and zh-TW
    
    * test: update snapshots
    
    Co-authored-by: awmleer <awmleer@icloud.com>

--- src/locales/zh-TW.ts
+++ src/locales/zh-TW.ts
@@ -1,12 +1,18 @@
 import { mergeLocale } from '../utils/merge-locale'
 import { base } from './base'
 
+const typeTemplate = '${label}不是一個有效的${type}'
+
 const zhTW = mergeLocale(base, {
   locale: 'zh-TW',
   common: {
     confirm: '確定',
     cancel: '取消',
     loading: '加載中……',
+  },
+  Calendar: {
+    markItems: ['一', '二', '三', '四', '五', '六', '日'],
+    renderYearAndMonth: (year: number, month: number) => `${year}年${month}月`,
   },
   Cascader: {
     placeholder: '請選擇',
@@ -30,6 +36,57 @@ const zhTW = mergeLocale(base, {
     empty: {
       title: '沒有找到您需要的東西',
       description: '找找其他的吧',
+    },
+  },
+  Form: {
+    required: '必填',
+    optional: '選填',
+    defaultValidateMessages: {
+      default: '字段驗證錯誤${label}',
+      required: '請輸入${label}',
+      enum: '${label}必須是其中一個[${enum}]',
+      whitespace: '${label}不能為空字符',
+      date: {
+        format: '${label}日期格式無效',
+        parse: '${label}不能轉換為日期',
+        invalid: '${label}是一個無效日期',
+      },
+      types: {
+        string: typeTemplate,
+        method: typeTemplate,
+        array: typeTemplate,
+        object: typeTemplate,
+        number: typeTemplate,
+        date: typeTemplate,
+        boolean: typeTemplate,
+        integer: typeTemplate,
+        float: typeTemplate,
+        regexp: typeTemplate,
+        email: typeTemplate,
+        url: typeTemplate,
+        hex: typeTemplate,
+      },
+      string: {
+        len: '${label}須為${len}個字符',
+        min: '${label}最少${min}個字符',
+        max: '${label}最多${max}個字符',
+        range: '${label}須在${min}-${max}字符之間',
+      },
+      number: {
+        len: '${label}必須等於${len}',
+        min: '${label}最小值為${min}',
+        max: '${label}最大值為${max}',
+        range: '${label}須在${min}-${max}之間',
+      },
+      array: {
+        len: '須為${len}個${label}',
+        min: '最少${min}個${label}',
+        max: '最多${max}個${label}',
+        range: '${label}数量須在${min}-${max}之間',
+      },
+      pattern: {
+        mismatch: '${label}與模式不匹配${pattern}',
+      },
     },
   },
   ImageUploader: {
