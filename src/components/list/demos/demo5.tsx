526b2813025eceb18dc6f45da61b42051edc5f63
awmleer <awmleer@icloud.com>
2022-04-14 11:13:22 +0800

    doc: (List) update demo with dnd

--- src/components/list/demos/demo5.tsx
+++ src/components/list/demos/demo5.tsx
@@ -3,11 +3,10 @@ import React, { useState } from 'react'
 import {
   DragDropContext,
   Draggable,
-  DraggingStyle,
   Droppable,
   DropResult,
-  NotDraggingStyle,
 } from 'react-beautiful-dnd'
+import { users } from './users'
 
 type IUser = {
   id: string
@@ -37,14 +36,6 @@ export default () => {
     setList([...newList])
   }
 
-  const getItemStyle = (
-    isDragging: boolean,
-    draggableStyle: DraggingStyle | NotDraggingStyle | undefined
-  ) => ({
-    background: isDragging ? 'lightgreen' : '',
-    ...draggableStyle,
-  })
-
   return (
     <List header='结合 react-beautiful-dnd 实现拖拽排序'>
       <DragDropContext onDragEnd={onDragEnd}>
@@ -53,15 +44,15 @@ export default () => {
             <div ref={droppableProvided.innerRef}>
               {list.map((item, index: number) => (
                 <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(draggableProvided, draggableSnapshot) => (
+                  {(provided, snapshot) => (
                     <div
-                      ref={draggableProvided.innerRef}
-                      {...draggableProvided.draggableProps}
-                      {...draggableProvided.dragHandleProps}
-                      style={getItemStyle(
-                        draggableSnapshot.isDragging,
-                        draggableProvided.draggableProps.style
-                      )}
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={{
+                        ...provided.draggableProps.style,
+                        opacity: snapshot.isDragging ? 0.8 : 1,
+                      }}
                     >
                       <List.Item
                         key={item.name}
@@ -90,34 +81,3 @@ export default () => {
     </List>
   )
 }
-
-const users = [
-  {
-    id: '1',
-    avatar:
-      'https://images.unsplash.com/photo-1548532928-b34e3be62fc6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-    name: 'Novalee Spicer',
-    description: 'Deserunt dolor ea eaque eos',
-  },
-  {
-    id: '2',
-    avatar:
-      'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9',
-    name: 'Sara Koivisto',
-    description: 'Animi eius expedita, explicabo',
-  },
-  {
-    id: '3',
-    avatar:
-      'https://images.unsplash.com/photo-1542624937-8d1e9f53c1b9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-    name: 'Marco Gregg',
-    description: 'Ab animi cumque eveniet ex harum nam odio omnis',
-  },
-  {
-    id: '4',
-    avatar:
-      'https://images.unsplash.com/photo-1546967191-fdfb13ed6b1e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-    name: 'Edith Koenig',
-    description: 'Commodi earum exercitationem id numquam vitae',
-  },
-]
---
+++ src/components/list/demos/users.ts
@@ -0,0 +1,30 @@
+export const users = [
+  {
+    id: '1',
+    avatar:
+      'https://images.unsplash.com/photo-1548532928-b34e3be62fc6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+    name: 'Novalee Spicer',
+    description: 'Deserunt dolor ea eaque eos',
+  },
+  {
+    id: '2',
+    avatar:
+      'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9',
+    name: 'Sara Koivisto',
+    description: 'Animi eius expedita, explicabo',
+  },
+  {
+    id: '3',
+    avatar:
+      'https://images.unsplash.com/photo-1542624937-8d1e9f53c1b9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+    name: 'Marco Gregg',
+    description: 'Ab animi cumque eveniet ex harum nam odio omnis',
+  },
+  {
+    id: '4',
+    avatar:
+      'https://images.unsplash.com/photo-1546967191-fdfb13ed6b1e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+    name: 'Edith Koenig',
+    description: 'Commodi earum exercitationem id numquam vitae',
+  },
+]
