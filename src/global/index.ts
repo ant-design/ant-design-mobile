18ed01928d6f7ca1a40b07ec085f7d545b20941b
awmleer <awmleer@icloud.com>
2022-04-26 18:45:13 +0800

    chore: add some comment

--- src/global/index.ts
+++ src/global/index.ts
@@ -2,5 +2,7 @@ import './global.less'
 import { canUseDom } from '../utils/can-use-dom'
 
 if (canUseDom) {
+  // Make sure the `:active` CSS selector of `button` and `a` take effect
+  // See: https://stackoverflow.com/questions/3885018/active-pseudo-class-doesnt-work-in-mobile-safari
   document.addEventListener('touchstart', () => {}, true)
 }
