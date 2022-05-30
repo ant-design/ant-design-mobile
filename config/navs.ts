64ccff9797b1a5cc3632832221155b1138579ef9
awmleer <awmleer@icloud.com>
2022-05-30 13:07:48 +0800

    chore: seperate navs and menus configs

---
+++ config/navs.ts
@@ -0,0 +1,106 @@
+export const navs = {
+  'en': [
+    {
+      title: 'Guide',
+      path: '/guide',
+    },
+    {
+      title: 'Components',
+      path: '/components',
+    },
+    {
+      title: 'Discover More',
+      children: [
+        {
+          title: 'Playground',
+          path: 'https://codesandbox.io/s/antd-mobile-snrxr?file=/package.json',
+        },
+        {
+          title: 'Roadmap',
+          path: 'https://github.com/ant-design/ant-design-mobile/discussions/3924',
+        },
+        {
+          title: 'Contributing',
+          path: 'https://github.com/ant-design/ant-design-mobile/blob/master/.github/CONTRIBUTING.md',
+        },
+      ],
+    },
+    {
+      title: 'Legacy Versions',
+      children: [
+        {
+          title: 'v2',
+          path: 'https://antd-mobile-v2.surge.sh',
+        },
+        {
+          title: 'v3 alpha',
+          path: 'https://antd-mobile-v3.surge.sh',
+        },
+      ],
+    },
+    {
+      title: 'Releases',
+      path: 'https://github.com/ant-design/ant-design-mobile/releases',
+    },
+    {
+      title: 'GitHub',
+      path: 'https://github.com/ant-design/ant-design-mobile',
+    },
+  ],
+  'zh': [
+    {
+      title: '指南',
+      path: '/zh/guide',
+    },
+    {
+      title: '组件',
+      path: '/zh/components',
+    },
+    {
+      title: '了解更多',
+      children: [
+        {
+          title: '在线体验',
+          path: 'https://codesandbox.io/s/antd-mobile-snrxr?file=/package.json',
+        },
+        {
+          title: 'Roadmap',
+          path: 'https://github.com/ant-design/ant-design-mobile/discussions/3924',
+        },
+        {
+          title: '参与贡献',
+          path: 'https://github.com/ant-design/ant-design-mobile/blob/master/.github/CONTRIBUTING.md',
+        },
+        {
+          title: '博客',
+          path: 'https://www.yuque.com/awmleer/rocket',
+        },
+      ],
+    },
+    {
+      title: '返回旧版',
+      children: [
+        {
+          title: 'v2',
+          path: 'https://antd-mobile-v2.surge.sh',
+        },
+        {
+          title: 'v2 国内站点',
+          path: 'https://antd-mobile-doc-v2.gitee.io',
+        },
+        {
+          title: 'v3 alpha',
+          path: 'https://antd-mobile-v3.surge.sh',
+        },
+      ],
+    },
+    {
+      title: '发布日志',
+      path: 'https://github.com/ant-design/ant-design-mobile/releases',
+    },
+    {
+      title: 'GitHub',
+      path: 'https://github.com/ant-design/ant-design-mobile',
+    },
+  ],
+}
