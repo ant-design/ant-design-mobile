8a968d4f7c8c1c0149e2ffa9441fa1ccb85336e6
awmleer <awmleer@icloud.com>
2022-06-09 22:10:15 +0800

    fix(Form): `setFieldsValue` should trigger the rerender of `Form.Subscribe` now

--- src/components/form/form-subscribe.tsx
+++ src/components/form/form-subscribe.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { FC, memo } from 'react'
+import { useWatch } from 'rc-field-form'
+import { useUpdate } from 'ahooks'
 import type { FormInstance } from 'rc-field-form'
 import type { NamePath } from 'rc-field-form/es/interface'
 import Form from '.'
+import { useIsomorphicUpdateLayoutEffect } from '../../utils/use-isomorphic-update-layout-effect'
 
 type RenderChildren<Values = any> = (
   changedValues: Record<string, any>,
@@ -14,16 +17,35 @@ export interface FormSubscribeProps {
   children: ChildrenType
 }
 
-export const FormSubscribe: React.VFC<FormSubscribeProps> = ({
-  children,
-  to,
-}) => {
+export const FormSubscribe: FC<FormSubscribeProps> = props => {
+  const update = useUpdate()
   return (
-    <Form.Item noStyle dependencies={to}>
-      {form => {
-        const changedValues = form.getFieldsValue(to)
-        return children(changedValues, form)
-      }}
+    <Form.Item noStyle dependencies={props.to}>
+      {form => (
+        <>
+          {props.children(form.getFieldsValue(props.to), form)}
+          {props.to.map(namePath => (
+            <Watcher
+              key={namePath.toString()}
+              form={form}
+              namePath={namePath}
+              onChange={update}
+            />
+          ))}
+        </>
+      )}
     </Form.Item>
   )
 }
+
+export const Watcher = memo<{
+  form: FormInstance
+  namePath: NamePath
+  onChange: () => void
+}>(props => {
+  const value = useWatch(props.namePath, props.form)
+  useIsomorphicUpdateLayoutEffect(() => {
+    props.onChange()
+  }, [value])
+  return null
+})
