5aa6d73beecb0b64f17c29854bbe0aee46fb25e4
awmleer <awmleer@icloud.com>
2022-06-17 11:44:00 +0800

    doc(Radio): update demos

--- src/components/radio/demos/demo1.tsx
+++ src/components/radio/demos/demo1.tsx
@@ -1,21 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Radio, Space } from 'antd-mobile'
 import { DemoBlock } from 'demos'
-import { SmileOutline, SmileFill } from 'antd-mobile-icons'
 
 export default () => {
-  const [value, setValue] = useState<string>()
   return (
     <>
       <DemoBlock title='基础用法'>
         <Space direction='vertical'>
-          <div
-            onClick={() => {
-              console.log('点击了')
-            }}
-          >
-            <Radio />
-          </div>
+          <Radio />
           <Radio>有描述的单选框</Radio>
         </Space>
       </DemoBlock>
@@ -29,54 +21,6 @@ export default () => {
           <Radio block>块级元素</Radio>
           <Radio>非块级元素</Radio>
         </Space>
-      </DemoBlock>
-
-      <DemoBlock title='自定义图标'>
-        <Radio.Group
-          value={value}
-          onChange={val => {
-            setValue(val as string)
-          }}
-        >
-          <Space direction='vertical'>
-            <Radio
-              value='radio1'
-              icon={checked =>
-                checked ? (
-                  <SmileFill style={{ color: 'var(--adm-color-primary)' }} />
-                ) : (
-                  <SmileOutline style={{ color: 'var(--adm-color-weak)' }} />
-                )
-              }
-            >
-              单选框一
-            </Radio>
-            <Radio
-              value='radio2'
-              icon={checked =>
-                checked ? (
-                  <SmileFill style={{ color: 'var(--adm-color-primary)' }} />
-                ) : (
-                  <SmileOutline style={{ color: 'var(--adm-color-weak)' }} />
-                )
-              }
-            >
-              单选框二
-            </Radio>
-          </Space>
-        </Radio.Group>
-      </DemoBlock>
-
-      <DemoBlock title='自定义大小'>
-        <Radio
-          style={{
-            '--icon-size': '18px',
-            '--font-size': '14px',
-            '--gap': '6px',
-          }}
-        >
-          小号的单选框
-        </Radio>
       </DemoBlock>
 
       <DemoBlock title='禁用状态'>
--- src/components/radio/demos/demo2.tsx
+++ src/components/radio/demos/demo2.tsx
@@ -1,19 +1,58 @@
-import { Radio } from 'antd-mobile'
+import React, { useState } from 'react'
+import { Radio, Space } from 'antd-mobile'
+import { SmileOutline, SmileFill } from 'antd-mobile-icons'
 import { DemoBlock } from 'demos'
-import React from 'react'
 
 export default () => {
+  const [value, setValue] = useState<string>()
   return (
-    <DemoBlock title='自定义大小'>
-      <Radio
-        style={{
-          '--icon-size': '18px',
-          '--font-size': '14px',
-          '--gap': '6px',
-        }}
-      >
-        小号的单选框
-      </Radio>
-    </DemoBlock>
+    <>
+      <DemoBlock title='自定义大小'>
+        <Radio
+          style={{
+            '--icon-size': '18px',
+            '--font-size': '14px',
+            '--gap': '6px',
+          }}
+        >
+          小号的单选框
+        </Radio>
+      </DemoBlock>
+      <DemoBlock title='自定义图标'>
+        <Radio.Group
+          value={value}
+          onChange={val => {
+            setValue(val as string)
+          }}
+        >
+          <Space direction='vertical'>
+            <Radio
+              value='radio1'
+              icon={checked =>
+                checked ? (
+                  <SmileFill style={{ color: 'var(--adm-color-primary)' }} />
+                ) : (
+                  <SmileOutline style={{ color: 'var(--adm-color-weak)' }} />
+                )
+              }
+            >
+              单选框一
+            </Radio>
+            <Radio
+              value='radio2'
+              icon={checked =>
+                checked ? (
+                  <SmileFill style={{ color: 'var(--adm-color-primary)' }} />
+                ) : (
+                  <SmileOutline style={{ color: 'var(--adm-color-weak)' }} />
+                )
+              }
+            >
+              单选框二
+            </Radio>
+          </Space>
+        </Radio.Group>
+      </DemoBlock>
+    </>
   )
 }
