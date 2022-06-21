e63ed10484167e6232de0a0615df2c037d5228df
Leisure <675258207@qq.com>
2022-06-21 12:00:36 +0800

    doc(Input): add password input demo with `Form.Item`

--- src/components/input/demos/demo2.tsx
+++ src/components/input/demos/demo2.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Input } from 'antd-mobile'
 import { DemoBlock } from 'demos'
 import styles from './demo2.less'
+import { EyeInvisibleOutline, EyeOutline } from 'antd-mobile-icons'
 
 export default () => {
+  const [visible, setVisible] = useState(false)
   return (
     <>
       <DemoBlock title='配合列表使用-水平布局' padding='0'>
@@ -39,6 +41,33 @@ export default () => {
             }
           >
             <Input placeholder='请输入验证码' clearable />
+          </Form.Item>
+        </Form>
+      </DemoBlock>
+
+      <DemoBlock title='带密码可见' padding='0'>
+        <Form layout='horizontal'>
+          <Form.Item label='用户名' name='username'>
+            <Input placeholder='请输入用户名' clearable />
+          </Form.Item>
+          <Form.Item
+            label='密码'
+            name='password'
+            extra={
+              <div className={styles.eye}>
+                {!visible ? (
+                  <EyeInvisibleOutline onClick={() => setVisible(true)} />
+                ) : (
+                  <EyeOutline onClick={() => setVisible(false)} />
+                )}
+              </div>
+            }
+          >
+            <Input
+              placeholder='请输入密码'
+              clearable
+              type={visible ? 'text' : 'password'}
+            />
           </Form.Item>
         </Form>
       </DemoBlock>
