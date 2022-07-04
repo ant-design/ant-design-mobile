fd22bbbd342064d90e2b6c7243f33229f7af7f3b
miracles1919 <lujie@yuantutech.com>
2022-07-04 14:21:40 +0800

    test(PickerView): add test (#5431)
    * test(PickerView): add test
    
    * test(Picker): adjust test

--- src/components/picker-view/tests/picker-view.test.tsx
+++ src/components/picker-view/tests/picker-view.test.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { render, testA11y, fireEvent, waitFor, actSleep } from 'testing'
+import { render, testA11y, fireEvent, waitFor, actSleep, screen } from 'testing'
 import PickerView from '../'
 import { basicColumns } from '../demos/columns-data'
 
@@ -41,5 +41,52 @@ describe('PickerView', () => {
     fireEvent.mouseUp(wheelEl)
     await actSleep(100)
     expect(getByTestId('res')).toHaveTextContent(JSON.stringify(['Thur', 'am']))
+  })
+
+  // https://github.com/ant-design/ant-design-mobile/issues/5426
+  test('wheel should scroll to the correct position', async () => {
+    const App = () => {
+      const [bool, setBool] = useState(true)
+      return (
+        <>
+          <button onClick={() => setBool(!bool)}>change</button>
+          <PickerView
+            columns={
+              bool
+                ? [
+                    [
+                      { label: '1', value: '' },
+                      { label: '2', value: '2' },
+                      { label: '3', value: '3' },
+                    ],
+                  ]
+                : [
+                    [
+                      { label: '1', value: '' },
+                      { label: '2', value: '2' },
+                    ],
+                  ]
+            }
+          />
+        </>
+      )
+    }
+
+    render(<App />)
+    const wheelEl = document.body.querySelectorAll(
+      `.${classPrefix}-column-wheel`
+    )[0]
+    fireEvent.mouseDown(wheelEl, {
+      clientY: 0,
+      buttons: 1,
+    })
+    fireEvent.mouseMove(wheelEl, {
+      clientY: -200,
+      buttons: 1,
+    })
+    fireEvent.mouseUp(wheelEl)
+    expect(wheelEl).toHaveStyle('transform: translateY(-68px)')
+    fireEvent.click(screen.getByText('change'))
+    await waitFor(() => expect(wheelEl).toHaveStyle('transform: none'))
   })
 })
