4ecbbf2cb663e7f2e912b2912f7f489c9f67086d
awmleer <awmleer@icloud.com>
2022-07-06 14:43:02 +0800

    enhance: refactor `mergeLocale` and `mergeProps` to reduce bundle size

--- src/utils/with-default-props.tsx
+++ src/utils/with-default-props.tsx
@@ -1,15 +1,13 @@
-import assign from 'lodash/assign'
 import assignWith from 'lodash/assignWith'
-import isUndefined from 'lodash/isUndefined'
 
 export function mergeProps<A, B>(a: A, b: B): B & A
 export function mergeProps<A, B, C>(a: A, b: B, c: C): C & B & A
 export function mergeProps(...items: any[]) {
   function customizer(objValue: any, srcValue: any) {
-    return isUndefined(srcValue) ? objValue : srcValue
+    return srcValue === undefined ? objValue : srcValue
   }
 
-  let ret = assign({}, items[0])
+  let ret = { ...items[0] }
   for (let i = 1; i < items.length; i++) {
     ret = assignWith(ret, items[i], customizer)
   }
