8799ae640165789663aba1a9f4ca8307179a84e3
五月的夏天 <285258675@qq.com>
2022-07-21 19:05:54 +0800

    refactor(SwipeAction): optimization onActionsReveal

--- src/components/swipe-action/swipe-action.tsx
+++ src/components/swipe-action/swipe-action.tsx
@@ -148,16 +148,15 @@ export const SwipeAction = forwardRef<SwipeActionRef, SwipeActionProps>(
     useImperativeHandle(ref, () => ({
       show: (side: 'left' | 'right' = 'right') => {
         if (side === 'right') {
-          p.onActionsReveal?.('right')
           api.start({
             x: -getRightWidth(),
           })
         } else if (side === 'left') {
-          p.onActionsReveal?.('left')
           api.start({
             x: getLeftWidth(),
           })
         }
+        p.onActionsReveal?.(side)
       },
       close,
     }))
