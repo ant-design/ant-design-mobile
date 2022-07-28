ba78f10c14150b9c2a2cdb6a38f18c606b5211cc
awmleer <awmleer@icloud.com>
2022-07-28 17:54:01 +0800

    fix(PickerView): add `mouseWheel` to custom memo compare

--- src/components/picker-view/wheel.tsx
+++ src/components/picker-view/wheel.tsx
@@ -238,6 +238,7 @@ export const Wheel = memo<Props>(
     if (prev.value !== next.value) return false
     if (prev.onSelect !== next.onSelect) return false
     if (prev.renderLabel !== next.renderLabel) return false
+    if (prev.mouseWheel !== next.mouseWheel) return false
     if (!isEqual(prev.column, next.column)) {
       return false
     }
