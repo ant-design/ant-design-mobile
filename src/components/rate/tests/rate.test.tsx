5d81c1f7e57e196af6942238187ce1ab5afeaa43
Cyr <cyrbuzz@foxmail.com>
2022-08-01 18:27:16 +0800

    test(Rate): improve

--- src/components/rate/tests/rate.test.tsx
+++ src/components/rate/tests/rate.test.tsx
@@ -5,6 +5,18 @@ import Rate from '..'
 const classPrefix = `adm-rate`
 
 describe('Rate', () => {
+  const getBoundingClientRectMock = jest.spyOn(
+    HTMLElement.prototype,
+    'getBoundingClientRect'
+  )
+
+  beforeAll(() => {
+    getBoundingClientRectMock.mockReturnValue({
+      left: 12,
+      width: 155.9375,
+    } as DOMRect)
+  })
+
   test('a11y', async () => {
     await testA11y(<Rate />)
   })
@@ -13,19 +25,6 @@ describe('Rate', () => {
     const onChange = jest.fn()
     render(<Rate value={4} readOnly onChange={onChange} />)
     const radioGroup = screen.getByRole('radiogroup')
-    radioGroup.getBoundingClientRect = jest.fn(
-      () =>
-        ({
-          bottom: 82,
-          height: 30,
-          left: 12,
-          right: 167.9375,
-          top: 52,
-          width: 155.9375,
-          x: 12,
-          y: 52,
-        } as DOMRect)
-    )
     const radio = screen.getByRole('radio', { name: '1' })
     mockDrag(radioGroup, [
       {
@@ -44,19 +43,6 @@ describe('Rate', () => {
     const radio = screen.getByRole('radio', { name: '1.5' })
     const shouldNotActiveRadio = screen.getByRole('radio', { name: '2' })
     const radioGroup = screen.getByRole('radiogroup')
-    radioGroup.getBoundingClientRect = jest.fn(
-      () =>
-        ({
-          bottom: 82,
-          height: 30,
-          left: 12,
-          right: 167.9375,
-          top: 52,
-          width: 155.9375,
-          x: 12,
-          y: 52,
-        } as DOMRect)
-    )
 
     mockDrag(radioGroup, [
       {
@@ -83,46 +69,10 @@ describe('Rate', () => {
     ).toHaveLength(0)
   })
 
-  test('can clear', () => {
-    render(<Rate defaultValue={1} />)
-    const radio = screen.getByRole('radio', { name: '1' })
-    const radioGroup = screen.getByRole('radiogroup')
-    radioGroup.getBoundingClientRect = jest.fn(
-      () =>
-        ({
-          bottom: 82,
-          height: 30,
-          left: 12,
-          right: 167.9375,
-          top: 52,
-          width: 155.9375,
-          x: 12,
-          y: 52,
-        } as DOMRect)
-    )
-    expect(radio).toHaveClass(`${classPrefix}-star-active`)
-    // fireEvent.click(radio)
-    mockDrag(radioGroup, [{ clientX: 40 }, { clientX: 40 }])
-    expect(radio).not.toHaveClass(`${classPrefix}-star-active`)
-  })
-
   test('can not clear', () => {
     render(<Rate allowClear={false} defaultValue={1} />)
     const radio = screen.getByRole('radio', { name: '1' })
     const radioGroup = screen.getByRole('radiogroup')
-    radioGroup.getBoundingClientRect = jest.fn(
-      () =>
-        ({
-          bottom: 82,
-          height: 30,
-          left: 12,
-          right: 167.9375,
-          top: 52,
-          width: 155.9375,
-          x: 12,
-          y: 52,
-        } as DOMRect)
-    )
     expect(radio).toHaveClass(`${classPrefix}-star-active`)
     mockDrag(radioGroup, [{ clientX: 40 }, { clientX: 40 }])
     expect(radio).toHaveClass(`${classPrefix}-star-active`)
