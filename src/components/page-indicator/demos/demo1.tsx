a6b877b2baae677bb44a1f970d9c769a17c72f81
liangchaofei <liangchaofei.lcf@alibaba-inc.com>
2022-08-22 11:35:35 +0800

    feat(pageindicator): adjust dot coloron demo

--- src/components/page-indicator/demos/demo1.tsx
+++ src/components/page-indicator/demos/demo1.tsx
@@ -10,7 +10,14 @@ export default () => {
       </DemoBlock>
 
       <DemoBlock title='白色' background='rgb(212 212 212)'>
-        <PageIndicator total={4} current={0} color='white' />
+        <PageIndicator
+          total={4}
+          current={0}
+          color='white'
+          style={{
+            '--dot-color': 'rgba(255,255,255,0.5)',
+          }}
+        />
       </DemoBlock>
 
       <DemoBlock title='竖直方向'>
