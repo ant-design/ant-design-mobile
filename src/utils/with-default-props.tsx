a693c87308ccec1750073968dc74b1aa3eed7568
二货机器人 <smith3816@gmail.com>
2022-09-28 15:34:55 +0800

    chore: update merge props ts

--- src/utils/with-default-props.tsx
+++ src/utils/with-default-props.tsx
@@ -1,7 +1,16 @@
 import assignWith from 'lodash/assignWith'
 
-export function mergeProps<A, B>(a: A, b: B): B & A
-export function mergeProps<A, B, C>(a: A, b: B, c: C): C & B & A
+type SharedKey<V, U> = Extract<keyof V, keyof U>
+type MergeProps<V, U> = Omit<V, SharedKey<V, U>> &
+  Omit<U, SharedKey<V, U>> &
+  (Pick<V, SharedKey<V, U>> | Pick<U, SharedKey<V, U>>)
+
+export function mergeProps<A, B>(a: A, b: B): MergeProps<A, B>
+export function mergeProps<A, B, C>(
+  a: A,
+  b: B,
+  c: C
+): MergeProps<A, MergeProps<B, C>>
 export function mergeProps(...items: any[]) {
   function customizer(objValue: any, srcValue: any) {
     return srcValue === undefined ? objValue : srcValue
