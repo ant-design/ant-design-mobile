2ca2a7603e50fb27d400c2247341a4ba07fc0c8d
lujie <lujie@yuantutech.com>
2022-10-13 15:37:35 +0800

    chore: use webpack build compatible umd

--- gulpfile.js
+++ gulpfile.js
@@ -160,31 +160,6 @@ async function buildBundles(cb) {
   cb && cb()
 }
 
-function buildCompatibleUMD() {
-  return gulp
-    .src('lib/bundle/antd-mobile.umd.js')
-    .pipe(
-      babel({
-        presets: [
-          [
-            '@babel/env',
-            {
-              targets: {
-                'chrome': '49',
-                'ios': '9',
-              },
-            },
-          ],
-        ],
-      })
-    )
-    .pipe(rename('antd-mobile.compatible.umd.js'))
-    .pipe(gulp.dest('lib/bundle/'))
-    .pipe(rename('antd-mobile.js'))
-    .pipe(gulp.dest('lib/umd/'))
-}
-
-// Deprecated
 function umdWebpack() {
   return gulp
     .src('lib/es/index.js')
@@ -223,7 +198,7 @@ function umdWebpack() {
           module: {
             rules: [
               {
-                test: /\.js$/,
+                test: /\.m?js$/,
                 use: {
                   loader: 'babel-loader',
                   options: {
@@ -235,7 +210,7 @@ function umdWebpack() {
                           'modules': false,
                           'targets': {
                             'chrome': '49',
-                            'ios': '10',
+                            'ios': '9',
                           },
                         },
                       ],
@@ -276,6 +251,13 @@ function umdWebpack() {
       )
     )
     .pipe(gulp.dest('lib/umd/'))
+}
+
+function copyUmd() {
+  return gulp
+    .src(['lib/umd/antd-mobile.js'])
+    .pipe(rename('antd-mobile.compatible.umd.js'))
+    .pipe(gulp.dest('lib/bundle/'))
 }
 
 function copyMetaFiles() {
@@ -352,7 +334,7 @@ exports.default = gulp.series(
   copyMetaFiles,
   generatePackageJSON,
   buildBundles,
-  buildCompatibleUMD,
   gulp.series(init2xFolder, build2xCSS),
-  umdWebpack
+  umdWebpack,
+  copyUmd
 )
