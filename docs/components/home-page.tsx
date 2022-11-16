a0938e744093f7ba2fb183da56a7048e5d2e6254
miracles1919 <516571350@qq.com>
2022-11-16 15:48:57 +0800

    style: prettier format (#5812)

--- docs/components/home-page.tsx
+++ docs/components/home-page.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import 'antd/dist/antd.css';
-import styles from './home-page.less';
+import React, { useEffect } from 'react'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import 'antd/dist/antd.css'
+import styles from './home-page.less'
 
 export default () => {
   useEffect(() => {
-    const algoliaSearch: any = document.querySelector('.__dumi-default-search-input') || {};
-    algoliaSearch.placeholder = '搜索';
-  }, []);
+    const algoliaSearch: any =
+      document.querySelector('.__dumi-default-search-input') || {}
+    algoliaSearch.placeholder = '搜索'
+  }, [])
 
   return (
     <div className={styles.homePage}>
