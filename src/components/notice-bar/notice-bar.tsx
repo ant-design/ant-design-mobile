50771e45a23c8f7af11148089238dabd9c356fdf
miracles1919 <516571350@qq.com>
2022-11-24 11:08:53 +0800

    chore: remove props comments

--- src/components/notice-bar/notice-bar.tsx
+++ src/components/notice-bar/notice-bar.tsx
@@ -10,23 +10,14 @@ import { useMutationEffect } from '../../utils/use-mutation-effect'
 const classPrefix = `adm-notice-bar`
 
 export type NoticeBarProps = {
-  /** 通告栏的类型 */
   color?: 'default' | 'alert' | 'error' | 'info'
-  /** 开始滚动的延迟，单位 ms */
   delay?: number
-  /** 滚动速度，单位 px/s */
   speed?: number
-  /** 公告内容 */
   content: React.ReactNode
-  /** 是否可关闭 */
   closeable?: boolean
-  /** 关闭时的回调 */
   onClose?: () => void
-  /** 额外操作区域，显示在关闭按钮左侧 */
   extra?: React.ReactNode
-  /** 左侧广播图标 */
   icon?: React.ReactNode
-  /** 是否自动换行 */
   wrap?: boolean
 } & NativeProps<
   | '--background-color'
