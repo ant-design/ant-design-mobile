013365a90ff0b4b8677f6caa628aefe2693255b9
miracles1919 <516571350@qq.com>
2023-02-06 10:30:44 +0800

    feat(Ellipsis): support default expand (#5955)
    * feat(Ellipsis): support default expand
    
    * test(Ellipsis): add default expand case

--- src/components/ellipsis/tests/ellipsis.test.tsx
+++ src/components/ellipsis/tests/ellipsis.test.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render, testA11y, fireEvent } from 'testing'
 import Ellipsis from '..'
 
+const classPrefix = `adm-ellipsis`
 const content =
   '蚂蚁的企业级产品是一个庞大且复杂的体系。这类产品不仅量级巨大且功能复杂，而且变动和并发频繁，常常需要设计与开发能够快速的做出响应。同时这类产品中有存在很多类似的页面以及组件，可以通过抽象得到一些稳定且高复用性的内容。'
 
@@ -100,6 +101,23 @@ describe('Ellipsis', () => {
 
     fireEvent.click(getByTestId('ellipsis'))
     expect(onClick).toBeCalled()
+  })
+
+  test('default expand should be work', async () => {
+    const { getByText } = render(
+      <Ellipsis
+        content={content}
+        defaultExpanded
+        expandText='expand'
+        collapseText='collapse'
+      />
+    )
+
+    const ellipsis = document.querySelector(`.${classPrefix}`)
+    expect(ellipsis).not.toHaveTextContent('...')
+    expect(ellipsis).toHaveTextContent('collapse')
+    fireEvent.click(getByText('collapse'))
+    expect(ellipsis).toHaveTextContent('...')
   })
 
   test('content not exceeded', async () => {
