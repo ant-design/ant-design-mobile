23acf39096efd938663dd8272e2e4b03fe772f0e
bobihuang <bobihuang@tencent.com>
2023-03-17 14:18:35 +0800

    feat: Support customizable arrow-size

--- src/components/popover/popover.tsx
+++ src/components/popover/popover.tsx
@@ -35,6 +35,7 @@ import { useClickAway, useIsomorphicLayoutEffect } from 'ahooks'
 import { DeprecatedPlacement, Placement } from './index'
 import { normalizePlacement } from './normalize-placement'
 import { convertPx } from '../../utils/convert-px'
+import { measureCSSLength } from '../../utils/measure-css-length'
 const classPrefix = `adm-popover`
 
 export type PopoverProps = {
@@ -155,12 +156,16 @@ export const Popover = forwardRef<PopoverRef, PopoverProps>((p, ref) => {
       left: 'right',
     }[side] as string
     const { x: arrowX, y: arrowY } = middlewareData.arrow ?? {}
+    // get arrow size from css variable.
+    const arrowSize = measureCSSLength(
+      window.getComputedStyle(arrowElement).getPropertyValue('--arrow-size')
+    )
     Object.assign(arrowElement.style, {
       left: arrowX != null ? `${arrowX}px` : '',
       top: arrowY != null ? `${arrowY}px` : '',
       right: '',
       bottom: '',
-      [arrowSide]: `-${convertPx(8)}px`,
+      [arrowSide]: `-${convertPx(arrowSize || 8)}px`,
     })
     const arrowRotate = {
       top: '0deg',
