16cc807ed7667711b56fa038c1f1811f2ce51468
Francesco Violante <47946866+astronomyf@users.noreply.github.com>
2023-04-04 17:32:16 +0200

    feat(locale): add nb-NO locale
    Adding norwegian Bokmål

--- src/components/config-provider/tests/config-provider.test.tsx
+++ src/components/config-provider/tests/config-provider.test.tsx
@@ -23,6 +23,7 @@ import idID from '../../../locales/id-ID'
 import kkKZ from '../../../locales/kk-KZ'
 import itIT from '../../../locales/it-IT'
 import daDK from '../../../locales/da-DK'
+import nbNO from '../../../locales/nb-NO'
 
 const locales = [
   zhCN,
@@ -38,6 +39,7 @@ const locales = [
   idID,
   itIT,
   daDK,
+  nbNO,
 ]
 
 describe('ConfigProvider', () => {
