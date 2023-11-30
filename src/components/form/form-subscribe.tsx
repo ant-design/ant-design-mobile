48e32e8c4562d96b92e54e163aadcf49cb318e57
叶枫 <645381995@qq.com>
2023-11-30 15:59:20 +0800

    feat: cloneByNamePathList

--- src/components/form/form-subscribe.tsx
+++ src/components/form/form-subscribe.tsx
@@ -3,7 +3,7 @@ import type { FC, ReactNode } from 'react'
 import { FieldContext, useWatch } from 'rc-field-form'
 import type { FormInstance } from 'rc-field-form'
 import type { NamePath } from 'rc-field-form/es/interface'
-import pick from 'lodash/pick'
+import { cloneByNamePathList } from 'rc-field-form/es/utils/valueUtil'
 
 type RenderChildren<Values = any> = (
   changedValues: Record<string, any>,
@@ -19,7 +19,7 @@ export interface FormSubscribeProps {
 export const FormSubscribe: FC<FormSubscribeProps> = props => {
   const form = useContext(FieldContext)
 
-  const value = useWatch(values => pick(values, props.to), form)
+  const value = useWatch(values => cloneByNamePathList(values, props.to), form)
 
   // Memo to avoid useless render
   const childNode = React.useMemo(
