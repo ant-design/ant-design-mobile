cb0a6e5b3793afd8073c995037726d4164887148
Guo Yunhe <i@guoyunhe.me>
2024-01-13 12:41:07 +0800

    refactor: replace lodash/assignWith with deepmerge

--- src/utils/with-default-props.tsx
+++ src/utils/with-default-props.tsx
@@ -1,15 +1,15 @@
-import assignWith from 'lodash/assignWith'
+import merge from 'deepmerge'
+
+function customMerge() {
+  return (objValue: any, srcValue: any) => {
+    return srcValue === undefined
+      ? objValue
+      : merge(objValue, srcValue, { customMerge })
+  }
+}
 
 export function mergeProps<A, B>(a: A, b: B): B & A
 export function mergeProps<A, B, C>(a: A, b: B, c: C): C & B & A
 export function mergeProps(...items: any[]) {
-  function customizer(objValue: any, srcValue: any) {
-    return srcValue === undefined ? objValue : srcValue
-  }
-
-  let ret = { ...items[0] }
-  for (let i = 1; i < items.length; i++) {
-    ret = assignWith(ret, items[i], customizer)
-  }
-  return ret
+  return merge.all(items, { customMerge })
 }
