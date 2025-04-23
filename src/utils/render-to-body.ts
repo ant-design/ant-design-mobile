a21958513f99012ac4f86db1345483e179010f64
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-04-23 18:13:54 +0800

    chore: equivalence

--- src/utils/render-to-body.ts
+++ src/utils/render-to-body.ts
@@ -1,15 +1,8 @@
 import type { ReactElement } from 'react'
-import { render, unmount as reactUnmount } from './render'
+import { getReactRender } from './unstable-render'
 
 export function renderToBody(element: ReactElement) {
   const container = document.createElement('div')
   document.body.appendChild(container)
-  function unmount() {
-    const unmountResult = reactUnmount(container)
-    if (unmountResult && container.parentNode) {
-      container.parentNode.removeChild(container)
-    }
-  }
-  render(element, container)
-  return unmount
+  return getReactRender()(element, container)
 }
