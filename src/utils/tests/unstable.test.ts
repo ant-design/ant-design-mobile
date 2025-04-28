1c99d16d04b645d421f1b64b42d427e878745759
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-04-28 14:24:53 +0800

    add unit test

---
+++ src/utils/tests/unstable.test.ts
@@ -0,0 +1,48 @@
+import { Dialog, unstableSetRender } from 'antd-mobile'
+import * as ReactDOM from 'react-dom'
+import { screen, waitFakeTimer19 } from 'testing'
+
+// TODO: Remove this. Mock for React 19
+jest.mock('react-dom', () => {
+  const realReactDOM = jest.requireActual('react-dom')
+
+  if (realReactDOM.version.startsWith('19')) {
+    const realReactDOMClient = jest.requireActual('react-dom/client')
+    realReactDOM.createRoot = realReactDOMClient.createRoot
+  }
+
+  return realReactDOM
+})
+
+describe('unstable', () => {
+  beforeEach(() => {
+    jest.useFakeTimers()
+  })
+
+  afterEach(() => {
+    jest.useRealTimers()
+  })
+
+  it('unstableSetRender', async () => {
+    if (ReactDOM.version.startsWith('19')) {
+      unstableSetRender((node, container) => {
+        const root = (ReactDOM as any).createRoot(container)
+        root.render(node)
+        return async () => {
+          root.unmount()
+        }
+      })
+
+      Dialog.show({ content: 'unstableSetRender' })
+
+      await waitFakeTimer19()
+
+      expect(screen.getByRole('dialog')).toBeTruthy()
+    }
+  })
+
+  it('unstableSetRender without param', async () => {
+    const currentRender = unstableSetRender()
+    expect(currentRender).toBeInstanceOf(Function)
+  })
+})
