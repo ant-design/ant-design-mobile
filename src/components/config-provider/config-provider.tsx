d463dbcddea0d439480d51231033fa9d52c68f42
刘欢 <lh01217311@antgroup.com>
2025-10-20 17:23:34 +0800

    feat(Collapse configProvider): unified provision of custom prefixCls

--- src/components/config-provider/config-provider.tsx
+++ src/components/config-provider/config-provider.tsx
@@ -48,6 +48,12 @@ type Config = {
 }
 export const defaultPrefixCls = 'adm'
 
+const getPrefixCls = (suffixCls?: string, customizePrefixCls?: string) => {
+  if (customizePrefixCls) {
+    return customizePrefixCls
+  }
+  return suffixCls ? `${defaultPrefixCls}-${suffixCls}` : defaultPrefixCls
+}
 export const defaultConfigRef: {
   current: Config
 } = {
