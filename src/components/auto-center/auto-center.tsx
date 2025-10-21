015185b87bd087740519c85144cfa4f7957901d0
刘欢 <lh01217311@antgroup.com>
2025-10-21 10:54:41 +0800

    feat(AutoCenter): unified provision of custom prefixCls

--- src/components/auto-center/auto-center.tsx
+++ src/components/auto-center/auto-center.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
 import type { FC, ReactNode } from 'react'
+import React from 'react'
 import { NativeProps, withNativeProps } from '../../utils/native-props'
+import { useConfig } from '../config-provider'
 
-const classPrefix = 'adm-auto-center'
+export type AutoCenterProps = {
+  children?: ReactNode
+  prefixCls?: string
+} & NativeProps
 
-export type AutoCenterProps = { children?: ReactNode } & NativeProps
-
-export const AutoCenter: FC<AutoCenterProps> = props =>
-  withNativeProps(
+export const AutoCenter: FC<AutoCenterProps> = props => {
+  const { getPrefixCls } = useConfig()
+  const prefixCls = getPrefixCls('auto-center', props.prefixCls)
+  return withNativeProps(
     props,
-    <div className={classPrefix}>
-      <div className={`${classPrefix}-content`}>{props.children}</div>
+    <div className={prefixCls}>
+      <div className={`${prefixCls}-content`}>{props.children}</div>
     </div>
   )
+}
