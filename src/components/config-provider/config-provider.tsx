4e75894441b9facceef4c07d7dbf0906dab5cf79
刘欢 <lh01217311@antgroup.com>
2025-10-21 10:37:42 +0800

    feat: default getPrefixCls

--- src/components/config-provider/config-provider.tsx
+++ src/components/config-provider/config-provider.tsx
@@ -48,12 +48,6 @@ type Config = {
 }
 export const defaultPrefixCls = 'adm'
 
-const getPrefixCls = (suffixCls?: string, customizePrefixCls?: string) => {
-  if (customizePrefixCls) {
-    return customizePrefixCls
-  }
-  return suffixCls ? `${defaultPrefixCls}-${suffixCls}` : defaultPrefixCls
-}
 export const defaultConfigRef: {
   current: Config
 } = {
