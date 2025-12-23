520ffca08f76e4c84de2a412698eba651a13f01d
Chaos（拾锋） <edokeh@163.com>
2025-12-23 11:57:26 +0800

    fix: 向前兼容分离式写法 (#7010)
    * fix: 向前兼容分离式写法
    
    * docs: 补充文档，强调不推荐分离式写法
    
    * fix: 禁用输入框时不会唤起键盘
    
    * fix: lint
    
    * fix: 补充注释
    
    * docs: 修正小问题
    
    * docs: 把不推荐使用的方式拆成单独 demo
    
    * fix: 修正注释，完善文档

--- src/components/virtual-input/virtual-input.tsx
+++ src/components/virtual-input/virtual-input.tsx
@@ -145,9 +145,13 @@ export const VirtualInput = forwardRef<VirtualInputRef, VirtualInputProps>(
       mergedProps.onBlur?.()
     }
 
-    useClickOutside(() => {
-      setBlur()
-    }, rootRef)
+    useClickOutside(
+      () => {
+        setBlur()
+      },
+      rootRef,
+      !!mergedProps.keyboard
+    )
 
     const keyboard = mergedProps.keyboard
     const keyboardElement =
@@ -175,6 +179,13 @@ export const VirtualInput = forwardRef<VirtualInputRef, VirtualInputProps>(
         },
         visible: hasFocus,
         onClose: () => {
+          const activeElement = document.activeElement as HTMLElement
+
+          if (activeElement === contentRef.current) {
+            // 点击 NumberKeyboard 的确认和关闭时不会触发输入框的 blur 事件，手动调用让其失焦，否则之后无法 focus
+            contentRef.current?.blur()
+          }
+
           setBlur()
           keyboard.props.onClose?.()
         },
@@ -183,6 +194,9 @@ export const VirtualInput = forwardRef<VirtualInputRef, VirtualInputProps>(
 
     // 点击输入框时，将光标置于最后
     const setCaretPositionToEnd = (e: React.MouseEvent<HTMLDivElement>) => {
+      if (mergedProps.disabled) {
+        return
+      }
       if (caretPosition !== value.length) {
         setCaretPosition(value.length)
         mergedProps.cursor?.onMove?.(value.length)
